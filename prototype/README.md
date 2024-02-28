3b41d36ad1891acde1065788e94424d9f088b584
Kevin Etchells <kevin.etchells@cabinetoffice.gov.uk>
2024-02-28 11:49:39 +0000

    Add frontend prototype

---
+++ prototype/README.md
@@ -0,0 +1,13 @@
+# Consultation Analyser Frontend Prototype
+
+Using the Gov.uk Prototype Kit. This is work in progress, not all pages are available yet.
+
+## How to run
+
+Ensure you have a recent version of Node.js installed (v16 or greater). Then, from the prototype directory, run:
+`npm install`
+
+To start development server:
+`npm run dev`
+
+Start at http://localhost:3000/
---
+++ prototype/app/data/session-data-defaults.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  questions: [
+    "Do you agree or disagree that the age of sale for tobacco products should be changed so that anyone born on or after 1 January 2009 will never be legally sold (and also in Scotland, never legally purchase) tobacco products?", 
+    "Do you think that proxy sales should also be prohibited?", 
+    "Do you agree or disagree that all tobacco products, cigarette papers and herbal smoking products should be covered in the new legislation?",
+    "Do you agree or disagree that warning notices in retail premises will need to be changed to read 'it is illegal to sell tobacco products to anyone born on or after 1 January 2009' when the law comes into effect?"
+  ],
+  responses: []
+
+}
