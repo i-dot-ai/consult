973b939df98f75866a040a66d1c0f169af73c463
Kevin Etchells <kevin.etchells@cabinetoffice.gov.uk>
2024-02-29 14:29:41 +0000

    Add tabs and charts to index.html

--- prototype/app/assets/javascripts/application.js
+++ prototype/app/assets/javascripts/application.js
@@ -1,8 +1,34 @@
-//
-// For guidance on how to add JavaScript see:
-// https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
-//
+// @ts-check
 
-window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-})
+
+// Key findings charts
+(() => {
+
+  /** @type {NodeListOf<HTMLCanvasElement>} */
+  const keyFindingCharts = document.querySelectorAll('[data-yes]');
+
+  keyFindingCharts.forEach((chart) => {
+
+    // @ts-ignore
+    new Chart(chart, {
+      type: 'doughnut',
+      data: {
+        labels: ['In favour', 'Opposed', 'Undecided'],
+        datasets: [{
+          data: [chart.dataset.yes, chart.dataset.no, 100 - parseInt(chart.dataset.yes || '0') - parseInt(chart.dataset.no || '0')],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            display: false
+          }
+        }
+      }
+    });
+
+  });
+
+})();
