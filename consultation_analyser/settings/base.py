fcd17dc178a21fcf6f547e500c6f00920ecd4859
Duncan Brown <duncanjbrown@gmail.com>
2024-02-29 12:30:29 +0000

    Introduce pytest-django and settings.test

--- consultation_analyser/settings/base.py
+++ consultation_analyser/settings/base.py
@@ -16,10 +16,10 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"), overwrite=False)
 
 env = environ.Env(DEBUG=(bool, False))
 
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.auth",
+    "consultation_analyser.consultations",
 ]
 
 MIDDLEWARE = [
@@ -42,7 +44,6 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
