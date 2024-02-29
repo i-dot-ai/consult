0b29bb5dbb00e4ca7327909fd5637ae6e00ba656
Duncan Brown <duncanjbrown@gmail.com>
2024-02-29 17:49:21 +0000

    Revert change to UUIDField
    Django doesn't support this yet :/

--- consultation_analyser/settings.py
+++ consultation_analyser/settings.py
@@ -113,4 +113,4 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.UUIDField"
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
