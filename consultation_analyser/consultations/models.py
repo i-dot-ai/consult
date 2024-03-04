49cd3dbe4f7655e17693767fca3c0ff84a9bdfd7
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-03-04 17:18:18 +0000

    Add models

--- consultation_analyser/consultations/models.py
+++ consultation_analyser/consultations/models.py
@@ -1,7 +1,78 @@
-# Create your models here.
+import uuid
 from django.db import models
 
 
-class Question(models.Model):
-    text = models.CharField(max_length=None)  # no idea what's a sensible value for max_length
-    slug = models.CharField(max_length=256)
+class UUIDPrimaryKeyModel(models.Model):
+    class Meta:
+        abstract = True
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(editable=False, auto_now_add=True)
+    modified_at = models.DateTimeField(editable=False, auto_now=True)
+
+    class Meta:
+        abstract = True
+        ordering = ["created_at"]
+
+
+class Consultation(UUIDPrimaryKeyModel, TimeStampedModel):
+    name = models.CharField(max_length=256)
+    slug = models.CharField(null=False, max_length=256)
+
+    class Meta(UUIDPrimaryKeyModel.Meta, TimeStampedModel.Meta):
+        pass
+
+
+class Section(UUIDPrimaryKeyModel, TimeStampedModel):
+    consultation = models.ForeignKey(Consultation, on_delete=models.CASCADE, null=True)
+    name = models.TextField()
+    slug = models.CharField(null=False, max_length=256)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["slug", "consultation"], name="unique_section_consultation"),
+        ]
+
+
+class Question(UUIDPrimaryKeyModel, TimeStampedModel):
+    text = models.CharField(null=False, max_length=None)  # no idea what's a sensible value for max_length
+    slug = models.CharField(null=False, max_length=256)
+    has_free_text = models.BooleanField(default=False)
+    multiple_choice_options = models.JSONField(null=True)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE, null=True)
+
+    class Meta(UUIDPrimaryKeyModel.Meta, TimeStampedModel.Meta):
+        constraints = [
+            models.UniqueConstraint(fields=["slug", "section"], name="unique_question_section"),
+        ]
+
+
+class ConsultationResponse(UUIDPrimaryKeyModel, TimeStampedModel):
+    # Characteristics may be different for different consultations
+    pass
+
+    class Meta(UUIDPrimaryKeyModel.Meta, TimeStampedModel.Meta):
+        pass
+
+
+class Theme(UUIDPrimaryKeyModel, TimeStampedModel):
+    label = models.CharField(max_length=256, blank=True)
+    summary = models.TextField(blank=True)
+    keywords = models.JSONField(default=list)
+
+    class Meta(UUIDPrimaryKeyModel.Meta, TimeStampedModel.Meta):
+        pass
+
+
+class Answer(UUIDPrimaryKeyModel, TimeStampedModel):
+    multiple_choice_responses = models.JSONField(null=True)  # Multiple choice can have more than one response
+    free_text = models.TextField(null=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    consultation_response = models.ForeignKey(ConsultationResponse, on_delete=models.CASCADE)
+    theme = models.ForeignKey(Theme, on_delete=models.CASCADE, null=True)  # For now, just one theme per answer
+
+    class Meta(UUIDPrimaryKeyModel.Meta, TimeStampedModel.Meta):
+        pass
