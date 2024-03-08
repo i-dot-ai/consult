0a457effd67930a591d7d239b247c166536345e4
Kevin Etchells <kevin.etchells@cabinetoffice.gov.uk>
2024-03-08 11:26:29 +0000

    Replace example responses with a link to filtered responses page

--- prototype/app/data/session-data-defaults.js
+++ prototype/app/data/session-data-defaults.js
@@ -6,6 +6,17 @@ module.exports = {
     "Do you agree or disagree that all tobacco products, cigarette papers and herbal smoking products should be covered in the new legislation?",
     "Do you agree or disagree that warning notices in retail premises will need to be changed to read 'it is illegal to sell tobacco products to anyone born on or after 1 January 2009' when the law comes into effect?"
   ],
-  responses: []
+  themes: [
+    {"theme": "Preventing related illnesses starts now", value: 671},
+    {"theme": "... is becoming increasingly popular among young people who perceive it as a safer alternative.", value: 443},
+    {"theme": "Strong public opposition to change in sale ages", value: 163},
+    {"theme": "Limit children's access to products", value: 136},
+    {"theme": "Legalise ban on products for current and future generations", value: 127},
+    {"theme": "Age of sale for products should be changed to eradicate ...", value: 112},
+    {"theme": "It is harmful and should not be legally sold to anyone born after 1 January 2009", value: 105},
+    {"theme": "... and ... both need to be completely unavailable", value: 91},
+    {"theme": "Protect future generations from the harmful effects", value: 84},
+    {"theme": "The majority of respondents support raising the legal age of sale for products to 18 years old in the UK, believing it will help reduce peer pressure among young people", value: 81}
+  ]
 
 }
