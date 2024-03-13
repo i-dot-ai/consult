655196b28647de6b72eba32161c5a63147c82c2b
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-03-13 10:02:21 +0000

    Add to questions summary view

--- consultation_analyser/consultations/views.py
+++ consultation_analyser/consultations/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpRequest
-from .models import Question
+from . import models
 
 
 def home(request: HttpRequest):
@@ -8,16 +8,13 @@ def home(request: HttpRequest):
 
 
 def show_question(request: HttpRequest, slug: str):
-    context = {"question": Question.objects.filter(slug=slug).first()}
+    context = {"question": models.Question.objects.filter(slug=slug).first()}
     return render(request, "show_question.html", context)
 
 
-def question_summary(request: HttpRequest, consultation_slug: str, question_slug: str):
-    # Question.objects.get(section__consultation__slug=consultation_slug, slug=question_slug)
-    question = Question.objects.filter(slug=question_slug)
-    question = question[0]
-    # TODO - get themes from DB
-
-    themes = {}
-    context = {"question": question, "themes": themes}
+def question_summary(request: HttpRequest, consultation_slug: str, section_slug:str, question_slug: str):
+    question = models.Question.objects.get(slug=question_slug, section__slug=section_slug, section__consultation__slug=consultation_slug)
+    answers = models.Answer.objects.filter(question=question)
+    # TODO - themes
+    context = {"question": question, "themes": []}
     return render(request, "question-summary.html", context)
