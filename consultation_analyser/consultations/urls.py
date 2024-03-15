c36dbbddca8c02e79f4740cd30344a25d554b491
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-03-15 22:49:04 +0000

    Tidy - remove commented out code, update names, follow naming conventions.

--- consultation_analyser/consultations/urls.py
+++ consultation_analyser/consultations/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.home),
     path(
-        "consultation/<str:consultation_slug>/section/<str:section_slug>/question/<str:question_slug>/",
+        "consultations/<str:consultation_slug>/sections/<str:section_slug>/questions/<str:question_slug>",
         views.show_question,
     ),
 ]
