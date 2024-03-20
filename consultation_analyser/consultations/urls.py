80efb00cfe127ef973c7d0e1da202fe9ce993f71
Duncan Brown <duncanjbrown@gmail.com>
2024-03-20 16:27:04 +0000

    Add question list to home page

--- consultation_analyser/consultations/urls.py
+++ consultation_analyser/consultations/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path(
         "consultations/<str:consultation_slug>/sections/<str:section_slug>/questions/<str:question_slug>",
         views.show_question,
+        name="show_question",
     ),
 ]
