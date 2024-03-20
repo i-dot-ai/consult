621b8b4c1b4dee919061ac02395c7ad78cea65dd
Duncan Brown <duncanjbrown@gmail.com>
2024-03-20 17:52:30 +0000

    Add test for home page

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -6,6 +6,10 @@ from tests.factories import ConsultationFactory
 
 @pytest.mark.django_db
 def test_get_question_summary_page(django_app):
-    ConsultationFactory(with_question=True)
-    resp = django_app.get("/")
-    assert resp.status_code == 200
+    consultation = ConsultationFactory(with_question=True)
+    question = consultation.section_set.first().question_set.first()
+    homepage = django_app.get("/")
+    question_page = homepage.click(f"{question.text}")
+
+    assert "Question summary" in question_page
+    assert f"{question.text}" in question_page
