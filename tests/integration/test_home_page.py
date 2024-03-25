b9de862aeb9cb8187b3be7b06ba446731e5242b2
Duncan Brown <duncanjbrown@gmail.com>
2024-03-25 10:24:58 +0000

    Add django-webtest

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -1,1 +1,11 @@
+import pytest
 
+
+from tests.factories import ConsultationFactory
+
+
+@pytest.mark.django_db
+def test_get_question_summary_page(django_app):
+    ConsultationFactory(with_question=True)
+    resp = django_app.get("/")
+    assert resp.status_code == 200
