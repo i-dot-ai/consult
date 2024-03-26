80a6aa35a7910db85ace1812fa35ea0f8b2281ee
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-03-26 16:10:24 +0000

    Add some dummy methods for generating themes.

---
+++ consultation_analyser/consultations/ml_pipeline.py
@@ -0,0 +1,29 @@
+"""
+For now, dummy code to test consultation flow.
+"""
+
+import random
+
+from . import models
+
+
+def dummy_generate_theme_for_question(question):
+    # This does not
+    answers_qs = models.Answer.objects.filter(question=question)
+    made_up_themes = []
+    for i in range(3):
+        label = f"Theme {i}: {question.slug}"
+        words = question.text.split()
+        keywords = [random.choice(words), random.choice(words)]
+        theme = models.Theme(keywords=keywords, label=label)
+        theme.save()
+        made_up_themes.append(theme)
+    for answer in answers_qs:
+        answer.theme = random.choice(made_up_themes)
+        answer.save()
+
+
+def dummy_save_themes_for_consultation(consultation_slug: str) -> None:
+    questions = models.Question.objects.filter(section__consultation__slug=consultation_slug, has_free_text=True)
+    for question in questions:
+        dummy_generate_theme_for_question(question)
