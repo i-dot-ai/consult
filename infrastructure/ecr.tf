da7cac960298526455a825145fcd2dc76abf6337
Guy Hannay <ghannay@no10.gov.uk>
2024-03-26 13:00:35 +0000

    tweak

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,7 +1,6 @@
 
 module "ecr" {
-  source = "../../i-ai-core-infrastructure//modules/ecr"
-
+  source                = "../../i-ai-core-infrastructure//modules/ecr"
   project_name          = var.project_name
   container_name_suffix = null
 }
