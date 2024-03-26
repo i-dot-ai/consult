74f9f36bba9f0e0bfa3db07089bee096835e3c97
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-03-26 13:24:14 +0000

    Skeleton code for homepage button.

--- consultation_analyser/consultations/views.py
+++ consultation_analyser/consultations/views.py
@@ -2,8 +2,21 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from . import models
 
+from tests.factories import ConsultationFactory, AnswerFactory
+
 
 def home(request: HttpRequest):
+    if request.POST:
+        # TODO - can't import from factories
+        dummy_consultation = ConsultationFactory(with_question=True)
+        dummy_question = models.Question.objects.filter(consultation=dummy_consultation).first()
+        dummy_question.has_free_text = True
+        dummy_question.save()
+        AnswerFactory(questions=dummy_question, theme=None)
+        consultation_slug = dummy_consultation.slug
+        # TODO - to be replaced by redirect
+        return render(request, "home.html", context)
+
     questions = models.Question.objects.all().order_by("id")[:10]
     context = {"questions": questions}
     return render(request, "home.html", context)
