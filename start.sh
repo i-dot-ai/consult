00c094be8d6277b36dce068d879b8f0ed11e6bb9
Duncan Brown <duncanjbrown@gmail.com>
2024-03-27 12:32:01 +0000

    Add exec to docker entrypoint script

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 venv/bin/django-admin migrate
 venv/bin/django-admin compress --force --engine jinja2
-venv/bin/gunicorn -c ./consultation_analyser/gunicorn.py consultation_analyser.wsgi
+exec venv/bin/gunicorn -c ./consultation_analyser/gunicorn.py consultation_analyser.wsgi
