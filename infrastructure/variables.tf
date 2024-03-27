c3c4e9ee8dfc59ce02364a0f2d4712e99b3b4b86
ghannay10 <76520424+ghannay10@users.noreply.github.com>
2024-03-27 13:05:32 +0000

    Fix/infrastructure conflict (#47)
    * Ensure we are pulling config from private directory
    
    * fix: infra conflicts
    
    * add: .dockerignore=
    
    * fixed: tf var conflicts
    
    ---------
    
    Co-authored-by: Toby Drane <tdrane@no10.gov.uk>
    Co-authored-by: mohit <MMotwani@no10.gov.uk>
    Co-authored-by: MotwaniM <73472421+MotwaniM@users.noreply.github.com>
    Co-authored-by: Duncan Brown <duncanjbrown@gmail.com>

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -1,10 +1,9 @@
-# locals {
-#   cloudwatch_log_group = "${var.project_name}-${terraform.workspace}-logs"
-# }
+variable "app-replica-count-desired" {
+  type        = number
+  default     = 1
+  description = "The desired number of replicas"
+}
 
-variable "project_name" {
-  type        = string
-  description = "Name of project"
 # variable "cloudwatch_log_group" {
 #   type        = string
 #   description = "CloudWatch log group name"
@@ -84,58 +83,21 @@ variable "image_tag" {
   description = "The tag of the image to use"
 }
 
-# variable "ecr_repository_uri" {
-#   type        = string
-#   description = "ECR repo uri"
-# }
-
-# variable "cpu" {
-#   type        = number
-#   default     = 512
-#   description = "The cpu resource to give to the task"
-# }
-
-# variable "memory" {
-#   type        = number
-#   description = "The memory resource to give to the task"
-#   default     = 1024
-# }
-
-variable "container_port" {
+variable "memory" {
   type        = number
-  description = "The port to run the application on"
-  default     = 80
+  description = "The memory resource to give to the task"
+  default     = 1024
 }
 
-# variable "environment_variables" {
-#   type        = map(any)
-#   default     = { "ENVIRONMENT" : "dev" }
-#   description = "A map of the environment variables to be passed to the ECS task"
-# }
-
-# variable "app-replica-count-desired" {
-#   type        = number
-#   default     = 1
-#   description = "The desired number of replicas"
-# }
-
-# variable "health_check" {
-#   type = object({
-#     accepted_response = string
-#     path              = string
-#     timeout           = number
-#   })
-#   default = {
-#     accepted_response = "200"
-#     path              = "/health"
-#     timeout           = 5
-#   }
-#   description = "Set the health check configuration for the target group"
-# }
-
-variable "state_bucket" {
+variable "prefix" {
   type        = string
-  description = "Name of the S3 bucket to use a terraform state"
+  description = "value to prefix resources with"
+  default     = "i-dot-ai"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Name of project"
 }
 
 variable "region" {
@@ -144,47 +106,7 @@ variable "region" {
   default     = "eu-west-2"
 }
 
-variable "env" {
+variable "state_bucket" {
   type        = string
-  description = "environment"
-  default     = "dev"
+  description = "Name of the S3 bucket to use a terraform state"
 }
-
-
-variable "cognito_usernames" {
-  type        = list(string)
-  description = "List of usernames to be added"
-}
-
-variable "domain_name" {
-  type        = string
-  description = "The base domain name for the project"
-}
-
-variable "developer_ips" {
-  type        = list(string)
-  description = "List of developer IPs"
-}
-
-variable "ecr_repository_uri" {
-  type        = string
-  description = "ECR repo uri"
-}
-
-variable "hosted_zone_id" {
-  type        = string
-  description = "Route 53 Hosted Zone"
-}
-
-
-
-# variable "prefix" {
-#   type        = string
-#   description = "value to prefix resources with"
-#   default     = "i-dot-ai"
-# }
-
-# variable "ecs_cluster_name" {
-#   type        = string
-#   description = "ECS cluster name to attach service to"
-# }
