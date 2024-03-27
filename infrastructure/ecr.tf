a21b4200bfb20c6fdffe9861cb8f87e9b065909f
ghannay10 <76520424+ghannay10@users.noreply.github.com>
2024-03-27 13:05:32 +0000

    Fix/infrastructure conflict (#47)
    * Ensure we are pulling config from private directory
    
    * fix: infra conflicts
    
    * add: .dockerignore=
    
    * fixed: tf var conflicts
    
    ---------
    
    Co-authored-by: Toby Drane <tdrane@no10.gov.uk>
    Co-authored-by: mohit <MMotwani@no10.gov.uk>
    Co-authored-by: MotwaniM <73472421+MotwaniM@users.noreply.github.com>
    Co-authored-by: Duncan Brown <duncanjbrown@gmail.com>

--- infrastructure/ecr.tf
+++ infrastructure/ecr.tf
@@ -1,17 +1,3 @@
-module "ecr_front_end" {
-  source = "../../i-ai-core-infrastructure//modules/ecr"
-
-  project_name_prefix = var.project_name
-  container_name      = "front-end"
-}
-
-module "ecr_pre_processing" {
-  source              = "../../i-ai-core-infrastructure//modules/ecr"
-
-  project_name_prefix = var.project_name
-  container_name      = "pre-processing"
-}
-
 module "ecr" {
   source                = "../../i-ai-core-infrastructure//modules/ecr"
   project_name          = var.project_name
