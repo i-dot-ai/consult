edbfef889a2bd6cc656bfb16836204454798a9f8
Kevin Etchells <kevetchells@hotmail.com>
2024-03-28 10:51:11 +0000

    Move question list to "/consultations"
    Make question links unique

--- consultation_analyser/consultations/urls.py
+++ consultation_analyser/consultations/urls.py
@@ -3,7 +3,12 @@ from django.urls import path
 from .views import questions, pages
 
 urlpatterns = [
+<<<<<<< HEAD
     path("", pages.home),
+=======
+    path("", views.home),
+    path("consultations", views.consultation),
+>>>>>>> 2a93322 (Move question list to "/consultations")
     path(
         "consultations/<str:consultation_slug>/sections/<str:section_slug>/questions/<str:question_slug>",
         questions.show,
