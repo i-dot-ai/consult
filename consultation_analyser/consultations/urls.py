ce3b6963f32b8c45c9d7bd1a5a529139252e05d9
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-02 10:58:43 +0100

    Tidy up URLs and move to new views structure.

--- consultation_analyser/consultations/urls.py
+++ consultation_analyser/consultations/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
 
-from .views import questions, pages
+from .views import questions, pages, consultations
 
 urlpatterns = [
-<<<<<<< HEAD
     path("", pages.home),
-=======
-    path("", views.home),
-    path("consultations", views.consultation),
->>>>>>> 2a93322 (Move question list to "/consultations")
+    path("consultations", consultations.show_questions),
     path(
         "consultations/<str:consultation_slug>/sections/<str:section_slug>/questions/<str:question_slug>",
         questions.show,
