7a5b713ec7ee64c97f8254027b1cd58bc3ec4a56
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-02 21:05:39 +0100

    Add another method to the hosting environment

--- consultation_analyser/hosting_environment.py
+++ consultation_analyser/hosting_environment.py
@@ -11,3 +11,11 @@ class HostingEnvironment:
     @staticmethod
     def is_test() -> bool:
         return env.str("ENVIRONMENT", "").upper() == "TEST"
+
+    @staticmethod
+    def is_deployed() -> bool:  # How do we check if AWS?
+        environment = env.str("ENVIRONMENT", "").upper()
+        deployed_envs = ["DEV", "DEVELOPMENT", "PREPROD", "PROD", "PRODUCTION"]
+        if environment in deployed_envs:
+            return True
+        return False
