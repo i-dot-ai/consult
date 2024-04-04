bec4c956bee3f50de47178f90c364f48119e4615
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-04 19:16:19 +0100

    Add draft view to create dummy data.

--- consultation_analyser/consultations/urls.py
+++ consultation_analyser/consultations/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 
+from consultation_analyser.hosting_environment import HostingEnvironment
+
 from .views import consultations, pages, questions, schema
+
+development_environments = HostingEnvironment.is_local() or HostingEnvironment.is_test() or HostingEnvironment.is_dev()
+
 
 urlpatterns = [
     path("", pages.home),
@@ -16,6 +21,13 @@ urlpatterns = [
         "consultations/<str:consultation_slug>/sections/<str:section_slug>/responses/<str:question_slug>",
         questions.show_responses,
     ),
-    path("batch-example", pages.batch_example, name="batch_example"),
     path("consultations/<str:consultation_slug>", consultations.show_consultation, name="show_consultation"),
 ]
+
+development_urlpatterns = [
+    path("consultation-example", consultations.create_dummy_data, name="consultation_example"),
+    path("batch-example", pages.batch_example, name="batch_example"),
+]
+
+if development_environments:
+    urlpatterns = urlpatterns + development_urlpatterns
--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -12,12 +12,19 @@ def show_questions(request: HttpRequest):
     return render(request, "consultation.html", context)
 
 
-# TODO placeholder view for testing
+# TODO placeholder view for testing - needs updating
 def show_consultation(request: HttpRequest, consultation_slug: str) -> HttpResponse:
     questions = models.Question.objects.filter(section__consultation__slug=consultation_slug)
     show_button = HostingEnvironment.is_dev() or HostingEnvironment.is_local() or HostingEnvironment.is_test()
     context = {"questions": questions, "show_button": show_button}
     if request.POST:
         pass
-        # TODO - run generate dummy data
+        # TODO - generate themes
     return render(request, "show_consultation.html", context)
+
+
+def create_dummy_data(request: HttpRequest) -> HttpResponse:
+    if request.POST:
+        pass
+        #
+    return render(request, "create_dummy_data.html")
