6dc4abfa96cf81c89a8d1d1ede822b95ec5b0f77
Duncan Brown <duncanjbrown@gmail.com>
2024-04-09 14:02:26 +0100

    Ensure .test.env takes precedence in test environments
    Before this change, I found that the contents of .test.env were being
    overridden by the contents of .env

--- consultation_analyser/settings/test.py
+++ consultation_analyser/settings/test.py
@@ -6,6 +6,6 @@ import environ
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # Take environment variables from .env file
-environ.Env.read_env(os.path.join(BASE_DIR, ".env.test"))
+environ.Env.read_env(os.path.join(BASE_DIR, ".env.test"), overwrite=True)
 
 from consultation_analyser.settings.base import *  # noqa
