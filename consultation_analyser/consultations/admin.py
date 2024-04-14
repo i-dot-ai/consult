4f27c41a8801c99a0f0ce47303fdb54d69e2b7c7
Duncan Brown <duncanjbrown@gmail.com>
2024-04-14 17:02:13 +0100

    Rename admin panel
    This is pretty untidy because in theory any admin.py in any app could
    override this effectively global setting, but it's ok for now

--- consultation_analyser/consultations/admin.py
+++ consultation_analyser/consultations/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from waffle.models import Flag, Sample
 
+admin.site.site_header = "Consultation analyser admin panel"
+
 # we aren't using these features of django-waffle yet
 admin.site.unregister(Flag)
 admin.site.unregister(Sample)
