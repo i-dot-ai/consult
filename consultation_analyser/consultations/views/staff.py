3a788d0448c9aaf5c7023decabb456242f546097
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-16 22:24:21 +0100

    Pass through messages for the testing dummy data page.

--- consultation_analyser/consultations/views/staff.py
+++ consultation_analyser/consultations/views/staff.py
@@ -1,5 +1,7 @@
 import uuid
 
+from django.contrib import messages
+from django.contrib.messages import get_messages
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 
@@ -7,16 +9,13 @@ from .. import dummy_data, ml_pipeline, models
 
 
 def create_dummy_data(request: HttpRequest) -> HttpResponse:
-    # TODO - sort out messages properly
-    message = ""
     if request.POST:
-        print(request.POST)
         if "generate_dummy_consultation" in request.POST:
             try:
                 dummy_data.DummyConsultation(include_themes=False)
-                message = "New consultation generated"
+                messages.add_message(request, messages.INFO, "New consultation generated")
             except RuntimeError as error:
-                message = error.args[0]
+                messages.add_message(request, messages.WARN, error.args[0])
         else:
             consultation_id = request.POST.get("consultation_id")
             themes_already_exist = models.Theme.objects.filter(
@@ -24,10 +23,11 @@ def create_dummy_data(request: HttpRequest) -> HttpResponse:
             ).exists()
             if not themes_already_exist:
                 ml_pipeline.save_themes_for_consultation(consultation_id=consultation_id)
-                message = "Themes created for consultation"
+                messages.add_message(request, messages.INFO, "Themes created for consultation")
             else:
-                message = "Themes already exist for consultation"
+                messages.add_message(request, messages.INFO, "Themes already exist for this consultation")
 
     consultations = models.Consultation.objects.all()
-    context = {"consultations": consultations, "message": message}
+    # Have to pass messages to template as we are using jinja not DTL
+    context = {"consultations": consultations, "messages": get_messages(request)}
     return render(request, "create_dummy_data.html", context=context)
