5ce9d0099d33c74a34b7a9a3d2d81ab24495d31d
Duncan Brown <duncanjbrown@gmail.com>
2024-04-16 17:56:44 +0100

    Make it possible to add users via the admin

--- consultation_analyser/authentication/admin.py
+++ consultation_analyser/authentication/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
+from .models import User
 
 # leave this out until we know what we’re doing with Groups etc
 admin.site.unregister(Group)
+admin.site.register(User)
