37e31c16d7eaf0aa06b91c17c4ea49dfd7acaabb
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-18 12:57:30 +0100

    Remove stray print statement.

--- tests/integration/test_home_page.py
+++ tests/integration/test_home_page.py
@@ -9,7 +9,5 @@ def test_nav_links(django_app):
     assert len(homepage.html.select(".govuk-header__navigation-item--active")) == 0
 
     schema_page = homepage.click("Data schema")
-    print(schema_page)
-
     assert len(schema_page.html.select(".govuk-header__navigation-item--active")) == 1
     assert "data schema" in schema_page
