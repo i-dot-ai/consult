0628c1522baa765db0c6f2452af88136ca6a8001
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-19 08:05:23 +0100

    more tests for support area

--- tests/integration/test_logging_in_to_support.py
+++ tests/integration/test_logging_in_to_support.py
@@ -1,6 +1,7 @@
 import pytest
 
-from consultation_analyser.consultations.models import Consultation
+from consultation_analyser.consultations.dummy_data import DummyConsultation
+from consultation_analyser.consultations.models import Consultation, Theme
 from consultation_analyser.factories import UserFactory
 
 
@@ -37,6 +38,25 @@ def test_generating_dummy_data(django_app):
 
     # Check dummy data button does generate a new consultation
     initial_count = Consultation.objects.all().count()
-    consultations_page.form.submit("generate_dummy_consultation")
+    consultations_page = consultations_page.form.submit("generate_dummy_consultation")
     count_after_dummy_data = Consultation.objects.all().count()
     assert count_after_dummy_data > initial_count
+
+    # Check redirected to individual consultation page in support
+    latest_consultation = Consultation.objects.all().order_by("created_at").last()
+    next_page = consultations_page.click(latest_consultation.name)
+    assert "Generate themes" in next_page
+
+
+@pytest.mark.django_db
+def test_generate_themes(django_app):
+    DummyConsultation(name="Test consultation", slug="test-consultation", responses=5, include_themes=False)
+    UserFactory(email="email@example.com", password="admin", is_staff=True)  # pragma: allowlist secret
+    login_page = django_app.get("/support/consultations/test-consultation/").follow()
+    login_page.form["username"] = "email@example.com"
+    login_page.form["password"] = "admin"  # pragma: allowlist secret
+    consultation_page = login_page.form.submit().follow()
+    assert "Test consultation" in consultation_page
+    consultation_page = consultation_page.form.submit("generate_themes")
+    generated_themes = Theme.objects.filter(answer__question__section__consultation__slug="test-consultation")
+    assert generated_themes.exists()
