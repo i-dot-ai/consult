6a824858292bb04420d31c39bc15c5bbd94f56bc
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-23 15:03:45 +0100

    Update name of django debug secret.

--- infrastructure/secrets.tf
+++ infrastructure/secrets.tf
@@ -16,19 +16,19 @@ data "aws_secretsmanager_secret_version" "django_secret" {
   secret_id = data.aws_secretsmanager_secret.django_secret.id
 }
 
-resource "aws_secretsmanager_secret" "django_debug" {
-  name        = "${var.prefix}-${var.project_name}-${var.env}-django-debug"
+resource "aws_secretsmanager_secret" "debug" {
+  name        = "${var.prefix}-${var.project_name}-${var.env}-debug"
   description = "Django debug value for ${var.project_name}"
   tags = {
     SecretPurpose = "general" # pragma: allowlist secret
   }
 }
 
-data "aws_secretsmanager_secret" "django_debug" {
-  arn = aws_secretsmanager_secret.django_debug.arn
+data "aws_secretsmanager_secret" "debug" {
+  arn = aws_secretsmanager_secret.debug.arn
 }
 
 
-data "aws_secretsmanager_secret_version" "django_debug" {
-  secret_id = data.aws_secretsmanager_secret.django_debug.id
+data "aws_secretsmanager_secret_version" "debug" {
+  secret_id = data.aws_secretsmanager_secret.debug.id
 }
