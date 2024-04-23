7d60cf822c49688bcd1aba98abc1c65861a7d9b3
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-23 20:06:28 +0100

    Test activate virtual env.

--- consultation_analyser/consultations/views/pages.py
+++ consultation_analyser/consultations/views/pages.py
@@ -34,9 +34,9 @@ def get_involved(request: HttpRequest):
 def batch_example(request: HttpRequest):
     message = ""
     if request.POST:
-        # TODO - run management command
         job_name = "batch_example"
-        command = {"command": ["python", "manage.py", "basic_management_command"]}
+        # command = {"command": ["python", "manage.py", "basic_management_command"]}
+        command = {"command": ["venv/bin", "python", "manage.py", "basic_management_command"]}
         if not HostingEnvironment.is_local():
             batch_handler = BatchJobHandler()
             batch_handler.submit_job_batch(jobName=job_name, containerOverrides=command)
