7da4965a8661a47ba2344feca5ff6f241e90a40d
Guy Hannay <ghannay@no10.gov.uk>
2024-04-24 20:03:00 +0100

    add: generic variable secret

--- infrastructure/secrets.tf
+++ infrastructure/secrets.tf
@@ -11,7 +11,6 @@ data "aws_secretsmanager_secret" "django_secret" {
   arn = aws_secretsmanager_secret.django_secret.arn
 }
 
-
 data "aws_secretsmanager_secret_version" "django_secret" {
   secret_id = data.aws_secretsmanager_secret.django_secret.id
 }
@@ -29,7 +28,23 @@ data "aws_secretsmanager_secret" "app_secrets" {
   arn = aws_secretsmanager_secret.app_secrets.arn
 }
 
-
 data "aws_secretsmanager_secret_version" "app_secrets" {
   secret_id = data.aws_secretsmanager_secret.app_secrets.id
+}
+
+
+resource "aws_secretsmanager_secret" "env_vars" {
+  name        = "${var.prefix}-${var.project_name}-${var.env}-environement-variables"
+  description = "Environment variables for ${var.project_name} in ${var.env} ECS. These are non-sensitive variables."
+  tags = {
+    SecretPurpose = "general"
+  }
+}
+
+data "aws_secretsmanager_secret" "env_vars" {
+  arn = aws_secretsmanager_secret.env_vars.arn
+}
+
+data "aws_secretsmanager_secret_version" "env_vars" {
+  secret_id = data.aws_secretsmanager_secret.env_vars.id
 }
