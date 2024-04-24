c83b1dbb6ea09399e6c68351b98ef97188019d81
Nina Menezes <77671865+nmenezes0@users.noreply.github.com>
2024-04-24 19:49:35 +0100

    Add debug variable. (#137)
    * Add debug variable.
    
    * Update description.
    
    * Update name of django debug secret.
    
    * Update typo
    
    * add batch job execution permission (#139)
    
    * add batch job execution permission
    
    * refine permissions
    
    ---------
    
    Co-authored-by: MotwaniM <73472421+MotwaniM@users.noreply.github.com>

--- infrastructure/secrets.tf
+++ infrastructure/secrets.tf
@@ -15,3 +15,20 @@ data "aws_secretsmanager_secret" "django_secret" {
 data "aws_secretsmanager_secret_version" "django_secret" {
   secret_id = data.aws_secretsmanager_secret.django_secret.id
 }
+
+resource "aws_secretsmanager_secret" "debug" {
+  name        = "${var.prefix}-${var.project_name}-${var.env}-debug"
+  description = "Django debug value for ${var.project_name}"
+  tags = {
+    SecretPurpose = "general" # pragma: allowlist secret
+  }
+}
+
+data "aws_secretsmanager_secret" "debug" {
+  arn = aws_secretsmanager_secret.debug.arn
+}
+
+
+data "aws_secretsmanager_secret_version" "debug" {
+  secret_id = data.aws_secretsmanager_secret.debug.id
+}
