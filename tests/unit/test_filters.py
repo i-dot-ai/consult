58b3785af63077b39bbf90633d36ea7b805f6bfc
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-25 15:18:49 +0100

    Refactoring tests.

--- tests/unit/test_filters.py
+++ tests/unit/test_filters.py
@@ -4,6 +4,20 @@ from consultation_analyser import factories
 from consultation_analyser.consultations.views import filters
 
 
+def set_up_for_filters():
+    question = factories.QuestionFactory()
+    theme1 = factories.ThemeFactory(label="1_dog_puppy")
+    theme2 = factories.ThemeFactory(label="2_cat_kitten")
+    factories.AnswerFactory(
+        theme=theme1, question=question, free_text="We love dogs.", multiple_choice_responses=["Option 1", "Option 2"]
+    )
+    factories.AnswerFactory(
+        theme=theme2, question=question, free_text="We like cats not dogs.", multiple_choice_responses=["Option 1"]
+    )
+    factories.AnswerFactory(theme=theme2, question=question, free_text="We love cats.")
+    return question, theme1
+
+
 @pytest.mark.parametrize(
     "applied_filters,expected_count",
     [
@@ -15,31 +29,14 @@ from consultation_analyser.consultations.views import filters
 )
 @pytest.mark.django_db
 def test_get_filtered_responses(applied_filters, expected_count):
-    question = factories.QuestionFactory()
-    theme1 = factories.ThemeFactory(label="1_dog_puppy")
-    theme2 = factories.ThemeFactory(label="2_cat_kitten")
-    factories.AnswerFactory(
-        theme=theme1, question=question, free_text="We love dogs.", multiple_choice_responses=["Option 1", "Option 2"]
-    )
-    factories.AnswerFactory(
-        theme=theme2, question=question, free_text="We like cats not dogs.", multiple_choice_responses=["Option 1"]
-    )
-    factories.AnswerFactory(theme=theme2, question=question, free_text="We love cats.")
+    question, _ = set_up_for_filters()
     queryset = filters.get_filtered_responses(question, applied_filters=applied_filters)
     assert queryset.count() == expected_count
 
 
 @pytest.mark.django_db
 def test_get_filtered_responses_themes():
-    question = factories.QuestionFactory()
-    theme1 = factories.ThemeFactory(label="1_dog_puppy")
-    theme2 = factories.ThemeFactory(label="2_cat_kitten")
-    factories.AnswerFactory(
-        theme=theme1, question=question, free_text="We love dogs", multiple_choice_responses=["Option 1", "Option 2"]
-    )
-    factories.AnswerFactory(
-        theme=theme2, question=question, free_text="We like cats not dogs.", multiple_choice_responses=["Option 1"]
-    )
+    question, theme1 = set_up_for_filters()
     applied_filters = {"keyword": "", "theme": theme1.id, "opinion": "All"}
     queryset = filters.get_filtered_responses(question, applied_filters=applied_filters)
     assert queryset.count() == 1
