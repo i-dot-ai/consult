40cf58e9915f38c72a356203779a74204205ed87
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-04-25 14:32:05 +0100

    Add tests for filtering.

---
+++ tests/unit/test_filters.py
@@ -0,0 +1,45 @@
+import pytest
+
+from consultation_analyser import factories
+from consultation_analyser.consultations.views import filters
+
+
+@pytest.mark.parametrize(
+    "applied_filters,expected_count",
+    [
+        ({"theme": "All", "keyword": "", "opinion": "All"}, 3),
+        ({"keyword": "dogs", "theme": "All", "opinion": "All"}, 2),
+        ({"keyword": "dogs", "theme": "All", "opinion": "Option 2"}, 1),
+        ({"keyword": "", "theme": "All", "opinion": "Option 1"}, 2),
+    ],
+)
+@pytest.mark.django_db
+def test_get_filtered_responses(applied_filters, expected_count):
+    question = factories.QuestionFactory()
+    theme1 = factories.ThemeFactory(label="1_dog_puppy")
+    theme2 = factories.ThemeFactory(label="2_cat_kitten")
+    factories.AnswerFactory(
+        theme=theme1, question=question, free_text="We love dogs.", multiple_choice_responses=["Option 1", "Option 2"]
+    )
+    factories.AnswerFactory(
+        theme=theme2, question=question, free_text="We like cats not dogs.", multiple_choice_responses=["Option 1"]
+    )
+    factories.AnswerFactory(theme=theme2, question=question, free_text="We love cats.")
+    queryset = filters.get_filtered_responses(question, applied_filters=applied_filters)
+    assert queryset.count() == expected_count
+
+
+@pytest.mark.django_db
+def test_get_filtered_responses_themes():
+    question = factories.QuestionFactory()
+    theme1 = factories.ThemeFactory(label="1_dog_puppy")
+    theme2 = factories.ThemeFactory(label="2_cat_kitten")
+    factories.AnswerFactory(
+        theme=theme1, question=question, free_text="We love dogs", multiple_choice_responses=["Option 1", "Option 2"]
+    )
+    factories.AnswerFactory(
+        theme=theme2, question=question, free_text="We like cats not dogs.", multiple_choice_responses=["Option 1"]
+    )
+    applied_filters = {"keyword": "", "theme": theme1.id, "opinion": "All"}
+    queryset = filters.get_filtered_responses(question, applied_filters=applied_filters)
+    assert queryset.count() == 1
