45a7993c23eb46dd9048f6b1d8b715d6c3604886
Duncan Brown <duncanjbrown@gmail.com>
2024-04-25 10:57:39 +0100

    Send magic link over email instead of printing on page

--- consultation_analyser/consultations/views/sessions.py
+++ consultation_analyser/consultations/views/sessions.py
@@ -1,12 +1,13 @@
 from django.contrib import messages
 from django.contrib.auth import logout
 from django.http import HttpRequest
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from magic_link.models import MagicLink
 from waffle.decorators import waffle_switch
 
 from consultation_analyser.authentication.models import User
 from consultation_analyser.consultations.forms.sessions import NewSessionForm
+from consultation_analyser.email import send_magic_link_email
 
 
 def get_magic_link_for_email(request: HttpRequest, email: str) -> str:
@@ -26,8 +27,9 @@ def new(request: HttpRequest):
         form = NewSessionForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data["email"]
-            url = get_magic_link_for_email(request, email)
-            return render(request, "magic_link/link_sent.html", {"link": url})
+            magic_link = get_magic_link_for_email(request, email)
+            send_magic_link_email(email, magic_link)
+            return render(request, "magic_link/link_sent.html")
 
     return render(request, "new_session.html", {"form": form})
 
