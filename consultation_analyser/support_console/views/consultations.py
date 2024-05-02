73cec7508325d20eab2bab740d56c8afe59fcbe7
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-05-02 11:13:04 +0100

    We don't care about if themes have already been generated.

--- consultation_analyser/support_console/views/consultations.py
+++ consultation_analyser/support_console/views/consultations.py
@@ -25,14 +25,7 @@ def index(request: HttpRequest) -> HttpResponse:
 def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
     consultation = models.Consultation.objects.get(slug=consultation_slug)
     if request.POST:
-        # TODO - run it just once for now
-        themes_already_exist = models.Theme.objects.filter(
-            answer__question__section__consultation=consultation
-        ).exists()
-        if not themes_already_exist:
-            run_processing_pipeline(consultation)
-            messages.success(request, "Themes have been generated for this consultation")
-        else:
-            messages.info(request, "This consultation already has themes")
+        run_processing_pipeline(consultation)
+        messages.success(request, "Themes have been generated for this consultation")
     context = {"consultation": consultation}
     return render(request, "support_console/consultation.html", context=context)
