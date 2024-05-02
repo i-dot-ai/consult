f03fbbb108a6b719431261fbea9a4202e34fe7f8
Duncan Brown <duncanjbrown@gmail.com>
2024-05-02 14:15:17 +0100

    Add staff_member_required to all support/users pages

--- consultation_analyser/support_console/views/users.py
+++ consultation_analyser/support_console/views/users.py
@@ -1,17 +1,20 @@
 from django.contrib import messages
 from django.http import HttpRequest
 from django.shortcuts import redirect, render, get_object_or_404
+from django.contrib.admin.views.decorators import staff_member_required
 
 from consultation_analyser.authentication.models import User
 
 from ..forms.new_user_form import NewUserForm
 
 
+@staff_member_required
 def index(request: HttpRequest):
     users = User.objects.all().order_by("-created_at")
     return render(request, "support_console/all-users.html", {"users": users})
 
 
+@staff_member_required
 def new(request: HttpRequest):
     if not request.POST:
         form = NewUserForm()
@@ -26,9 +29,8 @@ def new(request: HttpRequest):
     return render(request, "support_console/new-user.html", {"form": form})
 
 
-
+@staff_member_required
 def show(request: HttpRequest, user_id: int):
     user = get_object_or_404(User, pk=user_id)
     consultations = user.consultation_set.all()
     return render(request, "support_console/user.html", {"user": user, "consultations": consultations})
-
