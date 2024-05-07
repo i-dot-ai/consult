62cf980da627ef53c40fc88872c350d3af7feda6
Duncan Brown <duncanjbrown@gmail.com>
2024-05-07 16:17:57 +0100

    Add view for uploading consultations
    Currently redirects on submission. Next commit will add the upload routine itself

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -1,8 +1,10 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 
 from .. import models
+from ..forms.consultation_upload_form import ConsultationUploadForm
 from .decorators import user_can_see_consultation
 
 
@@ -19,3 +21,15 @@ def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
     questions = models.Question.objects.filter(section__consultation__slug=consultation_slug)
     context = {"questions": questions}
     return render(request, "consultation.html", context)
+
+
+@login_required
+def new(request: HttpRequest):
+    if not request.POST:
+        form = ConsultationUploadForm()
+    else:
+        form = ConsultationUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            return redirect(f"/consultations/{consultation.slug}/")
+
+    return render(request, "new-consultation.html", {"form": form})
