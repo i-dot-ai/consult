f35251ec593c01773b4f21a390c9f5d9a0989f29
Duncan Brown <duncanjbrown@gmail.com>
2024-05-14 22:57:21 +0100

    Dedupe tests
    The dummy_data UI behaviour is now covered in
    user_manages_consultations_via_support, and the check for outliers is
    bundled into the upper test as generate_dummy_data is quite a slow
    operation
    
    Now that we have two outside-in tests for generate dummy data and the
    unit test only made some count assertions that those didn't, delete
    the unit test and move the counts into the test for the management
    command

--- tests/integration/test_support_console_pages.py
+++ tests/integration/test_support_console_pages.py
@@ -27,28 +27,6 @@ def test_logging_in_to_support(django_app):
 
 
 @pytest.mark.django_db
-def test_generating_dummy_data(django_app):
-    page_url = "/support/consultations/"
-    UserFactory(email="email@example.com", password="admin", is_staff=True)  # pragma: allowlist secret
-    login_page = django_app.get(page_url).follow()
-    login_page.form["username"] = "email@example.com"
-    login_page.form["password"] = "admin"  # pragma: allowlist secret
-    consultations_page = login_page.form.submit().follow()
-    assert "Consultations" in consultations_page
-
-    # Check dummy data button does generate a new consultation
-    initial_count = Consultation.objects.all().count()
-    consultations_page = consultations_page.form.submit("generate_dummy_consultation")
-    count_after_dummy_data = Consultation.objects.all().count()
-    assert count_after_dummy_data > initial_count
-
-    # Check redirected to individual consultation page in support
-    latest_consultation = Consultation.objects.all().order_by("created_at").last()
-    next_page = consultations_page.click(latest_consultation.name)
-    assert "Generate themes" in next_page
-
-
-@pytest.mark.django_db
 def test_generate_themes(django_app):
     create_dummy_data(name="Test consultation", slug="test-consultation", include_themes=False)
     UserFactory(email="email@example.com", password="admin", is_staff=True)  # pragma: allowlist secret
