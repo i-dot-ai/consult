f33307d52d18cce6fe15e61f205d485fcb1a600e
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-05-17 10:01:43 +0100

    Remove unused import.

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -1,4 +1,3 @@
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
