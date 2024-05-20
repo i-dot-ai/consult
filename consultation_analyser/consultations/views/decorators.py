cb53ff4a258eceb71234d93809435ab3260a4836
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-05-20 16:45:50 +0100

    Display a message if themes have not been generated.

--- consultation_analyser/consultations/views/decorators.py
+++ consultation_analyser/consultations/views/decorators.py
@@ -1,19 +1,19 @@
+from django.contrib import messages
 from django.shortcuts import get_object_or_404, redirect
 
 from .. import models
 
 
 def user_can_see_consultation(view_function):
-    def decorator(*args, **kwargs):
-        request = args[0]
+    def decorator(request, *args, **kwargs):
         slug = kwargs.get("consultation_slug")
 
         # will kick out users by throwing a 404 if they don't own the consultation
         consultation = get_object_or_404(models.Consultation.objects.filter(slug=slug, users__in=[request.user]))
 
         if not consultation.has_themes():
-            return redirect("/consultations/processing/")
+            messages.info(request, "We are processing your consultation. Themes have not been generated yet.")
 
-        return view_function(*args, **kwargs)
+        return view_function(request, *args, **kwargs)
 
     return decorator
