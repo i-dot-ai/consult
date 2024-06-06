2ca1384c2ed81d2b3a0203b23aa2cd2af9535e7f
Duncan Brown <duncanjbrown@gmail.com>
2024-06-06 13:29:10 +0100

    Ensure answer data present when attempting to summarise

--- tests/integration/test_llm_summariser.py
+++ tests/integration/test_llm_summariser.py
@@ -1,21 +1,23 @@
 import pytest
 
 from consultation_analyser import factories
-from consultation_analyser.consultations.models import Theme
 from consultation_analyser.pipeline.llm_summariser import create_llm_summaries_for_consultation
 
 
 @pytest.mark.django_db
 def test_create_llm_summaries_for_consultation():
     consultation = factories.ConsultationFactory(name="My new consultation")
+    response = factories.ConsultationResponseFactory(consultation=consultation)
     section = factories.SectionFactory(consultation=consultation)
     question = factories.QuestionFactory(section=section, has_free_text=True)
     theme = factories.ThemeFactory(question=question, short_description="", summary="")
-    theme_id = theme.id
-    # Check that we create some value for theme summary and description
+    factories.AnswerFactory(theme=theme, question=question, consultation_response=response)
+
     assert not theme.summary
     assert not theme.short_description
+
     create_llm_summaries_for_consultation(consultation)
-    theme = Theme.objects.get(id=theme_id)
+
+    theme.refresh_from_db()
     assert theme.summary
     assert theme.short_description
