53c521cf457bb9bbf1a3247e6927b6dcc609aa9c
mohit <MMotwani@no10.gov.uk>
2024-06-12 15:14:08 +0100

    check split rds for consultations

--- infrastructure/postgres/main.tf
+++ infrastructure/postgres/main.tf
@@ -7,13 +7,15 @@ module "postgres" {
   name                = local.name
   db_name             = "postgres"
   db_instance_address = data.terraform_remote_state.consultations.outputs.db_instance_address
-  db_master_username  = data.terraform_remote_state.consultations.outputs.db_instance_address
-  db_master_password  = data.terraform_remote_state.consultations.outputs.db_instance_address
+  db_master_username  = data.terraform_remote_state.consultations.outputs.db_master_username
+  db_master_password  = data.terraform_remote_state.consultations.outputs.db_master_password
 
 }
 
+
 data "terraform_remote_state" "platform" {
-  backend = "s3"
+  backend   = "s3"
+  workspace = terraform.workspace
   config = {
     bucket = var.state_bucket
     key    = "platform/terraform.tfstate"
@@ -22,10 +24,40 @@ data "terraform_remote_state" "platform" {
 }
 
 data "terraform_remote_state" "consultations" {
-  backend = "s3"
+  backend   = "s3"
+  workspace = terraform.workspace
   config = {
     bucket = var.state_bucket
     key    = "consultation-analyser/terraform.tfstate"
     region = var.region
   }
+}
+
+provider "aws" {
+  default_tags {
+    tags = {
+      Environment = terraform.workspace
+      Deployed    = "github"
+    }
+  }
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.53.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.6.2"
+    }
+  }
+  required_version = ">= 1.3.5"
+
+  backend "s3" {
+    key = "consultation-analyser-postgres/terraform.tfstate"
+  }
+
 }
