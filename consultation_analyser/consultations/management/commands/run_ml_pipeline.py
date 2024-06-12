12461c48c4be94349c31476cbc34de8c087c5e8f
Ben Rowell <ben.rowell@cabinetoffice.gov.uk>
2024-06-12 17:12:50 +0100

    Adds in SageMaker backend for LLM summarisation when deployed.

--- consultation_analyser/consultations/management/commands/run_ml_pipeline.py
+++ consultation_analyser/consultations/management/commands/run_ml_pipeline.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand
 
 from consultation_analyser.consultations import models
 from consultation_analyser.pipeline.processing import process_consultation_themes
-
+from consultation_analyser.pipeline.backends.sagemaker_llm_backend import SagemakerLLMBackend
 
 class Command(BaseCommand):
     help = "Run the machine learning pipeline to generate themes for the consultation"
@@ -16,7 +16,7 @@ class Command(BaseCommand):
         if options["consultation_slug"]:
             try:
                 consultation = models.Consultation.objects.get(slug=options["consultation_slug"])
-                process_consultation_themes(consultation)
+                process_consultation_themes(consultation, llm_backend=SagemakerLLMBackend())
                 self.stdout.write(
                     f"Theme generating pipeline has been run for consultation with name {consultation.name}"
                 )
