a4171347434a322e14b568046fd659c26219ea61
Duncan Brown <duncanjbrown@gmail.com>
2024-06-19 14:17:43 +0100

    Pass execution context to sentry

--- consultation_analyser/settings/production.py
+++ consultation_analyser/settings/production.py
@@ -1,12 +1,15 @@
 import sentry_sdk
 
-from consultation_analyser.settings.base import *  # noqa
+from consultation_analyser.settings.base import *, env # noqa
 
-CSRF_TRUSTED_ORIGINS = ["https://" + env("DOMAIN_NAME")]  # noqa: F405
+CSRF_TRUSTED_ORIGINS = ["https://" + env("DOMAIN_NAME")]
 
-SENTRY_DSN = env("SENTRY_DSN")  # noqa: F405
+SENTRY_DSN = env("SENTRY_DSN")
+EXECUTION_CONTENT = env("EXECUTION_CONTENT")
 
 sentry_sdk.init(
     dsn=SENTRY_DSN,
-    environment=ENVIRONMENT,  # noqa: F405
+    environment=ENVIRONMENT,
 )
+
+sentry_sdk.set_tags({"execution_context": EXECUTION_CONTENT})
