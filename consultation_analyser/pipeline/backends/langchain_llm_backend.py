38d25d3b53ea6f53dfd9c634425c9c9f9a5f320b
Duncan Brown <duncanjbrown@gmail.com>
2024-06-20 09:08:14 +0100

    Try and fix prompt

--- consultation_analyser/pipeline/backends/langchain_llm_backend.py
+++ consultation_analyser/pipeline/backends/langchain_llm_backend.py
@@ -69,18 +69,21 @@ class LangchainLLMBackend(LLMBackend):
         You are serving as an expert AI assisting UK government \
         policy officers in analyzing public opinions on new policies. \
         The topic of the new policy is {consultation_name}. \
-        We want you to distill key sentiments and arguments expressed in the responses. \
-        We conducted a survey, and responses to a specific question have been categorized under a single common theme.\
-        We have provided a sample of responses for the theme and frequently occurring key words. \
+        We conducted a survey, and answers to a specific question have been categorized under a single common theme.\
+        We have provided a sample of answers for the theme and frequently occurring key words. \
+        We want you to distill key sentiments and arguments expressed in the answers. \
         We have also provided some background information about the chapter of the survey in which the question was asked. \
         Instead of general agreement/disagreement, focus on capturing specific perspectives. \
-        Your task is to generate ONLY:
-        1) A concise phrase that encapsulates both the prevalent keywords and the nuanced opinions conveyed in the responses.
-        2) A summary of the opinions expressed in the responses.
-        You should generate a short description (a phrase) and summary that reflects the MOST COMMON opinion expressed in the responses. \
+        You should generate a short description (a phrase) and summary that reflects the MOST COMMON opinion expressed
+        in the answers. \
+        1) 'short_description': A concise phrase that encapsulates both the prevalent keywords and the nuanced opinions
+        conveyed in the answers. \
+        2) 'summary': A summary of the opinions expressed in the answers. \
+        Ensure that the short_description does not include any of the following phrases or their equivalents: "Support for ...", "Agreement with the policy", "Disagreement with the policy", "Policies", "Opinions on ...", "Agreement with proposed policy", "Disagreement with proposed policy".
+
         You MUST return your answer in JSON format. DO NOT DEVIATE FROM THIS FORMAT. \
-        The response MUST be formatted with two fields: 'short_description' and 'summary'. \
-        IT IS ESSENTIAL THAT YOU FORMAT YOUR RESPONSE AS JSON. INCLUDE NO OTHER MATERIAL.
+        The response should include two fields: 'short_description' and 'summary'. \
+        RETURN ONLY JSON.
 
         == QUESTION ==
         {question}
@@ -90,11 +93,18 @@ class LangchainLLMBackend(LLMBackend):
         {keywords}
         == KEYWORDS END ==
 
-        == SAMPLE RESPONSES ==
+        == SAMPLE ANSWERS ==
         {responses}
-        == SAMPLE RESPONSES END ==
+        == SAMPLE ANSWERS END ==
 
-        Ensure that the phrase you generate does not include any of the following phrases or their equivalents: "Support for ...", "Agreement with the policy", "Disagreement with the policy", "Policies", "Opinions on ...", "Agreement with proposed policy", "Disagreement with proposed policy".
+        Output should be structured as follows:
+
+        {{
+            "short_description": '[the short description you have generated]',
+            "summary": '[the summary you have generated]',
+        }}
+
+        IT IS ESSENTIAL THAT YOU FORMAT YOUR RESPONSE AS JSON. INCLUDE NO OTHER MATERIAL.
         """
 
         return PromptTemplate.from_template(template=prompt_template)
