81c23b679bcdd84f201a05a63e1b45549e70a72d
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-06-21 14:06:55 +0100

    Add some helper functions.

---
+++ consultation_analyser/consultations/utils.py
@@ -0,0 +1,11 @@
+from django.db.models import QuerySet
+
+
+def remove_empty_free_text_responses(answers_qs: QuerySet):
+	qs = answers_qs.exclude(free_text="").exclude(free_text__isnull=True)
+	return qs
+
+
+def remove_responses_with_outliers(answers_qs: QuerySet):
+	qs = answers_qs.exclude(theme__is_outlier=True)
+	return qs
