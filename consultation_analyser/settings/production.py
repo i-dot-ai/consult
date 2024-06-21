89f30e11d94eec8d3ab55eed85acdd0c6a16343a
Benjamin Rowell <ben.rowell@cabinetoffice.gov.uk>
2024-06-21 08:29:29 +0100

    Fixes EXECUTION_CONTEXT typo
    References to env var EXECUTION_CONTEXT were mislabelled EXECUTION_CONTENT.

--- consultation_analyser/settings/production.py
+++ consultation_analyser/settings/production.py
@@ -5,7 +5,7 @@ from consultation_analyser.settings import base
 CSRF_TRUSTED_ORIGINS = ["https://" + base.env("DOMAIN_NAME")]
 
 SENTRY_DSN = base.env("SENTRY_DSN")
-EXECUTION_CONTENT = base.env("EXECUTION_CONTENT")
+EXECUTION_CONTEXT = base.env("EXECUTION_CONTEXT")
 GIT_SHA = base.env("GIT_SHA")
 
 sentry_sdk.init(
@@ -14,4 +14,4 @@ sentry_sdk.init(
     release=GIT_SHA,
 )
 
-sentry_sdk.set_tags({"execution_context": EXECUTION_CONTENT})
+sentry_sdk.set_tags({"execution_context": EXECUTION_CONTEXT})
