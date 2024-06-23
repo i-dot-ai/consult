98093a88c7330c52662e515c03892570af25b9a2
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-06-23 17:30:17 +0100

    Factor out into separate function.

--- consultation_analyser/pipeline/llm_summariser.py
+++ consultation_analyser/pipeline/llm_summariser.py
@@ -6,6 +6,32 @@ from consultation_analyser.pipeline.backends.llm_backend import LLMBackend
 logger = logging.getLogger("pipeline")
 
 
+
+def create_themes_for_answers_no_free_text(consultation):
+    """
+    If there are answers with empty free text (for questions that have free text) -
+    create a special case theme to group them.
+    """
+    free_text_questions = Question.objects.filter(section__consultation=consultation).filter(
+        has_free_text=True
+    )
+    for question in free_text_questions:
+        empty_answers = Answer.objects.filter(question=question).filter(free_text="")
+        for answer in empty_answers:
+            theme, _ = Theme.objects.get_or_create(
+                question=question,
+                topic_id=None,
+                short_description="No free text response",
+                summary="The respondent did not answer the free text part of this question.",
+            )
+            logger.info(
+                f"There are answers with no free text responses for question: {theme.question.text}"
+            )
+            answer.theme = theme
+            answer.save()
+
+
+
 def create_llm_summaries_for_consultation(consultation, llm_backend: LLMBackend):
     logger.info(
         f"Starting LLM summarisation for consultation: {consultation.name} with backend {llm_backend.__class__.__name__}"
@@ -26,23 +52,5 @@ def create_llm_summaries_for_consultation(consultation, llm_backend: LLMBackend)
             theme.short_description = theme_summary_data.short_description
             logger.info(f"Theme description: {theme.short_description}")
         theme.save()
-
-    # If there are answers with empty free text - create a special case theme to group them
-    free_text_questions = Question.objects.filter(section__consultation=consultation).filter(
-        has_free_text=True
-    )
-    for question in free_text_questions:
-        empty_answers = Answer.objects.filter(question=question).filter(free_text="")
-        for answer in empty_answers:
-            theme, _ = Theme.objects.get_or_create(
-                question=question,
-                topic_id=None,
-                short_description="No free text response",
-                summary="The respondent did not answer the free text part of this question.",
-            )
-            logger.info(
-                f"There are answers with no free text responses for question: {theme.question.text}"
-            )
-            answer.theme = theme
-            answer.save()
+    create_themes_for_answers_no_free_text(consultation)
     logger.info("Ending LLM summarisation")
