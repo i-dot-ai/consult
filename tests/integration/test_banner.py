a714c259b42f3951d2f0f5607eee42a4638d591e
Duncan Brown <duncanjbrown@gmail.com>
2024-06-24 12:14:48 +0100

    Show warning banner when logged in and viewing consultations

---
+++ tests/integration/test_banner.py
@@ -0,0 +1,17 @@
+import pytest
+
+from consultation_analyser.factories import UserFactory
+from tests.helpers import sign_in
+
+
+@pytest.mark.django_db
+def test_user_can_sign_in(django_app):
+    UserFactory(email="email@example.com")
+
+    logged_out_homepage = django_app.get("/")
+    assert "pilot stage" not in logged_out_homepage
+
+    consultations_index = sign_in(django_app, "email@example.com")
+    print(consultations_index)
+
+    assert "pilot stage" in consultations_index
