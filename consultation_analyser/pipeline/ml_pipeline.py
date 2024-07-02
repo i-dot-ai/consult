3eee4f7a7ef741a810aaf24bc273c0eede3ec85b
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-07-02 23:10:57 +0100

    Save scatter plot data to topic model.

--- consultation_analyser/pipeline/ml_pipeline.py
+++ consultation_analyser/pipeline/ml_pipeline.py
@@ -1,4 +1,7 @@
 import logging
+import json
+
+from django.core.serializers.json import DjangoJSONEncoder
 
 from consultation_analyser.consultations import models
 
@@ -10,11 +13,12 @@ logger = logging.getLogger("pipeline")
 
 def topic_assignment_to_dict(assignment: TopicAssignment) -> dict:
     output = {}
-    output["answer_id"] = assignment.answer.id
+    output["answer_id"] = str(assignment.answer.id)
     output["answer_free_text"] = assignment.answer.free_text
-    output["topic_id"] = assignment.topic_id
-    output["x_coordinate"] = assignment.x_coordinate
-    output["y_coordinate"] = assignment.y_coordinate
+    output["topic_id"] = assignment.topic_id # noqa (TODO - sort out mypy)
+    output["x_coordinate"] = assignment.x_coordinate # noqa (TODO - sort out mypy)
+    output["y_coordinate"] = assignment.y_coordinate # noqa (TODO - sort out mypy)
+    # TODO - what else do we want here?
     return output
 
 
@@ -22,10 +26,14 @@ def save_themes_for_question(
     question: models.Question, topic_backend: TopicBackend, processing_run: models.ProcessingRun
 ) -> None:
     logging.info(f"Get topics for question: {question.text}")
-    topic_model_metadata = models.TopicModelMetadata()
+
+    assignments = topic_backend.get_topics(question)
+    scatter_plot_data = [topic_assignment_to_dict(assignment) for assignment in assignments]
+    # # TODO - is this right? This just gives us a big string
+    # encoded_scatter_plot_data = json.dumps(scatter_plot_data, cls=DjangoJSONEncoder)
+    topic_model_metadata = models.TopicModelMetadata(scatter_plot_data=scatter_plot_data)
     topic_model_metadata.save()
     # TODO - add more metadata to the topic model
-    assignments = topic_backend.get_topics(question)
 
     for assignment in assignments:
         assignment.answer.save_theme_to_answer(
@@ -34,6 +42,8 @@ def save_themes_for_question(
             processing_run=processing_run,
             topic_model_metadata=topic_model_metadata,
         )
+
+
 
 
 def save_themes_for_processing_run(
