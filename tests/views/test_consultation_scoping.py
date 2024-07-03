c1972a4e69d51a1c3e19dcbe8f6690f13878b306
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-07-03 22:13:54 +0100

    Uncomment test as we want to use it.

--- tests/views/test_consultation_scoping.py
+++ tests/views/test_consultation_scoping.py
@@ -5,19 +5,18 @@ from django.test import RequestFactory
 from consultation_analyser.consultations.views import consultations
 from consultation_analyser.factories import ConsultationFactory, UserFactory
 
-# TODO - fix for messages
-# @pytest.mark.django_db
-# def test_get_consultation_we_own():
-#     user = UserFactory()
-#     consultation_we_own = ConsultationFactory(user=user, with_themes=True)
 
-#     request_factory = RequestFactory()
+@pytest.mark.django_db
+def test_get_consultation_we_own():
+    user = UserFactory()
+    consultation_we_own = ConsultationFactory(user=user, with_themes=True)
 
-#     valid_request = request_factory.get("/consultations/slug-does-not-matter-here/")
-#     valid_request.user = user
-#     resp = consultations.show(valid_request, consultation_slug=consultation_we_own.slug)
+    request_factory = RequestFactory()
+    valid_request = request_factory.get("/consultations/slug-does-not-matter-here/")
+    valid_request.user = user
 
-#     assert resp.status_code == 200
+    resp = consultations.show(valid_request, consultation_slug=consultation_we_own.slug)
+    assert resp.status_code == 200
 
 
 @pytest.mark.django_db
