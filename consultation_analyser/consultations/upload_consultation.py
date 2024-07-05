be15c05b463deab44538f688be1d399f8f062675
Duncan Brown <duncanjbrown@gmail.com>
2024-07-05 14:25:29 +0100

    Add ijson

--- consultation_analyser/consultations/upload_consultation.py
+++ consultation_analyser/consultations/upload_consultation.py
@@ -1,11 +1,10 @@
-import json
 import logging
 import time
-from itertools import batched
 
+import ijson
+import psutil
 from django.utils.text import slugify
 
-from consultation_analyser.consultations.public_schema import ConsultationWithResponses
 
 from .models import Answer, Consultation, ConsultationResponse, Question, Section
 
@@ -14,45 +13,35 @@ logger = logging.getLogger("upload")
 
 class Timer:
     def __init__(self):
-        self.timings = []
         self.last_time = time.time()
 
     def time(self, event):
         abs = time.time()
         delta = abs - self.last_time
+        used_mb = psutil.Process().memory_info().rss / 1024**2
 
-        logline = (abs, delta, event)
-        logger.info(f"{logline}")
-
-        self.timings.append(logline)
         self.last_time = abs
         self.last_delta = delta
+
+        logline = (abs, delta, used_mb, event)
+        logger.info(f"{logline}")
 
 
 def upload_consultation(file, user):
     timer = Timer()
     timer.time("start")
 
-    uploaded_json = file.read()
-    timer.time("file read")
+    sections = []
 
-    attrs = json.loads(uploaded_json)
-    timer.time("file parse")
+    consultation_handle = ijson.items(file, "consultation")
+    for consultation_attrs in consultation_handle:
+        sections = consultation_attrs.pop("sections")
+        consultation_attrs["slug"] = slugify(consultation_attrs["name"])
+        consultation = Consultation(**consultation_attrs)
+        consultation.save()
+        consultation.users.set([user])
 
-    consultation_with_responses = ConsultationWithResponses(**attrs)
-    timer.time("roundtrip to ConsultationWithResponses")
-    del consultation_with_responses
-    del uploaded_json
-
-    responses = attrs["consultation_responses"]
-    consultation_attrs = attrs["consultation"]
-    sections = consultation_attrs.pop("sections")
-    timer.time("separated parts")
-
-    consultation_attrs["slug"] = slugify(consultation_attrs["name"])
-    consultation = Consultation(**consultation_attrs)
-    consultation.save()
-    consultation.users.set([user])
+    del consultation_attrs
     timer.time("created Consultation")
 
     question_ids_map = {}
@@ -72,38 +61,49 @@ def upload_consultation(file, user):
             question = Question(**question_attrs)
             question.save()
             question_ids_map[supplied_id] = question.id
+
+    del sections
     timer.time("created Sections")
 
-    for responses_batch in batched(responses, 500):
-        consultation_responses = []
-        answers_attrs = []
+    file.seek(0)
+    responses_handle = ijson.items(file, "consultation_responses.item")
+    responses_to_process = []
 
-        for response_attrs in responses_batch:
-            answers_attrs.append(response_attrs.pop("answers"))
-            response_attrs["consultation_id"] = consultation.id
-            consultation_responses.append(ConsultationResponse(**response_attrs))
+    for i, response in enumerate(responses_handle):
+        responses_to_process.append(response)
+        if i % 500 == 0:
+            process_response_batch(responses_to_process, consultation.id, question_ids_map)
+            responses_to_process = []
 
-        created_responses = ConsultationResponse.objects.bulk_create(consultation_responses)
-
-        offset_to_response_id_map = {
-            offset: created.id for offset, created in enumerate(created_responses)
-        }
-
-        answers = []
-
-        for offset, answer_list in enumerate(answers_attrs):
-            for answer in answer_list:
-                if "theme_id" in answer:
-                    answer.pop("theme_id")
-                answer["consultation_response_id"] = offset_to_response_id_map[offset]
-                question_id = answer.pop("question_id")
-                answer["question_id"] = question_ids_map[question_id]
-                answers.append(Answer(**answer))
-
-        Answer.objects.bulk_create(answers)
-
-    response_count = len(responses)
-    timer.time(f"created {response_count} Responses")
-    logger.info(f"Time per response: {timer.last_delta / response_count}")
+    timer.time("created Responses")
 
     return consultation
+
+
+def process_response_batch(responses_to_process, consultation_id, question_ids_map):
+    consultation_responses = []
+    answers_attrs = []
+
+    for response_attrs in responses_to_process:
+        answers_attrs.append(response_attrs.pop("answers"))
+        response_attrs["consultation_id"] = consultation_id
+        consultation_responses.append(ConsultationResponse(**response_attrs))
+
+    created_responses = ConsultationResponse.objects.bulk_create(consultation_responses)
+
+    offset_to_response_id_map = {
+        offset: created.id for offset, created in enumerate(created_responses)
+    }
+
+    answers = []
+
+    for offset, answer_list in enumerate(answers_attrs):
+        for answer in answer_list:
+            if "theme_id" in answer:
+                answer.pop("theme_id")
+            answer["consultation_response_id"] = offset_to_response_id_map[offset]
+            question_id = answer.pop("question_id")
+            answer["question_id"] = question_ids_map[question_id]
+            answers.append(Answer(**answer))
+
+    Answer.objects.bulk_create(answers)
