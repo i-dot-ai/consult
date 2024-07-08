1af62ba2dddee1b9a3ad7d34db548493ffa5af63
Duncan Brown <duncanjbrown@gmail.com>
2024-07-08 11:55:47 +0100

    Add Bedrock for summarisation

---
+++ consultation_analyser/pipeline/backends/bedrock_llm_backend.py
@@ -0,0 +1,21 @@
+from langchain_aws import BedrockLLM
+
+from .langchain_llm_backend import LangchainLLMBackend
+
+
+class BedrockLLMBackend(LangchainLLMBackend):
+    def __init__(self):
+        llm = BedrockLLM(
+            # hardcoding this because the kwargs and model
+            # are coupled - can generalise later
+            model_id="mistral.mistral-large-2402-v1:0",
+            region_name=settings.AWS_REGION,
+            model_kwargs={
+                "temperature": 0.8,
+                "max_new_tokens": 2048,
+                "repetition_penalty": 1.03,
+                "stop": ["###", "</s>"],
+                "return_full_text": False,
+            },
+        )
+        super().__init__(llm)
