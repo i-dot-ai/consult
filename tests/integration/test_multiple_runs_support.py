24a5907b66ec9fa78528df031c0aa82991c3915a
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-07-22 12:00:11 +0100

    Fix the timezone issue in tests.

--- tests/integration/test_multiple_runs_support.py
+++ tests/integration/test_multiple_runs_support.py
@@ -1,7 +1,6 @@
-import datetime
 import pytest
+from django.utils import timezone
 
-from consultation_analyser.consultations import models
 from consultation_analyser import factories
 from tests.helpers import sign_in
 
@@ -17,21 +16,18 @@ def test_viewing_multiple_processing_runs_as_staff(django_app):
 
     # Go to consultation in support - are processing runs shown?
     consultation_page = django_app.get(f"/support/consultations/{consultation.id}/")
-    # TODO - sort out datetime/timezones
-    # started_at_str = pr1.started_at.strftime("%-d %B %Y at %H:%M")
-    # finished_at_str = pr2.finished_at.strftime("%-d %B %Y at %H:%M")
-    # assert started_at_str in consultation_page
-    # assert finished_at_str in consultation_page
+    started_at_str = timezone.localtime(pr1.started_at).strftime("%-d %B %Y at %H:%M")
+    finished_at_str = timezone.localtime(pr2.finished_at).strftime("%-d %B %Y at %H:%M")
+    assert started_at_str in consultation_page
+    assert finished_at_str in consultation_page
 
     # Go to the first processing run in frontend
     frontend_first_run_page = consultation_page.click("View on frontend", index=1)
-    assert frontend_first_run_page.request.url.endswith(f"/consultations/{consultation.slug}/runs/{pr1.slug}/")
+    assert frontend_first_run_page.request.url.endswith(
+        f"/consultations/{consultation.slug}/runs/{pr1.slug}/"
+    )
 
     # Check we're going to the URL for that processing run
     question_summary = frontend_first_run_page.click("Question summary", index=0)
     assert pr1.slug in question_summary.request.url
     assert "question" in question_summary.request.url
-
-
-
-
