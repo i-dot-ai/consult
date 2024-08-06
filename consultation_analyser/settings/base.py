01a39c88b74d18d48fdacfe88043af4bced1b525
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-08-06 16:52:05 +0100

    Just use token for auth, don't need to be logged in.

--- consultation_analyser/settings/base.py
+++ consultation_analyser/settings/base.py
@@ -57,9 +57,6 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
