23ca230fd74bb7e8b360acd028d723eef66cfba0
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-08-07 20:44:54 +0100

    Add a view for a support user to get their JWToken.

--- consultation_analyser/support_console/views/api.py
+++ consultation_analyser/support_console/views/api.py
@@ -1,26 +1,27 @@
 from ninja import NinjaAPI, Schema
 from ninja_jwt.authentication import JWTAuth
 
+from consultation_analyser.consultations.public_schema import ConsultationWithResponsesAndThemes, MultipleChoiceItem
+
 
 api = NinjaAPI()
 
 
 
-# TODO - this is made up for testing
-# We would actually like to use the already
-# defined JSON Schema
-class Item(Schema):
-    name: str
-    quantity: int
-
 
 @api.post("/upload_data/", auth=JWTAuth())
-def upload_themed_data(request, data: Item):
+@api.post("/upload-themed-data/")
+def upload_themed_data(request, data: ConsultationWithResponsesAndThemes):
     print("printing your data")
     print(data)
+    # TODO - what to do when validation errors?
+    # TODO - write to DB
     return "thanks for your data!"
 
 
 @api.get("/hello/", auth=JWTAuth())
 def hello(request):
+    print(request.user)
     return "Hello world"
+
+
