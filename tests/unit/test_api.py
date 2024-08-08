b23d20a2d6fd3f84233c11a155f547c4b87c5bd4
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-08-08 23:17:48 +0100

    more updates to api tests

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -18,14 +18,32 @@ def test_upload_consultation_no_auth(client):
 
 @pytest.mark.django_db
 def test_upload_consultation_valid(client):
-    user = factories.UserFactory(email="staff@example.com", is_staff=True)
-    client.force_login(user)
-    refresh = RefreshToken.for_user(user)
+    staff_user = factories.UserFactory(email="staff@example.com", is_staff=True)
+    client.force_login(staff_user)
+    refresh = RefreshToken.for_user(staff_user)
+    access_token = str(refresh.access_token)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    with open(VALID_CONSULTATION_UPLOAD, "r") as file:
+        data = json.load(file)
+
+    # Staff user should be able to upload consultation
+    response = client.post(UPLOAD_CONSULTATION_URL, headers=headers, data=json.dumps(data), content_type="application/json")
+    assert response.status_code == 200
+
+    # TODO - test for invalid data
+
+
+@pytest.mark.django_db
+def test_upload_consultation_non_staff_user(client):
+    regular_user = factories.UserFactory(email="email@example.com", is_staff=False)
+    client.force_login(regular_user)
+    refresh = RefreshToken.for_user(regular_user)
     access_token = str(refresh.access_token)
     headers = {"Authorization": f"Bearer {access_token}"}
     with open(VALID_CONSULTATION_UPLOAD, "r") as file:
         data = json.load(file)
 
     response = client.post(UPLOAD_CONSULTATION_URL, headers=headers, data=json.dumps(data), content_type="application/json")
-    assert response.status_code == 200
-
+    print(response.content)
+    assert response.status_code == 401
+    # TODO - what should response be? At the moment 302 redirect.
