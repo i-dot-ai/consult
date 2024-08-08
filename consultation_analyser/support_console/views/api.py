10583d698b0a09714026153bc228d255e16725d6
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-08-08 13:20:37 +0100

    actually upload data - do it for a consultation first

--- consultation_analyser/support_console/views/api.py
+++ consultation_analyser/support_console/views/api.py
@@ -1,20 +1,27 @@
-from ninja import NinjaAPI, Schema
+import datetime
+import logging
+
+from django.core.files.storage import default_storage as storage
+from ninja import NinjaAPI
 from ninja_jwt.authentication import JWTAuth
 
-from consultation_analyser.consultations.public_schema import ConsultationWithResponsesAndThemes, MultipleChoiceItem
+from consultation_analyser.consultations.jobs.upload_consultation import async_upload_consultation
+from consultation_analyser.consultations.public_schema import ConsultationWithResponses
 
+logger = logging.getLogger("api")
 
 api = NinjaAPI()
 
 
-@api.post("/upload_data/", auth=JWTAuth())
-@api.post("/upload-themed-data/")
-def upload_themed_data(request, data: ConsultationWithResponsesAndThemes):
-    print("printing your data")
-    print(data)
-    # TODO - what to do when validation errors?
-    # TODO - write to DB
-    return "thanks for your data!"
+# For now, just upload a consultation as we don't know format of themes
+@api.post("/upload-consultation/", auth=JWTAuth())
+def upload_consultation(request, data: ConsultationWithResponses):
+    logger.info("Saving uploaded consultation data")
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    filename = f"{timestamp}-consultation-uploaded.json"
+    file_path = storage.save(filename, data)
+    async_upload_consultation.delay(file_path, request.user.id)
+    return "Your data is being uploaded"
 
 
 @api.get("/hello/", auth=JWTAuth())
