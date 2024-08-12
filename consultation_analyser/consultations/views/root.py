0e1655f6483d9dbfa98f4aaf25d14fe88320e720
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-08-12 14:05:12 +0100

    public pages should still be accessible

--- consultation_analyser/consultations/views/root.py
+++ consultation_analyser/consultations/views/root.py
@@ -1,9 +1,11 @@
 from django.http import HttpRequest
 from django.shortcuts import redirect, reverse
+from django.contrib.auth.decorators import login_not_required
 
 from .pages import home
 
 
+@login_not_required
 def root(request: HttpRequest):
     if request.user.is_authenticated:
         return redirect(reverse("consultations"))
