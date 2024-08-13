dc829898d4633f19c8c687eb429f353337a61d1f
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-08-13 16:41:42 +0100

    Add a test for emails in different cases.

--- tests/unit/test_emails.py
+++ tests/unit/test_emails.py
@@ -1,15 +1,17 @@
 from django.core import mail
+import pytest
 
 from consultation_analyser.email import send_magic_link_email
 
-
-def test_magic_link_email():
+@pytest.mark.parametrize("email", [("email@example.com",), ("EMAIL@Example.com",)])
+def test_magic_link_email(email):
     send_magic_link_email(
-        to="email@example.com",
+        to=email,
         magic_link="https://example.com",
     )
 
     sent_mail = mail.outbox[0]
     assert sent_mail.subject == "Sign in to Consult"
-    assert ["email@example.com"] == sent_mail.to
+    assert [email] == sent_mail.to
     assert "https://example.com" in sent_mail.body
+
