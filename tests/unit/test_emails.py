f9bb5254ec9af67ef1d249fbae7eeb062d60a89b
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2024-08-13 16:52:12 +0100

    format

--- tests/unit/test_emails.py
+++ tests/unit/test_emails.py
@@ -1,7 +1,8 @@
-from django.core import mail
 import pytest
+from django.core import mail
 
 from consultation_analyser.email import send_magic_link_email
+
 
 @pytest.mark.parametrize("email", [("email@example.com",), ("EMAIL@Example.com",)])
 def test_magic_link_email(email):
@@ -14,4 +15,3 @@ def test_magic_link_email(email):
     assert sent_mail.subject == "Sign in to Consult"
     assert [email] == sent_mail.to
     assert "https://example.com" in sent_mail.body
-
