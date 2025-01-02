a47839560083ea053e359a8a880bf8b4f1092eba
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2025-01-02 16:40:24 +0000

    add tests and fix issues with factories

--- consultation_analyser/factories2.py
+++ consultation_analyser/factories2.py
@@ -126,12 +126,16 @@ class SentimentMappingFactory(DjangoModelFactory):
     position = factory.Iterator(models.SentimentMapping.PositionType.values)
 
 
-def create_consultation_from_yaml(
+def create_dummy_consultation_from_yaml(
     file_path: str = "./tests/examples/sample_questions.yml",
     number_respondents: int = 5,
     consultation: Optional[models.Consultation2] = None,
 ) -> Consultation2Factory:
-    """Create consultation with question, question parts and answers from yaml file."""
+    """
+    Create consultation with question, question parts, answers and themes from yaml file.
+    Creates relevant objects: Consultation, Question, QuestionPart, Answer, Theme, ThemeMapping,
+    SentimentMapping, Framework, ExecutionRun.
+    """
     if not consultation:
         consultation = Consultation2Factory()
     respondents = [RespondentFactory(consultation=consultation) for _ in range(number_respondents)]
@@ -141,10 +145,12 @@ def create_consultation_from_yaml(
 
     # Save all questions, and corresponding question parts and answers
     for question_data in questions_data:
-        question_text = question_data["question_text"]
-        order = question_data["order"]
+        question = Question2Factory(
+            text=question_data["question_text"],
+            order=question_data["order"],
+            consultation=consultation,
+        )
         parts = question_data["parts"]
-        question = Question2Factory(text=question_text, order=order, consultation=consultation)
 
         # Each question part is considered separately
         for part in parts:
@@ -152,21 +158,26 @@ def create_consultation_from_yaml(
             question_part = QuestionPartFactory(
                 question=question,
                 text=part["text"],
-                type=type,
+                type=question_part_type,
                 options=part.get("options", []),
                 order=part["order"],
             )
 
             # Get themes if free_text
-            if question_part_type == "free_text":
-                # Create themes (simulate theme generation)
-                execution_run_theme_generation = ExecutionRunFactory(
+            if question_part_type == models.QuestionPart.QuestionType.FREE_TEXT:
+                # Simulate execution runs for each question to generate sentiment, themes, theme mapping
+                sentiment_run = ExecutionRunFactory(
+                    type=models.ExecutionRun.TaskType.SENTIMENT_ANALYSIS
+                )
+                theme_generation_run = ExecutionRunFactory(
                     type=models.ExecutionRun.TaskType.THEME_GENERATION
                 )
                 framework = FrameworkFactory(
-                    execution_run=execution_run_theme_generation, question_part=question_part
+                    execution_run=theme_generation_run, question_part=question_part
                 )
-
+                theme_mapping_run = ExecutionRunFactory(
+                    type=models.ExecutionRun.TaskType.THEME_MAPPING
+                )
                 themes = part.get("themes", [])
                 for theme in themes:
                     theme_objects = [
@@ -177,21 +188,20 @@ def create_consultation_from_yaml(
                         )
                     ]
 
-                # Simulate theme mapping
-                execution_run_theme_mapping = ExecutionRunFactory(
-                    type=models.ExecutionRun.TaskType.THEME_MAPPING
-                )
-
+            # Now populate the answers and corresponding themes etc. for these question parts
             for respondent in respondents:
-                if question_part_type == "single_option":
+                print("=======")
+                print(question_part_type)
+                print(question_part.text)
+                if question_part_type == models.QuestionPart.QuestionType.SINGLE_OPTION:
                     chosen_options = random.choice(part["options"])
                     text = ""
-                elif question_part_type == "multiple_option":
+                elif question_part_type == models.QuestionPart.QuestionType.MULTIPLE_OPTIONS:
                     chosen_options = random.sample(
                         part["options"], k=random.randint(1, len(part["options"]))
                     )
                     text = ""
-                elif question_part_type == "free_text":
+                elif question_part_type == models.QuestionPart.QuestionType.FREE_TEXT:
                     text = random.choice(part.get("free_text_answers", [""]))
                     chosen_options = []
 
@@ -201,14 +211,20 @@ def create_consultation_from_yaml(
                     chosen_options=chosen_options,
                     respondent=respondent,
                 )
-                # Now map (multiple) themes to each answer
-                if question_part_type == "free_text":
+                # Now map (multiple) themes and sentiment to each answer for free-text questions.
+                # This is in a different order to how it would work in pipeline - but this is as we
+                # are reading from file.
+                if question_part_type == models.QuestionPart.QuestionType.FREE_TEXT:
                     themes_for_answer = random.sample(
                         theme_objects, k=random.randint(1, len(theme_objects))
                     )
                     for theme in themes_for_answer:
                         ThemeMappingFactory(
-                            answer=answer, theme=theme, execution_run=execution_run_theme_mapping
+                            answer=answer, theme=theme, execution_run=theme_mapping_run
                         )
+                    sentiment = random.choice(models.SentimentMapping.PositionType.values)
+                    SentimentMappingFactory(
+                        answer=answer, execution_run=sentiment_run, position=sentiment
+                    )
 
     return consultation
--- tests/unit/models/test_factories.py
+++ tests/unit/models/test_factories.py
@@ -5,6 +5,7 @@ Test factories for the new consultation models.
 import pytest
 
 from consultation_analyser import factories2
+from consultation_analyser.consultations import models
 
 
 @pytest.mark.django_db
@@ -23,3 +24,41 @@ def test_factories():
     factories2.Theme2Factory()
     factories2.ThemeMappingFactory()
     factories2.SentimentMappingFactory()
+
+
+@pytest.mark.django_db
+def test_create_dummy_consultation_from_yaml():
+    consultation = factories2.create_dummy_consultation_from_yaml(number_respondents=10)
+    questions = models.Question2.objects.filter(consultation=consultation)
+    assert questions.count() == 5
+    assert questions.filter(
+        text="What are your thoughts on how the current chocolate bar regulations could be improved to better address consumer needs and industry standards?"
+    ).exists()
+
+    question_1 = questions.get(
+        text="Do you agree with the proposal to align the flavour categories of chocolate bars as outlined in the draft guidelines of the Chocolate Bar Regulation for the United Kingdom?"
+    )
+    question_parts = models.QuestionPart.objects.filter(question=question_1).order_by("order")
+    assert question_parts[0].type == models.QuestionPart.QuestionType.SINGLE_OPTION
+    assert question_parts[0].options == ["Yes", "No", "Don't know", "No answer"]
+    assert question_parts[0].order == 1
+    assert not question_parts[0].text
+    assert question_parts[1].text == "Please explain your answer."
+    assert not question_parts[1].options
+    answers = models.Answer2.objects.filter(question_part=question_parts[1])
+    assert answers.count() == 10
+    theme_mapping = models.ThemeMapping.objects.filter(answer__in=answers)
+    assert theme_mapping.count() >= 10
+    themes = models.Theme2.objects.filter(framework__question_part=question_parts[1])
+    assert themes.count() == 2
+    assert themes.filter(theme_name="Standardized framework").exists()
+
+    assert models.QuestionPart.objects.filter(
+        type=models.QuestionPart.QuestionType.SINGLE_OPTION
+    ).exists()
+    assert models.QuestionPart.objects.filter(
+        type=models.QuestionPart.QuestionType.MULTIPLE_OPTIONS
+    ).exists()
+
+    assert models.ExecutionRun.objects.count() == 3 * 3  # 3 free-text parts, 3 types of run
+    assert models.Framework.objects.count() == 3
