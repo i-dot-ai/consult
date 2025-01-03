773d47e7c94d402d7993bb5d179218f815958fdc
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2025-01-03 18:55:36 +0000

    Change consultation views to use new consultation model.

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -1,7 +1,5 @@
 import logging
-from typing import Optional
 
-from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import Http404, HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
@@ -17,31 +15,45 @@ NO_THEMES_YET_MESSAGE = "We are processing your consultation. Themes have not be
 
 @login_required
 def index(request: HttpRequest) -> HttpResponse:
-    consultations = request.user.consultation_set.all()
     user = request.user
+    consultations_for_user = models.Consultation2.objects.filter(users=user)
     is_staff = user.is_staff
-    context = {"consultations": consultations, "is_staff": is_staff}
+    context = {"consultations": consultations_for_user, "is_staff": is_staff}
     return render(request, "consultations/consultations/index.html", context)
 
 
 @user_can_see_consultation
 @login_required
-def show(
-    request: HttpRequest, consultation_slug: str, processing_run_slug: Optional[str] = None
-) -> HttpResponse:
-    consultation = get_object_or_404(models.Consultation, slug=consultation_slug)
-    try:
-        processing_run = consultation.get_processing_run(processing_run_slug)
-    except models.ProcessingRun.DoesNotExist:
-        return Http404
-    if not processing_run:
-        messages.info(request, NO_THEMES_YET_MESSAGE)
-
-    questions = models.Question.objects.filter(section__consultation__slug=consultation_slug)
+def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
+    consultation = get_object_or_404(models.Consultation2, slug=consultation_slug)
+    questions = models.Question2.objects.filter(consultation__slug=consultation_slug).order_by(
+        "order"
+    )
     context = {
         "questions": questions,
         "consultation": consultation,
-        "processing_run": processing_run,
     }
-
+    # TODO - do something better if there is no question text, and get it from the question parts
     return render(request, "consultations/consultations/show.html", context)
+<<<<<<< HEAD
+=======
+
+
+# TODO - to be removed (separate PR?)
+@login_required
+def new(request: HttpRequest):
+    if not request.POST:
+        form = ConsultationUploadForm()
+    else:
+        logger.info("Upload received")
+        form = ConsultationUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            logger.info("Enqueueing upload_consultation job")
+            file_path = storage.save(
+                request.FILES["consultation_json"].name, request.FILES["consultation_json"]
+            )
+            async_upload_consultation.delay(file_path, request.user.id)
+            return render(request, "consultations/consultations/uploaded.html", {})
+
+    return render(request, "consultations/consultations/new.html", {"form": form})
+>>>>>>> 93526a0 (Change consultation views to use new consultation model.)
