67737c52eb8e4fb3151b8288de996d74961c1c63
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2025-01-03 17:24:59 +0000

    Change decorator to use new consultation model

--- consultation_analyser/consultations/views/decorators.py
+++ consultation_analyser/consultations/views/decorators.py
@@ -12,7 +12,7 @@ def user_can_see_consultation(view_function):
             raise Http404()
 
         # will kick out users by throwing a 404 if they don't own the consultation
-        get_object_or_404(models.Consultation.objects.filter(slug=slug, users__in=[request.user]))
+        get_object_or_404(models.Consultation2.objects.filter(slug=slug, users__in=[request.user]))
 
         return view_function(request, *args, **kwargs)
 
