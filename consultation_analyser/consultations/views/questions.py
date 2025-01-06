5b1ee989241c85a84b0445a10514cc3ff1c88258
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2025-01-06 21:41:17 +0000

    Remove the old, unused models.

--- consultation_analyser/consultations/views/questions.py
+++ consultation_analyser/consultations/views/questions.py
@@ -1,44 +1,10 @@
-from typing import Dict, List, Tuple
-
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count, QuerySet
+from django.db.models import Count
 from django.http import HttpRequest
 from django.shortcuts import get_object_or_404, render
 
 from .. import models
 from .decorators import user_can_see_consultation
-
-# from .filters import filter_answers_by_keyword, get_applied_filters, get_filtered_answers, get_filtered_themes
-
-
-def filter_scatter_plot_data(filtered_themes: QuerySet) -> List[Dict]:
-    if not filtered_themes:
-        return []
-    theme = filtered_themes.first()  # metadata same for all themes for question
-    topic_model_metadata = theme.topic_model_metadata
-    data = topic_model_metadata.scatter_plot_data
-    if not data:  # Old consultations - didn't calculate scatter data when generating themes
-        return []
-    data = data["data"]
-    topic_ids = [theme.topic_id for theme in filtered_themes]
-    filtered_scatter_data = [
-        coordinate for coordinate in data if coordinate["topic_id"] in topic_ids
-    ]
-    return filtered_scatter_data
-
-
-def get_outliers_info(processing_run: models.ProcessingRun, question: models.QuestionOld) -> Tuple:
-    outlier_theme = None
-    outliers_count = 0
-    if not processing_run:
-        return outlier_theme, outliers_count
-    outlier_themes = processing_run.get_themes_for_question(question_id=question.id).filter(
-        is_outlier=True
-    )
-    if outlier_themes:
-        outlier_theme = outlier_themes.first()
-        outliers_count = models.AnswerOld.objects.filter(themes=outlier_theme).count()
-    return outlier_theme, outliers_count
 
 
 @user_can_see_consultation
