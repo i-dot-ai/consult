876584b3c36dfffb4ddd8e86b44e5472f028d079
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2025-01-07 22:54:06 +0000

    add tests for history of theme mapping

--- tests/unit/models/test_theme_mapping.py
+++ tests/unit/models/test_theme_mapping.py
@@ -21,6 +21,7 @@ def test_get_latest_theme_mappings_for_question_part_returns_latest_mappings():
     theme1 = ThemeFactory(framework=first_framework)
     first_execution_run = ExecutionRunFactory(type=models.ExecutionRun.TaskType.THEME_MAPPING)
     ThemeMappingFactory(answer=answer, theme=theme1, execution_run=first_execution_run)
+
     # Second theme mapping execution run
     second_framework = FrameworkFactory(question_part=question_part)
     theme2 = ThemeFactory(framework=second_framework, name="theme 2")
@@ -41,3 +42,35 @@ def test_get_latest_theme_mappings_for_question_part_no_mappings():
     question_part = QuestionPartFactory()
     latest_mappings = models.ThemeMapping.get_latest_theme_mappings_for_question_part(question_part)
     assert latest_mappings.count() == 0
+
+
+@pytest.mark.django_db
+def test_get_history_of_changes_to_answer():
+    question_part = QuestionPartFactory()
+    answer1 = AnswerFactory(question_part=question_part)
+    framework = FrameworkFactory(question_part=question_part)
+    theme1 = ThemeFactory(framework=framework, name="theme 1")
+    theme2 = ThemeFactory(framework=framework, name="theme 2")
+    theme3 = ThemeFactory(framework=framework, name="theme 3")
+
+    # Now map a theme to the Answer
+    theme_mapping = ThemeMappingFactory(
+        answer=answer1, theme=theme1, reason="Theme 1 is most reasonable"
+    )
+
+    # Now change the theme mapping
+    theme_mapping.theme = theme2
+    theme_mapping.save()
+    theme_mapping.theme = theme3
+    theme_mapping.save()
+
+    # Check current status i.e. latest theme mapping
+    assert theme_mapping.theme == theme3
+
+    # Can we see the history of the changes to the answer?
+    assert theme_mapping.history.all().count() == 3  # total changes
+    earliest = theme_mapping.history.earliest()
+    assert earliest.theme == theme1, earliest.theme
+    latest = theme_mapping.history.latest()
+    assert latest.theme == theme3
+    assert earliest.modified_at < latest.modified_at
