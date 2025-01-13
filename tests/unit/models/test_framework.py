16d38d77fb11521c34ad3b222cdc0bf5cab66879
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2025-01-13 12:12:31 +0000

    update framework tests

--- tests/unit/models/test_framework.py
+++ tests/unit/models/test_framework.py
@@ -3,55 +3,6 @@ import pytest
 from consultation_analyser import factories
 from consultation_analyser.consultations import models
 
-# TODO - Redo these tests with updated fixtures
-
-# def set_up_frameworks():
-#     user_1 = factories.UserFactory()
-#     user_2 = factories.UserFactory()
-#     question_part = factories.QuestionPartFactory(type=models.QuestionPart.QuestionType.FREE_TEXT)
-
-#     # Set up initial framework - replicate a run of the theme generation task.
-#     theme_generation_run = factories.ExecutionRunFactory(
-#         type=models.ExecutionRun.TaskType.THEME_GENERATION
-#     )
-#     framework_1 = factories.InitialFrameworkFactory(
-#         execution_run=theme_generation_run, question_part=question_part
-#     )
-#     theme_x = factories.InitialThemeFactory(name="X", framework=framework_1)
-#     theme_y = factories.InitialThemeFactory(name="Y", framework=framework_1)
-#     factories.InitialThemeFactory(name="Z", framework=framework_1)
-
-#     # Create a new framework amending these themes
-#     framework_2a = framework_1.amend_framework(
-#         user=user_1, change_reason="I wanted to change the themes."
-#     )
-#     factories.InitialThemeFactory(name="X2", framework=framework_2a, precursor=theme_x)
-
-#     # Create another new framework amending the initial themes
-#     framework_2b = framework_1.amend_framework(
-#         user=user_2, change_reason="I wanted to change the themes in a different way."
-#     )
-#     factories.InitialThemeFactory(name="Y2", framework=framework_2b, precursor=theme_y)
-#     factories.InitialThemeFactory(name="W", framework=framework_2b, precursor=None)
-
-#     return framework_1, framework_2a, framework_2b
-
-
-# @pytest.mark.django_db
-# def test_amend_framework():
-#     framework_1, _, _ = set_up_frameworks()
-#     user = factories.UserFactory()
-#     amended_framework = framework_1.amend_framework(
-#         user=user, change_reason="I wanted to change the themes."
-#     )
-#     # ID, precursor, user, change reason should all be updated.
-#     assert amended_framework.id != framework_1.id
-#     assert amended_framework.precursor == framework_1
-#     assert amended_framework.user == user
-#     assert amended_framework.change_reason == "I wanted to change the themes."
-#     # Question part should be the same
-#     assert amended_framework.question_part == framework_1.question_part
-
 
 @pytest.mark.django_db
 def test_cant_save():
@@ -96,36 +47,22 @@ def test_create_descendant_framework():
     assert not new_framework.execution_run
 
 
-# @pytest.mark.django_db
-# def test_get_themes_removed_from_previous_framework():
-#     framework_1, framework_2a, framework_2b = set_up_frameworks()
-#     removed_themes = framework_1.get_themes_removed_from_previous_framework()
-#     assert len(removed_themes) == 0
+# TODO - turn into fixtures
+@pytest.mark.django_db
+def test_get_themes_removed_from_previous_framework():
+    # Create framework with 3 themes
+    initial_theme_1 = factories.InitialThemeFactory(name="initial_theme_1")
+    initial_framework = initial_theme_1.framework
+    initial_theme_2 = factories.InitialThemeFactory(name="initial_theme_2", framework=initial_framework)
+    initial_theme_3 = factories.InitialThemeFactory(name="initial_theme_3", framework=initial_framework)
 
-#     removed_themes = framework_2a.get_themes_removed_from_previous_framework()
-#     theme_y = models.Theme.objects.get(name="Y")
-#     assert len(removed_themes) == 2
-#     assert theme_y in removed_themes
-#     assert removed_themes[0].name == "Y"
+    # Create subsequent framework, two themes removed
+    descendent_theme_1 = factories.DescendantThemeFactory(name="descendent_theme_1")
+    descendent_framework = descendent_theme_1.framework
 
-#     removed_themes = framework_2b.get_themes_removed_from_previous_framework()
-#     assert len(removed_themes) == 2
-#     assert set(removed_themes.values_list("name", flat=True)) == {"X", "Z"}
+    themes_removed = descendent_framework.get_themes_removed_from_previous_framework()
+    assert len(themes_removed) == 2
+    assert initial_theme_2 in themes_removed
+    assert initial_theme_3 in themes_removed
+    assert initial_theme_1 not in themes_removed
 
-
-# @pytest.mark.django_db
-# def test_get_themes_added_from_previous_framework():
-#     framework_1, framework_2a, framework_2b = set_up_frameworks()
-#     theme_x = models.Theme.objects.get(name="X")
-#     theme_w = models.Theme.objects.get(name="W")
-
-#     themes = framework_1.get_themes_added_to_previous_framework()
-#     assert len(themes) == 3
-#     assert theme_x in themes
-
-#     themes = framework_2a.get_themes_added_to_previous_framework()
-#     assert len(themes) == 0
-
-#     themes = framework_2b.get_themes_added_to_previous_framework()
-#     assert len(themes) == 1
-#     assert theme_w in themes
