d13062c749a268ebeec7148263ea43098ebe9a29
Nina Menezes <nina.menezes@cabinetoffice.gov.uk>
2025-01-17 14:30:59 +0000

    add test to check that slug updates

--- tests/unit/models/test_consultation.py
+++ tests/unit/models/test_consultation.py
@@ -4,7 +4,7 @@ from consultation_analyser.factories import ConsultationFactory
 
 
 @pytest.mark.django_db
-def test_consultation_save():
+def test_consultation_save_unique_slugs():
     consultation_title = "My First Consultation"
     slugified = "my-first-consultation"
     consultation = ConsultationFactory(title=consultation_title)
@@ -15,3 +15,14 @@ def test_consultation_save():
     yet_another_consultation = ConsultationFactory(title=consultation_title)
     assert yet_another_consultation.slug != consultation.slug
     assert yet_another_consultation.slug == f"{slugified}-2"
+
+
+
+@pytest.mark.django_db
+def test_consultation_save_update_slugs():
+    consultation_title = "My First Consultation"
+    consultation = ConsultationFactory(title=consultation_title)
+    assert consultation.slug == "my-first-consultation"
+    consultation.title = "My Second Consultation"
+    consultation.save()
+    assert consultation.slug == "my-second-consultation"
