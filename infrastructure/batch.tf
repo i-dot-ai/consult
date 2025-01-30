166c4945966961d4a4a5ca96cd22044b022ecafa
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-01-30 13:21:05 +0000

    Updated modules throughout

--- infrastructure/batch.tf
+++ infrastructure/batch.tf
@@ -5,7 +5,7 @@ locals {
 module "batch_compute" {
   # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
   # source          = "../../i-ai-core-infrastructure/modules/batch/batch_compute_environment"
-  source = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/batch-compute-environment?ref=v1.0.0-batch-compute-environment"
+  source          = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/batch-compute-environment?ref=v3.1.0-batch-compute-environment"
   account_id      = var.account_id
   name            = local.name
   region          = var.region
@@ -14,22 +14,20 @@ module "batch_compute" {
   min_vcpus       = 0
   max_vcpus       = 20
   private_subnets = data.terraform_remote_state.vpc.outputs.private_subnets
-  state_bucket    = var.state_bucket
   instance_type   = "g5.xlarge"
-  additional_iam_policy   = aws_iam_policy.batch.arn
+  additional_iam_policies = { "batch" : aws_iam_policy.batch.arn }
 }
 
 module "batch_job_definition" {
   # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
   # source                  = "../../i-ai-core-infrastructure/modules/batch/batch_job_definitons"
-  source = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/batch-job-definitions?ref=v1.0.0-batch-job-definitions"
-  name                    = local.name
+  source                   = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/batch-job-definitions?ref=v3.2.0-batch-job-definitions"
+  name                     = local.name
   compute_environment_arn = [module.batch_compute.ec2_compute_environment_arn]
-  state_bucket            = var.state_bucket
-  image                   = "${local.batch_image_ecr_url}:${var.image_tag}"
-  fargate_flag            = false
-  env_vars                = local.batch_env_vars
-  additional_iam_policy   = aws_iam_policy.batch.arn
+  image                    = "${local.batch_image_ecr_url}:${var.image_tag}"
+  use_fargate              = false
+  env_vars                 = local.batch_env_vars
+  additional_iam_policies = { "batch" : aws_iam_policy.batch.arn }
   task_memory_requirements = local.batch_memory
   task_vcpu_requirements   = local.batch_vcpus
 }
