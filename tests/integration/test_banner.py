dd730a92bc815cdf63cb6bb2b66a6828acf47c67
Nina Menezes <nina.menezes@ai.gov.uk>
2025-02-03 15:59:37 +0000

    fix test now warning is out

--- tests/integration/test_banner.py
+++ tests/integration/test_banner.py
@@ -9,8 +9,8 @@ def test_user_can_sign_in(django_app):
     UserFactory(email="email@example.com")
 
     logged_out_homepage = django_app.get("/")
-    assert "pilot stage" not in logged_out_homepage
+    assert "Your consultations" not in logged_out_homepage
 
     consultations_index = sign_in(django_app, "email@example.com")
 
-    assert "pilot stage" in consultations_index
+    assert "Your consultations" in consultations_index
