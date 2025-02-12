413e4156da73e2e935a6f13c53cb4395eed9ac88
Nina Menezes <nina.menezes@ai.gov.uk>
2025-02-12 21:20:42 +0000

    update type hint

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -26,6 +26,29 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "consultations/consultations/index.html", context)
 
 
+def get_top_themes_for_free_text_question_part(
+    free_text_question_part: QuestionPart, number_top_themes: int = 8
+) -> list[dict]:
+    # For now, just get latest theme mappings
+    theme_mappings_qs = ThemeMapping.get_latest_theme_mappings_for_question_part(
+        free_text_question_part
+    )
+    top_themes_counts_qs = (
+        theme_mappings_qs.values("theme")
+        .annotate(theme_count=Count("theme"))
+        .order_by("-theme_count")
+    )[:number_top_themes]
+    top_themes_by_counts_list = []
+    for item in top_themes_counts_qs:
+        theme_obj = Theme.objects.get(id=item["theme"])
+        output = {
+            "theme": theme_obj,
+            "count": item["theme_count"],
+        }
+        top_themes_by_counts_list.append(output)
+    return top_themes_by_counts_list
+
+
 @user_can_see_consultation
 @login_required
 def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
@@ -44,25 +67,10 @@ def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
         )
         if free_text_question_part:
             question_dict["free_text_question_part"] = free_text_question_part
-            # For now, just get latest theme mappings
-            number_of_themes = 8  # Bit arbitrary
-            theme_mappings_qs = ThemeMapping.get_latest_theme_mappings_for_question_part(
+            top_themes_by_counts_list = get_top_themes_for_free_text_question_part(
                 free_text_question_part
             )
-            top_counts_by_theme_qs = (
-                theme_mappings_qs.values("theme")
-                .annotate(theme_count=Count("theme"))
-                .order_by("-theme_count")
-            )[:number_of_themes]
-            top_counts_by_theme_list = []
-            for item in top_counts_by_theme_qs:
-                theme_obj = Theme.objects.get(id=item["theme"])
-                output = {
-                    "theme": theme_obj,
-                    "count": item["theme_count"],
-                }
-                top_counts_by_theme_list.append(output)
-            question_dict["theme_counts"] = top_counts_by_theme_list
+            question_dict["theme_counts"] = top_themes_by_counts_list
         # For now, assume that we just take the first closed question (in many cases there will be just one)
         # Just take the single option for now
         single_option_question_part = (
