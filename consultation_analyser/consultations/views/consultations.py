862e3fef587d8d63d6c549365b6dd1e992990713
Nina Menezes <nina.menezes@ai.gov.uk>
2025-02-12 12:11:11 +0000

    Add top themes.

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -3,8 +3,9 @@ import logging
 from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
+from django.db.models import Count
 
-from consultation_analyser.consultations.models import QuestionPart, Question, Consultation, ThemeMapping
+from consultation_analyser.consultations.models import QuestionPart, Question, Consultation, ThemeMapping, Theme
 from .decorators import user_can_see_consultation
 
 logger = logging.getLogger("upload")
@@ -33,9 +34,19 @@ def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
         free_text_question_part = QuestionPart.objects.filter(question=question).filter(type=QuestionPart.QuestionType.FREE_TEXT).first()
         if free_text_question_part:
             question_dict["free_text_question_part"] = free_text_question_part
-            # TODO - add top themes
             # For now, just get latest theme mappings
-            theme_mappings = ThemeMapping.get_latest_theme_mappings_for_question_part(free_text_question_part)
+            number_of_themes = 8 # Bit arbitrary
+            theme_mappings_qs = ThemeMapping.get_latest_theme_mappings_for_question_part(free_text_question_part)
+            top_counts_by_theme_qs = (theme_mappings_qs.values('theme').annotate(theme_count=Count('theme')).order_by('-theme_count'))[:number_of_themes]
+            top_counts_by_theme_list = []
+            for item in top_counts_by_theme_qs:
+                theme_obj = Theme.objects.get(id=item["theme"])
+                output = {
+                    "theme": theme_obj,
+                    "count": item["theme_count"],
+                }
+                top_counts_by_theme_list.append(output)
+            question_dict["theme_counts"] = top_counts_by_theme_list
         # For now, assume that we just take the first closed question (in many cases there will be just one)
         # Just take the single option for now
         single_option_question_part = QuestionPart.objects.filter(question=question).filter(
@@ -45,9 +56,6 @@ def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
             question_dict["single_option_question_part"] = single_option_question_part
             # TODO - add counts
         questions_list.append(question_dict)
-
-    print("======== questions_list ===========")
-    print(questions_list)
 
     context = {
         "all_questions": questions_list,
