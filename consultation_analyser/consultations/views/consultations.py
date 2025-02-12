4ddaccd5c35923998716a28f312a090334cccd65
Nina Menezes <nina.menezes@ai.gov.uk>
2025-02-12 15:17:49 +0000

    Get the rest of the data we need to display on the dashboard page

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -1,11 +1,18 @@
 import logging
 
 from django.contrib.auth.decorators import login_required
+from django.db.models import Count
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import get_object_or_404, render
-from django.db.models import Count
 
-from consultation_analyser.consultations.models import QuestionPart, Question, Consultation, ThemeMapping, Theme
+from consultation_analyser.consultations.models import (
+    Consultation,
+    Question,
+    QuestionPart,
+    Theme,
+    ThemeMapping,
+)
+
 from .decorators import user_can_see_consultation
 
 logger = logging.getLogger("upload")
@@ -23,21 +30,29 @@ def index(request: HttpRequest) -> HttpResponse:
 def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
     # Dashboard page
     consultation = get_object_or_404(Consultation, slug=consultation_slug)
-    questions = Question.objects.filter(consultation__slug=consultation_slug).order_by(
-        "number"
-    )
+    questions = Question.objects.filter(consultation__slug=consultation_slug).order_by("number")
 
     questions_list = []
     for question in questions:
         question_dict = {"question": question}
         # Get free text question stuff
-        free_text_question_part = QuestionPart.objects.filter(question=question).filter(type=QuestionPart.QuestionType.FREE_TEXT).first()
+        free_text_question_part = (
+            QuestionPart.objects.filter(question=question)
+            .filter(type=QuestionPart.QuestionType.FREE_TEXT)
+            .first()
+        )
         if free_text_question_part:
             question_dict["free_text_question_part"] = free_text_question_part
             # For now, just get latest theme mappings
-            number_of_themes = 8 # Bit arbitrary
-            theme_mappings_qs = ThemeMapping.get_latest_theme_mappings_for_question_part(free_text_question_part)
-            top_counts_by_theme_qs = (theme_mappings_qs.values('theme').annotate(theme_count=Count('theme')).order_by('-theme_count'))[:number_of_themes]
+            number_of_themes = 8  # Bit arbitrary
+            theme_mappings_qs = ThemeMapping.get_latest_theme_mappings_for_question_part(
+                free_text_question_part
+            )
+            top_counts_by_theme_qs = (
+                theme_mappings_qs.values("theme")
+                .annotate(theme_count=Count("theme"))
+                .order_by("-theme_count")
+            )[:number_of_themes]
             top_counts_by_theme_list = []
             for item in top_counts_by_theme_qs:
                 theme_obj = Theme.objects.get(id=item["theme"])
@@ -49,17 +64,19 @@ def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
             question_dict["theme_counts"] = top_counts_by_theme_list
         # For now, assume that we just take the first closed question (in many cases there will be just one)
         # Just take the single option for now
-        single_option_question_part = QuestionPart.objects.filter(question=question).filter(
-            type=QuestionPart.QuestionType.SINGLE_OPTION
-        ).first()
+        single_option_question_part = (
+            QuestionPart.objects.filter(question=question)
+            .filter(type=QuestionPart.QuestionType.SINGLE_OPTION)
+            .first()
+        )
         if single_option_question_part:
             question_dict["single_option_question_part"] = single_option_question_part
-            # TODO - add counts
+            counts = single_option_question_part.get_option_counts()
+            question_dict["option_counts"] = counts
         questions_list.append(question_dict)
 
     context = {
         "all_questions": questions_list,
         "consultation": consultation,
     }
-    # TODO - do something better if there is no question text, and get it from the question parts
     return render(request, "consultations/consultations/show.html", context)
