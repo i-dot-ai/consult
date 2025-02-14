2b55c91ea2610942b47119153ab24d4eb69774aa
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-02-14 15:22:43 +0000

    Moved the db to aurora

--- infrastructure/postgres.tf
+++ infrastructure/postgres.tf
@@ -1,22 +1,22 @@
 module "rds" {
- # checkov:skip=CKV_TF_1: We're using semantic versions instead of commit hash
- # source                  = "../../i-dot-ai-core-terraform-modules//modules/infrastructure/rds"
- # For testing local changes
- source                  = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/rds?ref=v1.0.0-rds"
- vpc_id                  = data.terraform_remote_state.vpc.outputs.vpc_id
- public_subnet_ids_list  = data.terraform_remote_state.vpc.outputs.public_subnets
- private_subnet_ids_list = data.terraform_remote_state.vpc.outputs.private_subnets
- instance_type = "db.t3.large"
- # Hardcoded because the old db name is consultations, a migration is needed to change
- db_name                 = "consultations"
- kms_secrets_arn         = data.terraform_remote_state.platform.outputs.kms_key_arn
- name                    = local.name
- domain_name             = var.domain_name
- state_bucket            = var.state_bucket
- service_sg_ids = [module.ecs.ecs_sg_id]
- publicly_accessible     = var.publicly_accessible
- securelist_ips = concat(var.developer_ips, var.internal_ips)
- secret_tags = {
-  "platform:secret-purpose" : "general"
- }
+    source = "../../i-dot-ai-core-terraform-modules//modules/infrastructure/rds"  # For testing local changes
+  # source                 = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/rds?ref=v3.4.0-rds"
+  db_name                = "consultations"
+  kms_secrets_arn        = data.terraform_remote_state.platform.outputs.kms_key_arn
+  name                   = local.name
+  public_subnet_ids_list = data.terraform_remote_state.vpc.outputs.public_subnets
+  securelist_ips = toset(var.developer_ips)
+  service_sg_ids = [
+    module.ecs.ecs_sg_id,
+    module.worker.ecs_sg_id
+  ]
+  vpc_id                 = data.terraform_remote_state.vpc.outputs.vpc_id
+  engine                 = "aurora-postgresql"
+  engine_version         = "16.3"
+  family                 = null
+  engine_mode            = "provisioned"
+  aurora_min_scaling     = 0.5
+  aurora_max_scaling     = 1
+  aurora_instance_count  = 1
+  deletion_protection    = var.env == "dev" ? false : true
 }
