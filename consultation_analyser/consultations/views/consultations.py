b4fb55e7f28dfa3c4be277fa658b1b71f1a25b35
Nina Menezes <nina.menezes@ai.gov.uk>
2025-02-17 18:13:42 +0000

    update tests

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -135,7 +135,7 @@ def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
         all_questions.append(question_dict)
 
     # What is the right number per page?
-    pagination = Paginator(all_questions, 5)
+    pagination = Paginator(all_questions, 2)
     page_index = request.GET.get("page", "1")
     current_page = pagination.page(page_index)
     paginated_questions = current_page.object_list
