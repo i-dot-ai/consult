81a2c399f4a8c058959ec10ef637dc832f8997c8
Nina Menezes <nina.menezes@ai.gov.uk>
2025-03-03 14:28:49 +0000

    remove second declaration

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -14,7 +14,6 @@ locals {
     "DOMAIN_NAME"                          = "${local.host}",
     "GIT_SHA"                              = var.image_tag,
     "APP_BUCKET"                           = local.secret_env_vars.APP_BUCKET,
-    "AWS_BUCKET_NAME"                      = "${var.team_name}-${terraform.workspace}-consultations-data",
   }
 
   batch_env_vars = merge(local.base_env_vars, {
