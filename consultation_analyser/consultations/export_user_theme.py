c14a7ed7dd09efe14cb38522286e8baf12e5bd4c
Nina Menezes <nina.menezes@ai.gov.uk>
2025-03-13 08:17:46 +0000

    refactor and get sentiment

--- consultation_analyser/consultations/export_user_theme.py
+++ consultation_analyser/consultations/export_user_theme.py
@@ -13,6 +13,7 @@ from consultation_analyser.consultations.models import (
     Consultation,
     ExecutionRun,
     QuestionPart,
+    SentimentMapping,
     ThemeMapping,
 )
 
@@ -27,15 +28,60 @@ def get_timestamp() -> str:
 def get_latest_sentiment_execution_run_for_question_part(
     question_part: QuestionPart,
 ) -> ExecutionRun | None:
-    sentiment_mapping_execution_runs = ExecutionRun.objects.filter(
-        type=ExecutionRun.TaskType.SENTIMENT_ANALYSIS
+    sentiment_qs = SentimentMapping.objects.select_related("execution_run").filter(
+        answer__question_part=question_part,
+        execution_run__type=ExecutionRun.TaskType.SENTIMENT_ANALYSIS,
     )
-    execution_runs_for_question_part = sentiment_mapping_execution_runs.objects.filter(
-        sentiment_mapping__answer__question_part=question_part
-    )
-    if execution_runs_for_question_part:
-        return execution_runs_for_question_part.order_by("created_at").last()
+    execution_run_ids = sentiment_qs.values_list("execution_run", flat=True)
+    execution_runs = ExecutionRun.objects.filter(id__in=execution_run_ids)
+
+    if execution_runs:
+        return execution_runs.order_by("created_at").last()
     return None
+
+
+def get_position(answer: Answer, execution_run: ExecutionRun) -> str | None:
+    sentiment = SentimentMapping.objects.filter(answer=answer, execution_run=execution_run)
+    if sentiment:
+        # There will only be one
+        return sentiment.first().position
+    return None
+
+
+def get_output_row(response: Answer, sentiment_execution_run: ExecutionRun) -> dict:
+    question_part = response.question_part
+    question = question_part.question
+    consultation_title = question.consultation.title
+
+    position = get_position(answer=response, execution_run=sentiment_execution_run)
+    original_themes = (
+        ThemeMapping.history.filter(answer=response)
+        .filter(user_audited=False)
+        .filter(history_type="+")
+    )
+    current_themes = ThemeMapping.objects.filter(answer=response).filter(user_audited=True)
+    auditors = set(
+        [r.history_user.email for r in response.history.filter(is_theme_mapping_audited=True)]
+    )
+    row_data = {
+        "Response ID": response.respondent.themefinder_respondent_id,
+        "Consultation": consultation_title,
+        "Question number": question.number,
+        "Question text": question.text,
+        "Question part text": question_part.text,
+        "Response text": response.text,
+        "Response has been audited": response.is_theme_mapping_audited,
+        "Original themes": ", ".join(
+            sorted([theme_mapping.theme.get_identifier() for theme_mapping in original_themes])
+        ),
+        "Current themes": ", ".join(
+            sorted([theme_mapping.theme.get_identifier() for theme_mapping in current_themes])
+        ),
+        "Position": position,
+        "Auditors": ", ".join(list(auditors)),
+        "First audited at": response.datetime_theme_mapping_audited,  # First time audited
+    }
+    return row_data
 
 
 @job("default")
@@ -47,50 +93,13 @@ def export_user_theme(consultation_slug: str, s3_key: str) -> None:
         question__consultation=consultation,
         type=QuestionPart.QuestionType.FREE_TEXT,
     ):
-        question = question_part.question
-        answer_qs = Answer.objects.filter(question_part=question_part)
-        for response in answer_qs:
-            original_themes = (
-                ThemeMapping.history.filter(answer=response)
-                .filter(user_audited=False)
-                .filter(history_type="+")
-            )
-            current_themes = ThemeMapping.objects.filter(answer=response).filter(user_audited=True)
-            auditors = set(
-                [
-                    r.history_user.email
-                    for r in response.history.filter(is_theme_mapping_audited=True)
-                ]
-            )
-            output.append(
-                {
-                    "Response ID": response.respondent.themefinder_respondent_id,
-                    "Consultation": consultation.title,
-                    "Question number": question.number,
-                    "Question text": question.text,
-                    "Question part text": question_part.text,
-                    "Response text": response.text,
-                    "Response has been audited": response.is_theme_mapping_audited,
-                    "Original themes": ", ".join(
-                        sorted(
-                            [
-                                theme_mapping.theme.get_identifier()
-                                for theme_mapping in original_themes
-                            ]
-                        )
-                    ),
-                    "Current themes": ", ".join(
-                        sorted(
-                            [
-                                theme_mapping.theme.get_identifier()
-                                for theme_mapping in current_themes
-                            ]
-                        )
-                    ),
-                    "Auditors": ", ".join(list(auditors)),
-                    "First audited at": response.datetime_theme_mapping_audited,  # First time audited
-                }
-            )
+        # Default to latest execution run
+        sentiment_run = get_latest_sentiment_execution_run_for_question_part(question_part)
+        if sentiment_run:
+            answer_qs = Answer.objects.filter(question_part=question_part)
+            for response in answer_qs:
+                row = get_output_row(response, sentiment_run)
+                output.append(row)
 
     timestamp = get_timestamp()
 
