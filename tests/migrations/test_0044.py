a7e67af5b39d8fedef477ca3c3a2275f48492b84
Nina Menezes <nina.menezes@ai.gov.uk>
2025-03-23 07:04:05 +0000

    Add comments

--- tests/migrations/test_0044.py
+++ tests/migrations/test_0044.py
@@ -6,7 +6,7 @@ def test_0044_delete_duplicate_theme_mappings(migrator):
     old_state = migrator.apply_initial_migration(
         ("consultations", "0043_convert_jsonfield_data_to_json")
     )
-
+    # Set-up old models
     Consultation = old_state.apps.get_model("consultations", "Consultation")
     Question = old_state.apps.get_model("consultations", "Question")
     QuestionPart = old_state.apps.get_model("consultations", "QuestionPart")
@@ -17,10 +17,10 @@ def test_0044_delete_duplicate_theme_mappings(migrator):
     Framework = old_state.apps.get_model("consultations", "Framework")
     ExecutionRun = old_state.apps.get_model("consultations", "ExecutionRun")
 
+    # Set-up example consultation
     consultation = Consultation.objects.create(title="Consultation")
     respondent = Respondent.objects.create(consultation=consultation)
     execution_run = ExecutionRun.objects.create(type="theme_mapping")
-
     question = Question.objects.create(consultation=consultation)
     qp = QuestionPart.objects.create(type="free_text", question=question)
     framework = Framework.objects.create(execution_run=execution_run, question_part=qp)
@@ -29,18 +29,22 @@ def test_0044_delete_duplicate_theme_mappings(migrator):
     theme1 = Theme.objects.create(name="Theme 1", framework=framework)
     theme2 = Theme.objects.create(name="Theme 2", framework=framework)
 
+    # Set-up theme mappings with some duplicates
     tm1 = ThemeMapping.objects.create(answer=answer1, theme=theme1)
     duplicate0 = ThemeMapping.objects.create(answer=answer1, theme=theme2)
     duplicate1 = ThemeMapping.objects.create(answer=answer1, theme=theme2)
     duplicate2 = ThemeMapping.objects.create(answer=answer1, theme=theme2)
     tm2 = ThemeMapping.objects.create(answer=answer2, theme=theme1)
 
+    # Run migration
     new_state = migrator.apply_tested_migration(
         ("consultations", "0044_delete_duplicate_theme_mappings")
     )
 
     ThemeMapping = new_state.apps.get_model("consultations", "ThemeMapping")
 
+    # Check that non-duplicates, and the first duplicate, are kept
+    # Delete subsequent duplicates
     assert ThemeMapping.objects.count() == 3
     assert ThemeMapping.objects.filter(id=tm1.id).exists()
     assert ThemeMapping.objects.filter(id=duplicate0.id).exists()
