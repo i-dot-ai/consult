3184045894ae6fe2512f27ce227bb8d6f09a90a7
Nina Menezes <nina.menezes@ai.gov.uk>
2025-03-23 07:58:39 +0000

    formatting

--- consultation_analyser/consultations/migrations/0045_thememapping_unique_theme_answer.py
+++ consultation_analyser/consultations/migrations/0045_thememapping_unique_theme_answer.py
@@ -4,7 +4,6 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         ("consultations", "0044_delete_duplicate_theme_mappings"),
     ]
