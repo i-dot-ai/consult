72de199de367a36b7b4fa8a580230dcc6549462b
Nina Menezes <nina.menezes@ai.gov.uk>
2025-04-08 21:40:27 +0100

    Remove old import of themefinder data.

--- consultation_analyser/support_console/views/consultations.py
+++ consultation_analyser/support_console/views/consultations.py
@@ -12,7 +12,7 @@ from consultation_analyser.consultations.dummy_data import create_dummy_consulta
 from consultation_analyser.consultations.export_user_theme import export_user_theme_job
 from consultation_analyser.hosting_environment import HostingEnvironment
 from consultation_analyser.support_console.export_url_guidance import get_urls_for_consultation
-from consultation_analyser.support_console.ingest import import_themefinder_data_for_question_job, get_all_question_subfolders, import_for_question_part
+from consultation_analyser.support_console.ingest import get_all_question_subfolders, import_for_question_part
 
 
 logger = logging.getLogger("export")
@@ -95,39 +95,6 @@ def export_consultation_theme_audit(request: HttpRequest, consultation_id: UUID)
         return redirect("/support/consultations/")
 
     return render(request, "support_console/consultations/export_audit.html", context)
-
-
-def import_theme_mapping(request: HttpRequest) -> HttpResponse:
-    current_user = request.user
-    if request.POST:
-        consultation_slug = request.POST.get("consultation_slug")
-        consultation_name = request.POST.get("consultation_name")
-        path_to_outputs = request.POST.get("path")
-        if consultation_slug:
-            consultation = models.Consultation.objects.get(slug=consultation_slug)
-            if consultation_name:
-                consultation.title = consultation_name
-                consultation.save()
-        else:
-            if not consultation_name:
-                consultation_name = "New Consultation"
-            consultation = models.Consultation.objects.create(title=consultation_name)
-        consultation.users.add(current_user)
-        consultation.save()
-
-        question_numbers = models.Question.objects.filter(consultation=consultation).values_list(
-            "number", flat=True
-        )
-        max_existing = max(question_numbers) if question_numbers else 0
-        next_question_number = max_existing + 1
-        import_themefinder_data_for_question_job.delay(
-            consultation=consultation,
-            question_number=next_question_number,
-            question_folder=path_to_outputs,
-        )
-        return redirect("/support/consultations/")
-    context = {"bucket_name": settings.AWS_BUCKET_NAME}
-    return render(request, "support_console/consultations/import.html", context=context)
 
 
 def export_urls_for_consultation(request: HttpRequest, consultation_id: UUID) -> HttpResponse:
