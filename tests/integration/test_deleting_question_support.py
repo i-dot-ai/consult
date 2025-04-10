f619e48a024f7b6ace001135945d79113db669b7
Nina Menezes <nina.menezes@ai.gov.uk>
2025-04-10 15:38:58 +0100

    Don't delete questions, delete question part.

--- tests/integration/test_deleting_question_support.py
+++ tests/integration/test_deleting_question_support.py
@@ -6,7 +6,7 @@ from tests.helpers import sign_in
 
 
 @pytest.mark.django_db
-def test_deleting_consultation_questions_via_support(django_app):
+def test_deleting_consultation_question_parts_via_support(django_app):
     question_part = factories.FreeTextQuestionPartFactory()
     question = question_part.question
     consultation = question.consultation
@@ -21,12 +21,14 @@ def test_deleting_consultation_questions_via_support(django_app):
     consultations_page = django_app.get(f"/support/consultations/{consultation.id}/")
 
     # Click delete question
-    delete_confirmation_page = consultations_page.click("Delete this question")
+    delete_confirmation_page = consultations_page.click("Delete this question part")
 
     # Confirm the delete question page URL is correct
-    expected_url = f"/support/consultations/{consultation.id}/questions/{question.id}/delete/"
+    expected_url = (
+        f"/support/consultations/{consultation.id}/question-parts/{question_part.id}/delete/"
+    )
     assert delete_confirmation_page.request.path == expected_url
-    assert "Are you sure you want to delete the question" in delete_confirmation_page
+    assert "Are you sure" in delete_confirmation_page
 
     # Cancel deletion
     delete_confirmation_page.form.submit("cancel_deletion")
