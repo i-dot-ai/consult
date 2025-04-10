79886698cc27f937dba1189594069c46cb3aa765
Nina Menezes <nina.menezes@ai.gov.uk>
2025-04-10 07:29:23 +0100

    update tests

--- tests/unit/test_ingest.py
+++ tests/unit/test_ingest.py
@@ -13,20 +13,8 @@ from consultation_analyser.support_console.ingest import (
     import_question_part_data,
     import_respondent_data,
     import_responses,
+    import_all_respondents_from_jsonl
 )
-
-# TODO - keep for a while to see how to test reading from bucket
-# def test_get_themefinder_outputs_for_question(mock_s3_objects, monkeypatch):
-#     monkeypatch.setattr(settings, "AWS_BUCKET_NAME", "test-bucket")
-#     outputs = get_themefinder_outputs_for_question("folder/question_0/", "question")
-#     assert outputs.get("question") == "What do you think?"
-#     outputs = get_themefinder_outputs_for_question("folder/question_0/", "updated_mapping")
-#     assert outputs[1].get("response_id") == 1
-#     assert outputs[1].get("labels") == ["C", "D"]
-#     outputs = get_themefinder_outputs_for_question("folder/question_0/", "themes")
-#     assert len(outputs) == 5
-#     outputs = get_themefinder_outputs_for_question("folder/question_1/", "themes")
-#     assert len(outputs) == 3
 
 
 @pytest.mark.django_db
@@ -91,10 +79,8 @@ def test_import_question_part_data():
 def test_import_responses():
     question_part = factories.FreeTextQuestionPartFactory()
     consultation = question_part.question.consultation
-    Respondent.objects.create(consultation=consultation, themefinder_respondent_id=1)
-
-    # Not every respondent will respond to every q
-    # Some respondents will already exist, some will be created
+    for i in range(1, 5):
+        factories.RespondentFactory(consultation=consultation, themefinder_respondent_id=i)
     responses_data = [
         b'{"themefinder_id":1, "response":"response 1"}',
         b'{"themefinder_id":2, "response":"response 2"}',
@@ -102,14 +88,11 @@ def test_import_responses():
     ]
 
     import_responses(question_part=question_part, responses_data=responses_data)
-    assert Respondent.objects.all().count() == 3
     assert Answer.objects.all().count() == 3
-
     response = Answer.objects.get(
         question_part=question_part, respondent__themefinder_respondent_id=1
     )
     assert response.text == "response 1"
-
     response = Answer.objects.get(
         question_part=question_part, respondent__themefinder_respondent_id=2
     )
@@ -121,7 +104,7 @@ def test_import_all_responses_from_jsonl(mock_consultation_input_objects):
     question = factories.QuestionFactory(number=3)
     question_part = factories.FreeTextQuestionPartFactory(question=question, number=1)
     consultation = question.consultation
-    for i in range(5):
+    for i in range(1, 6):
         factories.RespondentFactory(consultation=consultation, themefinder_respondent_id=i)
     import_all_responses_from_jsonl(
         question_part,
@@ -154,3 +137,21 @@ def test_import_respondent_data():
     assert respondents.count() == 3
     assert respondents[0].themefinder_respondent_id == 1
     assert respondents[2].themefinder_respondent_id == 3
+
+
+
+@pytest.mark.django_db
+def test_import_all_respondents_from_jsonl(mock_consultation_input_objects):
+    consultation = factories.ConsultationFactory()
+    import_all_respondents_from_jsonl(
+        consultation=consultation,
+        bucket_name="test-bucket",
+        inputs_folder_key="app_data/CON1/inputs/",
+        batch_size=2,
+    )
+    # TODO - improve this, but it works for now!
+    time.sleep(5)
+    respondents = Respondent.objects.filter(consultation=consultation).order_by("themefinder_respondent_id")
+    assert respondents.count() == 5
+    assert respondents[0].themefinder_respondent_id == 1
+    assert respondents[4].themefinder_respondent_id == 5
