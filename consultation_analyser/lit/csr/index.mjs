b977ae10558a28c5df91b346e6785caee321f150
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-04-17 15:18:31 +0100

    created radio input component

--- consultation_analyser/lit/csr/index.mjs
+++ consultation_analyser/lit/csr/index.mjs
@@ -3,3 +3,4 @@ import IaiExpandingText from "./IaiExpandingText/iai-expanding-text.lit.csr.mjs"
 import IaiTextWithFallback from "./IaiTextWithFallback/iai-text-with-fallback.lit.csr.mjs";
 import IaiDataTable from "./IaiDataTable/iai-data-table.lit.csr.mjs";
 import IaiCsvDownload from "./IaiCsvDownload/iai-csv-download.lit.csr.mjs";
+import IaiRadioInput from "./filters/IaiRadioInput/iai-radio-input.lit.csr.mjs";
