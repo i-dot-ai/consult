c1c612bf21ef3fa2680ff9c83de10089c0e6a4c8
Nina Menezes <nina.menezes@ai.gov.uk>
2025-04-30 15:50:50 +0100

    formatting

--- tests/migrations/test_0003_dash_access_group.py
+++ tests/migrations/test_0003_dash_access_group.py
@@ -4,16 +4,12 @@ import pytest
 @pytest.mark.django_db()
 def test_0003_dashboard_permission(migrator):
     # Check group doesn't exist before migration
-    old_state = migrator.apply_initial_migration(
-        ("authentication", "0002_insert_user")
-    )
+    old_state = migrator.apply_initial_migration(("authentication", "0002_insert_user"))
     Group = old_state.apps.get_model("auth", "Group")
     assert not Group.objects.filter(name="Dashboard access").exists()
 
     # Run the migration that creates the group
-    new_state = migrator.apply_tested_migration(
-        ("authentication", "0003_dashboard_permission")
-    )
+    new_state = migrator.apply_tested_migration(("authentication", "0003_dashboard_permission"))
 
     # Verify that the group is created successfully
     Group = new_state.apps.get_model("auth", "Group")
