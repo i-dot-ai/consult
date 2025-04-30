8e65f98b90b4f78c01db9bac437bf8072de34c51
Nina Menezes <nina.menezes@ai.gov.uk>
2025-04-30 22:12:26 +0100

    draft tests

---
+++ tests/views/test_dashboard_access.py
@@ -0,0 +1,12 @@
+# Case 1
+# Not logged in
+
+# Case 2
+# Logged in, has dashboard access, but not consultation access
+
+# Case 3
+# Logged in, has consultation access but not dashboard access
+
+
+# Case 4
+# Logged in, consultation access and dashboard access
