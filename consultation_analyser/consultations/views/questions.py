0d46b7a49bf96d9e6947a19f15114ed8c8f2f982
Nina Menezes <nina.menezes@ai.gov.uk>
2025-05-01 15:46:02 +0100

    just display questions with themes mapped

--- consultation_analyser/consultations/views/questions.py
+++ consultation_analyser/consultations/views/questions.py
@@ -61,9 +61,13 @@ def index(request, consultation_slug: str):
     question_parts = models.QuestionPart.objects.filter(
         question__consultation=consultation, type=models.QuestionPart.QuestionType.FREE_TEXT
     )
+    question_parts_with_themes = [
+        q
+        for q in question_parts
+        if models.ThemeMapping.get_latest_theme_mappings(question_part=q).exists()
+    ]
     context = {
         "consultation": consultation,
-        "question_parts": question_parts,
+        "question_parts": question_parts_with_themes,
     }
-    print(f"context:{context}")
     return render(request, "consultations/questions/index.html", context)
