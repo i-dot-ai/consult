747c6652fff765ad0e23bbbbc597e5b0cd42d764
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-05-07 16:20:31 +0100

    added search hightlighting

--- consultation_analyser/lit/csr/questionsArchive/IaiQuestionBody/iai-question-body.lit.csr.mjs
+++ consultation_analyser/lit/csr/questionsArchive/IaiQuestionBody/iai-question-body.lit.csr.mjs
@@ -1,11 +1,13 @@
 import { html, css } from 'lit';
 import IaiLitBase from '../../../IaiLitBase.mjs';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 
 
 export default class IaiQuestionBody extends IaiLitBase {
     static properties = {
         ...IaiLitBase.properties,
         text: { type: String },
+        searchValue: { type: String },
     }
 
     static styles = [
@@ -17,6 +19,9 @@ export default class IaiQuestionBody extends IaiLitBase {
                 margin-block: 0;
                 min-height: 6em;
             }
+            iai-question-body .matched-text {
+                background-color: var(--iai-colour-focus);
+            }
         `
     ]
 
@@ -26,13 +31,19 @@ export default class IaiQuestionBody extends IaiLitBase {
 
         // Prop defaults
         this.text = "";
+        this.searchValue = "";
         
         this.applyStaticStyles("iai-question-body", IaiQuestionBody.styles);
+    }
+
+    getHighlightedText = (fullText, matchedText) => {
+        const regex = new RegExp(matchedText, "gi");
+        return unsafeHTML(fullText.replace(regex, match => `<span class="matched-text">${match}</span>`));
     }
     
     render() {
         return html`
-            <p>${this.text}</p>
+            <p>${this.getHighlightedText(this.text, this.searchValue)}</p>
         `;
     }
 }
