176416c4f8cab0df2863b5e846c78430b08164b8
Nina Menezes <nina.menezes@ai.gov.uk>
2025-05-08 07:47:18 +0100

    Add tests for new property

---
+++ tests/unit/models/test_user.py
@@ -0,0 +1,15 @@
+import pytest
+from django.contrib.auth.models import Group
+
+from consultation_analyser.constants import DASHBOARD_ACCESS
+from consultation_analyser.factories import UserFactory
+
+
+@pytest.mark.django_db
+def test_user_has_dashboard_access():
+    user = UserFactory()
+    assert not user.has_dashboard_access
+    dash_access = Group.objects.get(name=DASHBOARD_ACCESS)
+    user.groups.add(dash_access)
+    user.save()
+    assert user.has_dashboard_access
