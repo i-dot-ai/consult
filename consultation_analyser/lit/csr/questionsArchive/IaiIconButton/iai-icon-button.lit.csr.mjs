4804c6bd4d5aa44a81c9f0d1c57105462de366c5
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-05-12 21:00:07 +0100

    fixed unintentional form submission

--- consultation_analyser/lit/csr/questionsArchive/IaiIconButton/iai-icon-button.lit.csr.mjs
+++ consultation_analyser/lit/csr/questionsArchive/IaiIconButton/iai-icon-button.lit.csr.mjs
@@ -54,6 +54,7 @@ export default class IaiIconButton extends IaiLitBase {
     render() {
         return html`
             <button
+                type="button"
                 title=${this.title}
                 @click=${this.handleClick}
             >
