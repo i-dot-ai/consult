fa6c237c3a8bcebc4f0429ac71604f61c3100d3f
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-05-23 14:36:51 +0100

    added animated number component

---
+++ consultation_analyser/lit/csr/IaiAnimatedNumber/iai-animated-number.lit.csr.mjs
@@ -0,0 +1,61 @@
+import { html, css } from 'lit';
+
+import IaiLitBase from '../../IaiLitBase.mjs';
+
+
+export default class IaiAnimatedNumber extends IaiLitBase {
+    static properties = {
+        ...IaiLitBase.properties,
+        number: { type: Number },
+        duration: { type: Number }, // miliseconds
+        _displayNumber: { type: Number },
+    }
+
+    static styles = [
+        IaiLitBase.styles,
+        css``
+    ]
+
+    constructor() {
+        super();
+        this.contentId = this.generateId();
+
+        // Prop defaults
+        this.number = 0;
+        this.duration = 1000;
+        this._displayNumber = 0;
+        
+        this.applyStaticStyles("iai-animated-number", IaiAnimatedNumber.styles);
+    }
+
+    animate(start, end, duration) {
+        const element = this.querySelector("span");
+        const startTime = performance.now();
+
+        function update_number(currTime) {
+            const elapsedTime = currTime - startTime;
+            const time = Math.min(elapsedTime / duration, 1);
+            const currValue = start * (1 - time) + end * time;
+            element.textContent = currValue.toFixed(2);
+
+            if (time < 1) {
+                requestAnimationFrame(update_number);
+            }
+        }
+
+        requestAnimationFrame(update_number);
+    }
+
+    updated(changedProps) {
+        if (changedProps.has("number")) {
+            this.animate(this._displayNumber, this.number, this.duration);
+        }
+    }
+
+    render() {
+        return html`
+            <span>${this._displayNumber}</span>
+        `;
+    }
+}
+customElements.define("iai-animated-number", IaiAnimatedNumber);
