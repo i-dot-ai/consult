a63c79d3b4a9397dde5e8967eade05f85cff5682
Nina Menezes <nina.menezes@ai.gov.uk>
2025-05-29 14:37:33 +0100

    make it clear we're clearing the default django cache

--- consultation_analyser/support_console/views/cache.py
+++ consultation_analyser/support_console/views/cache.py
@@ -2,12 +2,6 @@ from django.contrib import messages
 from django.core.cache import cache
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
-from django_rq import job
-
-
-@job("default", timeout=900)
-def delete_cache_job():
-    cache.clear()
 
 
 def show(request: HttpRequest) -> HttpResponse:
@@ -19,10 +13,8 @@ def show(request: HttpRequest) -> HttpResponse:
 def delete(request: HttpRequest) -> HttpResponse:
     if request.POST:
         if "confirm_deletion" in request.POST:
-            delete_cache_job.delay()
-            messages.success(
-                request,
-                "The consultation has been sent for deletion - check queue dashboard for progress",
-            )
+            # Note this clears the default cache only
+            cache.clear()
+            messages.success(request, "The default cache has been deleted")
             return redirect("support")
     return render(request, "support_console/cache/delete.html")
