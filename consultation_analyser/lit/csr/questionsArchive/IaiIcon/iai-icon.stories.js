abf34a7eca952c22bc11517f0c985c031f34669b
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-06-10 15:07:09 +0100

    added icon stories

---
+++ consultation_analyser/lit/csr/questionsArchive/IaiIcon/iai-icon.stories.js
@@ -0,0 +1,40 @@
+import { html } from 'lit';
+
+import IaiIcon from './iai-icon.mjs';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+export default {
+  title: 'Csr/Icon',
+  tags: ['autodocs'],
+  argTypes: {
+    name: {
+      control: { type: "select" },
+      options: ["visibility", "close", "star", "search", "thumb_up", "thumb_down", "thumbs_up_down", "arrow_drop_down_circle", "download", "diamond", "progress_activity", "sort"],
+    },
+    color: {
+      control: { type: "color" },
+    }
+  },
+  render: (args) => {
+    return html`
+      <iai-icon
+        .name=${args.name}
+        .opsz=${args.opsz}
+        .color=${args.color}
+        .fill=${args.fill}
+        .wght=${args.wght}
+      ></iai-icon>
+    `
+  },
+};
+
+// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
+export const Default = {
+  args: {
+    name: "thumb_up",
+    opsz: 12,
+    color: "red",
+    fill: true,
+    wght: 300,
+  },
+};
