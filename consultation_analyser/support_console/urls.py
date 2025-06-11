b7459754febda2a296651120a0eb708cefeb6418
Alex Moore <alex.moore@cabinetoffice.gov.uk>
2025-06-11 08:44:32 +0100

    Remove: legacy multistage import process.

--- consultation_analyser/support_console/urls.py
+++ consultation_analyser/support_console/urls.py
@@ -48,21 +48,6 @@ urlpatterns = [
         name="delete_question_part",
     ),
     path(
-        "consultations/import-respondents/",
-        consultations.import_consultation_respondents,
-        name="import_respondents",
-    ),
-    path(
-        "consultations/import-inputs/",
-        consultations.import_consultation_inputs,
-        name="import_inputs",
-    ),
-    path(
-        "consultations/import-themes/",
-        consultations.import_consultation_themes,
-        name="import_themes",
-    ),
-    path(
         "consultations/import-summary/",
         consultations.import_summary,
         name="import_summary",
