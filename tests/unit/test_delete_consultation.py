cb47a3501a4ddf5d6b7c94cfc76b5de1f01e3e41
Nina Menezes <nina.menezes@ai.gov.uk>
2025-06-11 22:03:36 +0100

    update test

--- tests/unit/test_delete_consultation.py
+++ tests/unit/test_delete_consultation.py
@@ -4,24 +4,21 @@ from consultation_analyser import factories
 from consultation_analyser.consultations import models
 
 
-@pytest.mark.skip(reason="Doesn't work whilst in the middle of model changes")
 @pytest.mark.django_db
 def test_delete_consultation():
     consultation = factories.ConsultationFactory()
     question = factories.QuestionFactory(consultation=consultation)
-    question_part = factories.FreeTextQuestionPartFactory(question=question)
     respondent = factories.RespondentFactory(consultation=consultation)
-    factories.FreeTextAnswerFactory(question_part=question_part, respondent=respondent)
+    factories.ResponseFactory(respondent=respondent, question=question)
 
-    assert models.ConsultationOld.objects.count() == 1
-    assert models.RespondentOld.objects.count() >= 1
-    assert models.QuestionOld.objects.count() >= 1
-    assert models.QuestionPart.objects.count() >= 1
-    assert models.Answer.objects.count() >= 1
+    assert models.Consultation.objects.count() == 1
+    assert models.Respondent.objects.count() == 1
+    assert models.Question.objects.count() == 1
+    assert models.Response.objects.count() == 1
 
     consultation.delete()
 
-    assert models.ConsultationOld.objects.count() == 0
-    assert models.RespondentOld.objects.count() == 0
-    assert models.QuestionPart.objects.count() == 0
-    assert models.Answer.objects.count() == 0
+    assert models.Consultation.objects.count() == 0
+    assert models.Respondent.objects.count() == 0
+    assert models.Question.objects.count() == 0
+    assert models.Response.objects.count() == 0
