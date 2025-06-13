24e8918fef8f361fcde2e3f10903cb5c7cf2ba7b
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-06-13 14:39:25 +0100

    added tests to stories

--- consultation_analyser/lit/csr/IaiProgressBar/iai-progress-bar.stories.js
+++ consultation_analyser/lit/csr/IaiProgressBar/iai-progress-bar.stories.js
@@ -1,5 +1,6 @@
 import { html } from 'lit';
 import IaiProgressBar from './iai-progress-bar.lit.csr.mjs';
+import { expect, within } from '@storybook/test';
 
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
@@ -22,6 +23,14 @@ export const FullBar = {
   args: {
     value: 100,
     label: "100",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const barElement = canvasElement.querySelector(".bar");
+    expect(barElement.style.width).toBe("100%");
+
+    const labelElement = canvas.getByText("100");
+    expect(labelElement).toBeInTheDocument();
   }
 };
 
@@ -29,6 +38,14 @@ export const EmptyBar = {
   args: {
     value: 0,
     label: "0",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const barElement = canvasElement.querySelector(".bar");
+    expect(barElement.style.width).toBe("0%");
+
+    const labelElement = canvas.getByText("0");
+    expect(labelElement).toBeInTheDocument();
   }
 };
 
@@ -36,6 +53,17 @@ export const LowBar = {
   args: {
     value: 20,
     label: "20",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const barElement = canvasElement.querySelector(".bar");
+    expect(barElement.style.width).toBe("20%");
+
+    const labelElement = canvas.getByText("20");
+    expect(labelElement).toBeInTheDocument();
+
+    const lowValueElement = canvasElement.querySelector(".low-value");
+    expect(lowValueElement).toBeInTheDocument();
   }
 };
 
@@ -43,6 +71,17 @@ export const HighBar = {
   args: {
     value: 90,
     label: "90",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const barElement = canvasElement.querySelector(".bar");
+    expect(barElement.style.width).toBe("90%");
+
+    const labelElement = canvas.getByText("90");
+    expect(labelElement).toBeInTheDocument();
+
+    const lowValueElement = canvasElement.querySelector(".low-value");
+    expect(lowValueElement).toBe(null);
   }
 };
 
@@ -50,6 +89,14 @@ export const DifferentBar = {
   args: {
     value: 40,
     label: "8000",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const barElement = canvasElement.querySelector(".bar");
+    expect(barElement.style.width).toBe("40%");
+
+    const labelElement = canvas.getByText("8000");
+    expect(labelElement).toBeInTheDocument();
   }
 };
 
@@ -57,5 +104,13 @@ export const StringLabelBar = {
   args: {
     value: 80,
     label: "Test Label",
+  },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const barElement = canvasElement.querySelector(".bar");
+    expect(barElement.style.width).toBe("80%");
+
+    const labelElement = canvas.getByText("Test Label");
+    expect(labelElement).toBeInTheDocument();
   }
 };
