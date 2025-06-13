3bf1b395da9141541c559bb948a0c609e30ee7e0
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-06-13 14:28:45 +0100

    added tests to stories

--- consultation_analyser/lit/csr/IaiResponsesTitle/iai-responses-title.stories.js
+++ consultation_analyser/lit/csr/IaiResponsesTitle/iai-responses-title.stories.js
@@ -1,6 +1,7 @@
 import { html } from 'lit';
 
 import IaiResponsesTitle from './iai-responses-title.lit.csr.mjs';
+import { expect, within } from '@storybook/test';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
 export default {
@@ -20,22 +21,45 @@ export const Default = {
   args: {
     total: 100,
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const numberElement = canvas.getByText("100 responses");
+    expect(numberElement).toBeInTheDocument();
+
+    const titleElement = canvas.getByText("Individual responses");
+    expect(titleElement).toBeInTheDocument();
+  }
 };
 
 export const ZeroTotal = {
   args: {
     total: 0,
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const numberElement = canvas.getByText("0 responses");
+    expect(numberElement).toBeInTheDocument();
+  }
 };
 
 export const LargeTotal = {
   args: {
     total: 100000,
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const numberElement = canvas.getByText("100000 responses");
+    expect(numberElement).toBeInTheDocument();
+  }
 };
 
 export const NegativeTotal = {
   args: {
     total: -10,
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const numberElement = canvas.getByText("-10 responses");
+    expect(numberElement).toBeInTheDocument();
+  }
 };
