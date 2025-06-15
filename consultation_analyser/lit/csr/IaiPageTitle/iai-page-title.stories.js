8e74ab1c57d71d42822bb6d9ae532406f5c4ae8b
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-06-15 11:35:04 +0100

    added tests to stories

--- consultation_analyser/lit/csr/IaiPageTitle/iai-page-title.stories.js
+++ consultation_analyser/lit/csr/IaiPageTitle/iai-page-title.stories.js
@@ -1,5 +1,7 @@
 import { html } from 'lit';
 
+import { within, expect } from '@storybook/test';
+
 import IaiPageTitle from './iai-page-title.lit.csr.mjs';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
@@ -22,6 +24,14 @@ export const Default = {
     title: "Test Title",
     subtitle: "Test Subtitle",
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const titleElement = canvas.getByText("Test Title");
+    expect(titleElement).toBeInTheDocument();
+
+    const subtitleElement = canvas.getByText("Test Subtitle");
+    expect(subtitleElement).toBeInTheDocument();
+  }
 };
 
 export const LongStrings = {
@@ -29,6 +39,14 @@ export const LongStrings = {
     title: "Test Title".repeat(10),
     subtitle: "Test Subtitle".repeat(10),
   },
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    const titleElement = canvas.getByText("Test Title".repeat(10));
+    expect(titleElement).toBeInTheDocument();
+
+    const subtitleElement = canvas.getByText("Test Subtitle".repeat(10));
+    expect(subtitleElement).toBeInTheDocument();
+  }
 };
 
 export const EmptyStrings = {
@@ -36,5 +54,12 @@ export const EmptyStrings = {
     title: "",
     subtitle: "",
   },
+  play: async ({ canvasElement }) => {
+    const titleElement = canvasElement.querySelector("h1");
+    expect(titleElement.innerText).toBe("");
+
+    const subtitleElement = canvasElement.querySelector("h2");
+    expect(subtitleElement.innerText).toBe("");
+  }
 };
 
