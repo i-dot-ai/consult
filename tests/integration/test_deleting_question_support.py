bf282af2e5dcb5a669865bffc09f52c1cdcf3973
Alex Moore <alex.moore@cabinetoffice.gov.uk>
2025-06-17 17:41:42 +0100

    Fix: all tests to work post refactor

--- tests/integration/test_deleting_question_support.py
+++ tests/integration/test_deleting_question_support.py
@@ -1,14 +1,13 @@
 import pytest
 
 from consultation_analyser import factories
-from consultation_analyser.consultations.models import QuestionPart
+from consultation_analyser.consultations.models import Question
 from tests.helpers import sign_in
 
 
 @pytest.mark.django_db
-def test_deleting_consultation_question_parts_via_support(django_app):
-    question_part = factories.FreeTextQuestionPartFactory()
-    question = question_part.question
+def test_deleting_consultation_question_via_support(django_app):
+    question = factories.QuestionFactory()
     consultation = question.consultation
 
     # given I am an admin user
@@ -21,12 +20,10 @@ def test_deleting_consultation_question_parts_via_support(django_app):
     consultations_page = django_app.get(f"/support/consultations/{consultation.id}/")
 
     # Click delete question
-    delete_confirmation_page = consultations_page.click("Delete this question part")
+    delete_confirmation_page = consultations_page.click("Delete this question")
 
     # Confirm the delete question page URL is correct
-    expected_url = (
-        f"/support/consultations/{consultation.id}/question-parts/{question_part.id}/delete/"
-    )
+    expected_url = f"/support/consultations/{consultation.id}/questions/{question.id}/delete/"
     assert delete_confirmation_page.request.path == expected_url
     assert "Are you sure" in delete_confirmation_page
 
@@ -34,7 +31,7 @@ def test_deleting_consultation_question_parts_via_support(django_app):
     delete_confirmation_page.form.submit("cancel_deletion")
 
     # Check question still exists
-    assert QuestionPart.objects.filter(id=question_part.id).count() == 1
+    assert Question.objects.filter(id=question.id).count() == 1
 
     # Go to consultation page in support
     consultations_page = django_app.get(f"/support/consultations/{consultation.id}/")
@@ -46,4 +43,4 @@ def test_deleting_consultation_question_parts_via_support(django_app):
     delete_confirmation_page.form.submit("confirm_deletion")
 
     # Check question deleted
-    assert QuestionPart.objects.filter(id=question.id).count() == 0
+    assert Question.objects.filter(id=question.id).count() == 0
