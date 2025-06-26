7a8ed64dcf15bc2a9dd8a6bd39b17bc6a6c01866
Dheeraj Singh <33820920+dk-singh@users.noreply.github.com>
2025-06-26 14:41:33 +0100

    uncomment sqs terraform module

--- infrastructure/sqs.tf
+++ infrastructure/sqs.tf
@@ -1,6 +1,6 @@
 module "batch_job_queue" {
-  source        = "../../i-dot-ai-core-terraform-modules//modules/infrastructure/sqs" # For testing local changes
-  # source         = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/sqs?ref=v1.0.2-sqs"
+  # source        = "../../i-dot-ai-core-terraform-modules//modules/infrastructure/sqs" # For testing local changes
+  source         = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/sqs?ref=v1.0.2-sqs"
   name           = "${local.name}-batch-job-queue"
   kms_key        = data.terraform_remote_state.platform.outputs.kms_key_arn
   universal_tags = var.universal_tags
