7c1f798e701f98aae7a7521705e37a10bf277c52
Nina Menezes <nina.menezes@ai.gov.uk>
2025-06-26 14:05:30 +0100

    remove existing options data

--- consultation_analyser/consultations/models.py
+++ consultation_analyser/consultations/models.py
@@ -69,9 +69,9 @@ class Question(UUIDPrimaryKeyModel, TimeStampedModel):
     # Question configuration
     has_free_text = models.BooleanField(default=True)
     has_multiple_choice = models.BooleanField(default=False)
-    multiple_choice_options = models.JSONField(
-        null=True, blank=True
-    )  # List of options when has_multiple_choice=True
+    # multiple_choice_options = models.JSONField(
+    #     null=True, blank=True
+    # )  # List of options when has_multiple_choice=True
 
     def save(self, *args, **kwargs):
         if not self.slug:
@@ -136,7 +136,7 @@ class Response(UUIDPrimaryKeyModel, TimeStampedModel):
 
     # Response content
     free_text = models.TextField(blank=True)  # Free text response
-    chosen_options = models.JSONField(default=list)  # Multiple choice selections
+    # chosen_options = models.JSONField(default=list)  # Multiple choice selections
 
     class Meta(UUIDPrimaryKeyModel.Meta, TimeStampedModel.Meta):
         constraints = [
