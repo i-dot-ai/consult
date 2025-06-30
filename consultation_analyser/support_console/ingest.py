84b1df46b2c6857b2bbd4d7295cb2a310e7f1912
gecBurton <g.e.c.burton@gmail.com>
2025-06-30 12:10:23 +0100

    removed hack

--- consultation_analyser/support_console/ingest.py
+++ consultation_analyser/support_console/ingest.py
@@ -362,7 +362,7 @@ def import_questions(
             f"app_data/{consultation_code}/inputs/", settings.AWS_BUCKET_NAME
         )
 
-        for question_folder in question_folders[:4]:  # temp hack
+        for question_folder in question_folders:
             question_num_str = question_folder.split("/")[-2].replace("question_part_", "")
             question_number = int(question_num_str)
 
