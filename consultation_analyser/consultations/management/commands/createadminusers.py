e9c9b5e5cbdd8cae0eefc38e838c1c177a44d373
gecBurton <g.e.c.burton@gmail.com>
2025-07-03 10:18:23 +0100

    simplified logic

--- consultation_analyser/consultations/management/commands/createadminusers.py
+++ consultation_analyser/consultations/management/commands/createadminusers.py
@@ -4,16 +4,12 @@ from django.core.management import BaseCommand
 
 from consultation_analyser.authentication.models import User
 
-
 class Command(BaseCommand):
     help = "creates initial admin users who can add others"
 
     def handle(self, *args, **options):
         for email in os.environ.get("ADMIN_USERS", "").split(","):
-            email = email.strip()
-            try:
-                user = User.objects.get(email=email)
-                user.is_staff = True
-                user.save()
-            except User.DoesNotExist:
-                User.objects.create(email=email, is_staff=True)
+            User.objects.update_or_create(
+                email=email.strip(),
+                defaults={'is_staff': True}
+            )
