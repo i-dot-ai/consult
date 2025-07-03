7c119be1e144333f9528f83e1a507bcc1c9e76f8
gecBurton <g.e.c.burton@gmail.com>
2025-07-03 10:20:13 +0100

    formatting

--- consultation_analyser/consultations/management/commands/createadminusers.py
+++ consultation_analyser/consultations/management/commands/createadminusers.py
@@ -1,12 +1,12 @@
 import os
+from logging import getLogger
 
 from django.core.management import BaseCommand
 
 from consultation_analyser.authentication.models import User
 
-from logging import getLogger
-
 logger = getLogger(__file__)
+
 
 class Command(BaseCommand):
     help = "creates initial admin users who can add others"
@@ -14,8 +14,7 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         for email in os.environ.get("ADMIN_USERS", "").split(","):
             user, created = User.objects.update_or_create(
-                email=email.strip(),
-                defaults={'is_staff': True}
+                email=email.strip(), defaults={"is_staff": True}
             )
             if created:
                 logger.info("created %s as admin", user.email)
