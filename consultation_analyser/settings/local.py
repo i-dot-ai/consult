751b62711e001215885e3cb01e77892d2215b8cd
gecBurton <g.e.c.burton@gmail.com>
2025-07-04 05:58:04 +0100

    formatting

--- consultation_analyser/settings/local.py
+++ consultation_analyser/settings/local.py
@@ -1,7 +1,7 @@
 from django.conf.global_settings import STORAGES
+from langchain_openai import OpenAIEmbeddings
 
 from consultation_analyser.settings.base import *  # noqa
-from langchain_openai import OpenAIEmbeddings
 
 INSTALLED_APPS.append("django_extensions")  # noqa F405
 INSTALLED_APPS.append("pyflame")  # noqa F405
