eca33de10db42a4be5bc6b9d827676b32e374fcb
gecBurton <g.e.c.burton@gmail.com>
2025-07-07 15:53:22 +0100

    added missing envvars

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -16,8 +16,12 @@ locals {
     "APP_BUCKET"                           = local.secret_env_vars.APP_BUCKET,
     "GUNICORN_WORKERS"                     = local.secret_env_vars.GUNICORN_WORKERS,
     "GUNICORN_TIMEOUT"                     = local.secret_env_vars.GUNICORN_TIMEOUT,
-    "ADMIN_USERS"                          = local.secret_env_vars.ADMIN_USERS
-    "SIMILARITY_THRESHOLD"                 = local.secret_env_vars.SIMILARITY_THRESHOLD
+    "ADMIN_USERS"                          = local.secret_env_vars.ADMIN_USERS,
+    "SIMILARITY_THRESHOLD"                 = local.secret_env_vars.SIMILARITY_THRESHOLD,
+    "AZURE_OPENAI_API_KEY"                 = local.secret_env_vars.AZURE_OPENAI_API_KEY,
+    "AZURE_OPENAI_API_KEY"                 = local.secret_env_vars.AZURE_OPENAI_API_KEY,
+    "OPENAI_API_VERSION"                   = local.secret_env_vars.OPENAI_API_VERSION,
+    "AZURE_OPENAI_ENDPOINT"                = local.secret_env_vars.AZURE_OPENAI_ENDPOINT
   }
 
   batch_env_vars = merge(local.base_env_vars, {
