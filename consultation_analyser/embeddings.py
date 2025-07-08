a01b03aae5877fc8e58bbdb3019341e212e14091
gecBurton <g.e.c.burton@gmail.com>
2025-07-08 15:57:42 +0100

    simplified embed_text

--- consultation_analyser/embeddings.py
+++ consultation_analyser/embeddings.py
@@ -14,22 +14,17 @@ def _uniform_vector(txt):
     return [random.uniform(-1, 1) for _ in range(settings.EMBEDDING_DIMENSION)]
 
 
-if hosting_environment.is_deployed():
-    client = AzureOpenAI()
-
-    def embed_text(text: str | list[str]) -> list[float] | list[list[float]]:
+def embed_text(text: str | list[str]) -> list[float] | list[list[float]]:
+    if hosting_environment.is_deployed():
+        client = AzureOpenAI()
         response = client.embeddings.create(
             input=text, model="text-embedding-3-small", dimensions=settings.EMBEDDING_DIMENSION
         )
         if isinstance(text, str):
             return response.data[0].embedding
         return [item.embedding for item in response.data]
-
-else:
-
-    def embed_text(text: str | list[str]) -> list[float] | list[list[float]]:
-        if isinstance(text, str):
-            return _uniform_vector(text)
-        if isinstance(text, list):
-            return list(map(_uniform_vector, text))
-        raise ValueError(f"expected str or list[str] not {type(text)}")
+    if isinstance(text, str):
+        return _uniform_vector(text)
+    if isinstance(text, list):
+        return list(map(_uniform_vector, text))
+    raise ValueError(f"expected str or list[str] not {type(text)}")
