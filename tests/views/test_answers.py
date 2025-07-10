1dfdaebcb7a3cbf6be16fb846500e106fbaf3cb2
gecBurton <g.e.c.burton@gmail.com>
2025-07-10 11:19:50 +0100

    added test

--- tests/views/test_answers.py
+++ tests/views/test_answers.py
@@ -615,6 +615,36 @@ def test_semantic_search(fake_embed_text, consultation, question):
 
 
 @pytest.mark.django_db
+@patch("consultation_analyser.consultations.views.answers.embed_text")
+def test_lexical_search(fake_embed_text, consultation, question):
+    filters = {"search_value": "mary lambs"}
+
+    # we use two orthogonal vectors to ensure that the semantic search has no effect
+    baseline = list(0 for _ in range(settings.EMBEDDING_DIMENSION))
+    baseline[0] = 1
+
+    orthogonal = list(0 for _ in range(settings.EMBEDDING_DIMENSION))
+    orthogonal[1] = 1
+
+    text_1 = "Mary had a little lamb, His fleece was white as snow, And everywhere that Mary went, The lamb was sure to go"
+    text_2 = "He followed her to school one day, Which was against the rule, It made the children laugh and play, To see a lamb at school."
+    text_3 = "And so the teacher turned him out, But still he lingered near, And waited patiently about, Till Mary did appear."
+    text_4 = "What makes the lamb love Mary so? The eager children cry; Why, Mary loves the lamb, you know, The teacher did reply."
+
+
+    ResponseFactory(question=question, free_text=text_1, embedding=baseline)
+    ResponseFactory(question=question, free_text=text_2, embedding=baseline)
+    ResponseFactory(question=question, free_text=text_3, embedding=baseline)
+    ResponseFactory(question=question, free_text=text_4, embedding=baseline)
+
+    fake_embed_text.return_value = orthogonal
+
+    responses = get_filtered_responses_with_themes(question, filters)
+    assert [x.free_text for x in responses] == [text_1, text_4, text_2, text_3]
+    assert [x.distance for x in responses] == [0.9797747898846865, 0.9859406910836697, 1.0, 1.0]
+
+
+@pytest.mark.django_db
 def test_question_responses_json_theme_filtering_and_logic(client, consultation_user, question):
     """Test that the JSON endpoint uses AND logic for theme filtering"""
     # Create themes
