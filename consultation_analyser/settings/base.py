ee1c9dd4b4d6fa3c797796af82a1085f073f09be
gecBurton <g.e.c.burton@gmail.com>
2025-07-18 08:32:29 +0100

    feature/toolkit-in-dev

--- consultation_analyser/settings/base.py
+++ consultation_analyser/settings/base.py
@@ -278,5 +278,23 @@ if DEBUG:
     INSTALLED_APPS += ["debug_toolbar"]
     INTERNAL_IPS = ["127.0.0.1"]
 
+    DEBUG_TOOLBAR_PANELS = [
+        "debug_toolbar.panels.history.HistoryPanel",
+        "debug_toolbar.panels.versions.VersionsPanel",
+        "debug_toolbar.panels.timer.TimerPanel",
+        "debug_toolbar.panels.settings.SettingsPanel",
+        "debug_toolbar.panels.headers.HeadersPanel",
+        "debug_toolbar.panels.request.RequestPanel",
+        "debug_toolbar.panels.sql.SQLPanel",
+        "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+        "debug_toolbar.panels.templates.TemplatesPanel",
+        "debug_toolbar.panels.alerts.AlertsPanel",
+        "debug_toolbar.panels.cache.CachePanel",
+        "debug_toolbar.panels.signals.SignalsPanel",
+        "debug_toolbar.panels.redirects.RedirectsPanel",
+        "debug_toolbar.panels.profiling.ProfilingPanel",
+        "pyflame.djdt.panel.FlamegraphPanel",
+    ]
+
 # changing this will require a database migration
 EMBEDDING_DIMENSION = 1024
