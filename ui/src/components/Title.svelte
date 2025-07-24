04e4f8051d12b3c7206d36e6fb4e03ffa6f426b6
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-07-24 16:55:52 +0100

    created astro ui

---
+++ ui/src/components/Title.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let level: 1 | 2 | 3 | 4 | 5 | 6 = 1;
+    export let text: string = "";
+
+    const tagMap = {
+        1: "h1",
+        2: "h2",
+        3: "h3",
+        4: "h4",
+        5: "h5",
+        6: "h6",
+    } as const;
+
+    const tag = tagMap[level];
+</script>
+
+<svelte:element this={tag} class="font-bold text-2xl">
+    {text}
+</svelte:element>
