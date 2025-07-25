483f249f866b30bdeb726ee1670186660ebedeb5
gecBurton <g.e.c.burton@gmail.com>
2025-07-25 18:07:42 +0100

    drf_orjson_renderer

--- consultation_analyser/settings/base.py
+++ consultation_analyser/settings/base.py
@@ -312,4 +312,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_RENDERER_CLASSES": (
+        "drf_orjson_renderer.renderers.ORJSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ),
 }
