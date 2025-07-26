e10a1eb8e7725b763fb98939bd16a27c192b5b42
gecBurton <g.e.c.burton@gmail.com>
2025-07-26 07:31:00 +0100

    builds as part of ci

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-    output: "server",
+    output: "static",
     integrations: [svelte()],
     vite: {
         plugins: [tailwindcss()],
