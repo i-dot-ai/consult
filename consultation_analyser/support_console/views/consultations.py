3b975f1b467cec4547594ce6a2ea3a09ca16cb2c
gecBurton <g.e.c.burton@gmail.com>
2025-07-29 13:34:43 +0100

    remove unised import

--- consultation_analyser/support_console/views/consultations.py
+++ consultation_analyser/support_console/views/consultations.py
@@ -16,7 +16,6 @@ from consultation_analyser.consultations.dummy_data import (
 from consultation_analyser.consultations.export_user_theme import export_user_theme_job
 from consultation_analyser.hosting_environment import HostingEnvironment
 from consultation_analyser.support_console import ingest
-from consultation_analyser.support_console.validation_utils import format_validation_error
 
 logger = logging.getLogger("export")
 
