d88b5696d470767eae3b55d2ddd3a3cabe530a8f
gecBurton <g.e.c.burton@gmail.com>
2025-08-05 06:47:41 +0100

    reverted to slug

--- consultation_analyser/consultations/views/consultations.py
+++ consultation_analyser/consultations/views/consultations.py
@@ -1,5 +1,3 @@
-from uuid import UUID
-
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
@@ -12,9 +10,9 @@ def index(request: HttpRequest) -> HttpResponse:
 
 @user_can_see_dashboards
 @user_can_see_consultation
-def show(request: HttpRequest, consultation_id: UUID) -> HttpResponse:
-    # Dashboard page - pass consultation_id to template for API calls
+def show(request: HttpRequest, consultation_slug: str) -> HttpResponse:
+    # Dashboard page - pass consultation_slug to template for API calls
     context = {
-        "consultation_id": consultation_id,
+        "consultation_slug": consultation_slug,
     }
     return render(request, "consultations/consultations/show.html", context)
