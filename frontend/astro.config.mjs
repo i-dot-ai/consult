ffbbaf7e1c2fa07da652c0ce03e577b0733fcd08
gecBurton <g.e.c.burton@gmail.com>
2025-08-08 17:45:14 +0100

    bind to all ports

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -9,6 +9,11 @@ import node from "@astrojs/node";
 export default defineConfig({
   output: "server",
   integrations: [svelte(), tailwind({ applyBaseStyles: false })],
+  
+  server: {
+    host: "0.0.0.0",
+    port: 4321,
+  },
 
   adapter: node({
     mode: "standalone",
