c1188f3129d35126c3aca6f79b6cfc30335f3cd7
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-08-08 16:57:41 +0100

    added sign out page and logic

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,44 +1,31 @@
 import type { MiddlewareHandler } from "astro";
-import { getToken } from "./global/auth";
 import { getBackendUrl } from "./global/utils";
 
-// AuthMiddleware
-// TODO: Activate after transfer. Currently disabled.
-// export const onRequest: MiddlewareHandler = async ({request, redirect, url}, next) => {
-//     const pathname = url.pathname;
-
-//     const protectedRoutes = [
-//         "/consultations",
-//     ];
-
-//     const isProtected = protectedRoutes.some(route => {
-//         return pathname === route || pathname.startsWith(`${route}/`);
-//     })
-
-//     if (!isProtected) {
-//         return next();
-//     }
-
-//     const cookie = request.headers.get("cookie") || "";
-//     const token = getToken(cookie);
-
-//     if (!token) {
-//         // // TODO: Activate after auth implementation
-//         // return redirect("/");
-//     }
-//     return next();
-// }
 
 export const onRequest: MiddlewareHandler = async (context, next) => {
     const accessToken = context.cookies.get("access")?.value;
     const url = context.url;
+
+    // 404 if not logged in
+    const protectedRoutes = [
+        /^\/sign-out[\/]?$/,
+        /^\/consultations.*/,
+    ];
+
+    for (const protectedRoute of protectedRoutes) {
+        if (protectedRoute.test(context.url.pathname) && !context.cookies.get("access")?.value) {
+            return new Response(null, { status: 404 });
+        }
+    }
+
     const backendUrl = getBackendUrl(url.hostname);
     const fullBackendUrl = backendUrl + url.pathname + url.search;
 
-    // TODO: add logic to skip as new pages are moved to astro
+    // skip as new pages are moved to astro
     const toSkip = [
         /^\/$/,
         /^\/sign-in[\/]?$/,
+        /^\/sign-out[\/]?$/,
         /^\/magic-link\/[A-Za-z0-9\-]*[\/]?$/,
         /^\/api\/astro\/.*/,
         /^\/.well-known\/.*/,
@@ -64,6 +51,10 @@ export const onRequest: MiddlewareHandler = async (context, next) => {
             ? context.request.body
             : undefined,
     });
+
+    if (response.status === 401) {
+        return context.redirect("/sign-out");
+    }
 
     const body = await response.arrayBuffer();
 
