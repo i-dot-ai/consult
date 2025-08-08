5ca6db0f976ca2e871bb05483a4c7ed66fbf1b3b
gecBurton <g.e.c.burton@gmail.com>
2025-08-08 16:35:50 +0100

    working locally

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,18 +2,12 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
 
-import tailwindcss from "@tailwindcss/vite";
-
 import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [svelte()],
-
-  vite: {
-      plugins: [tailwindcss()],
-  },
 
   adapter: node({
     mode: "standalone",
