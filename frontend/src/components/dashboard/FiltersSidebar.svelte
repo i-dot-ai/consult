509a5519dd62f3e6cc272fe6e8769969c7af2e4e
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-08-15 13:20:35 +0100

    created demo filter component and added expand logic

--- frontend/src/components/dashboard/FiltersSidebar.svelte
+++ frontend/src/components/dashboard/FiltersSidebar.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-    import { slide, fade } from "svelte/transition";
-
     import TitleRow from "./TitleRow.svelte";
     import Panel from "./Panel.svelte";
-    import Button from "../inputs/Button.svelte";
+    import DemoFilter from "./DemoFilter.svelte";
     import Star from "../svg/material/Star.svelte";
-
-    import { getPercentage } from "../../global/utils.ts";
 
     let {
         showEvidenceRich = true,
@@ -33,41 +29,14 @@
         </TitleRow>
 
         {#each Object.keys(demoOptions) as category (category)}
-            <div transition:fade>
-                <Panel level={2} border={true} bg={true}>
-                    <Button variant="ghost" size="sm">
-                        {category}
-                    </Button>
-
-                    {#each demoOptions[category] as rowKey}
-                        {@const rowValue = demoData[category] && demoData[category][rowKey] || 0}
-                        {@const percentage = getPercentage(rowValue, totalCounts[category])}
-
-                        <div class="my-1">
-                            <Button
-                                variant="ghost"
-                                size="xs"
-                                fullWidth={true}
-                                handleClick={() => setDemoFilters(category, rowKey)}
-                                highlighted={demoFilters[category]?.includes(rowKey)}
-                                highlightVariant="light"
-                            >
-                                <div class="demo-filter w-full">
-                                    <div class="grid grid-cols-3 gap-1 mb-1">
-                                        <span class="text-left">{rowKey}</span>
-                                        <span class="text-right">{percentage}%</span>
-                                        <span class="text-right">{rowValue}</span>
-                                    </div>
-                                    <iai-silver-progress-bar
-                                        value={percentage}
-                                    ></iai-silver-progress-bar>
-                                </div>
-                            </Button>
-                        </div>
-                    {/each}
-                </Panel>
-            </div>
+            <DemoFilter
+                {setDemoFilters}
+                {category}
+                {demoOptions}
+                {demoData}
+                {demoFilters}
+                {totalCounts}
+            />
         {/each}
-        
     </Panel>
 </aside>
