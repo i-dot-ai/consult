bc9ca9b19471bc13836b70215a95e394a1a4c2af
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-08-15 12:13:02 +0100

    added filters sidebar

--- frontend/src/components/dashboard/Panel.svelte
+++ frontend/src/components/dashboard/Panel.svelte
@@ -1,13 +1,22 @@
 <script>
     import clsx from "clsx";
+
+    let {
+        border = true,
+        bg = false,
+        level = 1,
+    } = $props();
 </script>
 
 <div class={clsx([
     "my-4",
-    "p-4",
+    level === 1 && "p-4",
+    level === 2 && "p-2",
+    level === 3 && "p-1",
     "rounded-2xl",
-    "border",
-    "border-gray-200",
+    border && "border",
+    border && "border-gray-200",
+    bg && "bg-neutral-50",
 ])}>
     <slot />
 </div>
