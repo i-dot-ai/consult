02000d4062342b3b277806219c94637fcd3ed62b
Ekin Odabas <ekin.odabas@co-co071541.ad.cabinetoffice.gov.uk>
2025-08-15 13:20:35 +0100

    created demo filter component and added expand logic

---
+++ frontend/src/components/dashboard/DemoFilter.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import { slide, fade } from "svelte/transition";
+
+    import { getPercentage } from "../../global/utils.ts";
+    import Panel from "./Panel.svelte";
+    import Button from "../inputs/Button.svelte";
+
+    let {
+        category = "",
+        demoOptions = {},
+        demoData = {},
+        demoFilters = {},
+        setDemoFilters = () => {},
+        totalCounts = {},
+    } = $props();
+
+    let expanded = $state(true);
+</script>
+
+<div transition:fade>
+    <Panel level={2} border={true} bg={true}>
+        <Button
+            variant="ghost"
+            size="sm"
+            fullWidth={true}
+            handleClick={() => expanded = !expanded}
+        >
+            {category}
+        </Button>
+
+        {#each demoOptions[category] as rowKey}
+            {@const rowValue = demoData[category] && demoData[category][rowKey] || 0}
+            {@const percentage = getPercentage(rowValue, totalCounts[category])}
+
+            {#if expanded}
+                <div transition:slide class="my-1 p-1">
+                    <Button
+                        variant="ghost"
+                        size="xs"
+                        fullWidth={true}
+                        handleClick={() => setDemoFilters(category, rowKey)}
+                        highlighted={demoFilters[category]?.includes(rowKey)}
+                        highlightVariant="light"
+                    >
+                        <div class="demo-filter w-full">
+                            <div class="grid grid-cols-3 gap-1 mb-1">
+                                <span class="text-left">{rowKey}</span>
+                                <span class="text-right">{percentage}%</span>
+                                <span class="text-right">{rowValue}</span>
+                            </div>
+                            <iai-silver-progress-bar
+                                value={percentage}
+                            ></iai-silver-progress-bar>
+                        </div>
+                    </Button>
+                </div>
+            {/if}
+        {/each}
+    </Panel>
+</div>
