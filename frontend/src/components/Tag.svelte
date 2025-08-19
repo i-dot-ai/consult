df618c1346b6044ae2d795f547cb1f7db1eab7cb
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-08-19 15:59:07 +0100

    converted tags to svelte tags, refactored tag component

--- frontend/src/components/Tag.svelte
+++ frontend/src/components/Tag.svelte
@@ -1,3 +1,32 @@
-<div class="flex justify-between items-center gap-2 bg-primary text-white rounded-md w-max py-0.5 px-2 text-xs">
+<script lang="ts">
+    import clsx from "clsx";
+
+    interface Props {
+        variant?: "default" | "warning" | "primary";
+    }
+    let {
+        variant="default",
+    }: Props = $props();
+</script>
+
+<div class={clsx([
+    "flex",
+    "justify-between",
+    "items-center",
+    "gap-2",
+    "rounded-md",
+    "w-max",
+    "py-0.5",
+    "px-2",
+    "text-xs",
+    variant === "default" && "bg-neutral-100",
+    variant === "default" && "text-neutral-800",
+
+    variant === "primary" && "bg-primary",
+    variant === "primary" && "text-white",
+
+    variant === "warning" && "text-yellow-700",
+    variant === "warning" && "bg-yellow-100",
+])}>
     <slot />
 </div>
