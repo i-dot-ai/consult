bd35efa5dd502d8edb3f1960bb94affa8bd8334c
Dheeraj Singh <33820920+dk-singh@users.noreply.github.com>
2025-08-27 21:08:38 +0100

    Add lambda for consulation import

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -39,3 +39,25 @@ module "slack_notifier_lambda" {
     "LAMBDA_AWS_REGION" = local.secret_env_vars.AWS_REGION,
   }
 }
+
+module "consultation_import_lambda" {
+  source = "git::https://github.com/i-dot-ai/i-dot-ai-core-terraform-modules.git//modules/infrastructure/lambda?ref=v2.0.1-lambda"
+
+  file_path                     = "${path.root}/../lambda/consultation_import.zip"
+  handler                      = "lambda_function.lambda_handler"  
+  runtime                      = "python3.12"
+  function_name                = "${local.name}-import-lambda"
+  package_type                 = "Zip"
+  memory_size                  = 1024
+  source_code_hash             = data.archive_file.consultation_import_archive.output_base64sha256
+  account_id                   = var.account_id
+  lambda_additional_policy_arns = {for idx, arn in [aws_iam_policy.lambda_exec_custom_policy.arn] : idx => arn}
+  aws_security_group_ids = [aws_security_group.lambda_sg.id]
+  subnet_ids              = data.terraform_remote_state.vpc.outputs.private_subnets
+  # Environment variables
+  environment_variables = {
+    REDIS_HOST = module.elasticache.redis_address  # or hardcode your Redis host
+    REDIS_PORT = module.elasticache.redis_port  # or hardcode "6379"
+    AWS_BUCKET_NAME = module.app_bucket.id  # or hardcode your bucket ARN
+  }
+}
