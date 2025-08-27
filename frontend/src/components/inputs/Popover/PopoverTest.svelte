4d66b33a0304149e058100235de0348a213db7af
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-08-27 17:56:32 +0100

    added popover tests

---
+++ frontend/src/components/inputs/Popover/PopoverTest.svelte
@@ -0,0 +1,13 @@
+<script>
+    import Popover from "./Popover.svelte";
+
+    export let open = false;
+    export let label = "";
+    export let triggerContent = "";
+    export let panelContent = '';
+</script>
+
+<Popover open={open} label={label}>
+    <p slot="trigger">{triggerContent}</p>
+    <p slot="panel">{panelContent}</p>
+</Popover>
