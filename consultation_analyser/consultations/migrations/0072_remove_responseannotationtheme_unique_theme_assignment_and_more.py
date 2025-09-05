1984b287b1d7e7be30332d9e5ae78a298b33cc3e
gecBurton <g.e.c.burton@gmail.com>
2025-09-05 12:07:29 +0100

    fixed migration order

--- consultation_analyser/consultations/migrations/0072_remove_responseannotationtheme_unique_theme_assignment_and_more.py
+++ consultation_analyser/consultations/migrations/0072_remove_responseannotationtheme_unique_theme_assignment_and_more.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("consultations", "0070_historicalresponseannotation_and_more"),
+        ("consultations", "0071_responseannotation_flagged_by"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
