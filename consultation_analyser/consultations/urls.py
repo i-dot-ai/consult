873859f5171c46951c0eafae9f6093978f8da7f4
gecBurton <g.e.c.burton@gmail.com>
2025-09-09 07:32:11 +0100

    formatting

--- consultation_analyser/consultations/urls.py
+++ consultation_analyser/consultations/urls.py
@@ -7,10 +7,10 @@ from .api.views import (
     QuestionViewSet,
     ResponseViewSet,
     ThemeViewSet,
+    UserViewSet,
     generate_magic_link,
     get_current_user,
     verify_magic_link,
-    UserViewSet,
 )
 from .views import answers, pages, questions, root, sessions
 
