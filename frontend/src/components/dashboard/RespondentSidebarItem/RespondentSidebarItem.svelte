d3bb9d1ee985c9ac87d0d41d139967b9e6500608
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-09-17 14:10:07 +0100

    added respondent sidebar item and story

---
+++ frontend/src/components/dashboard/RespondentSidebarItem/RespondentSidebarItem.svelte
@@ -0,0 +1,105 @@
+<script lang="ts">
+    import MaterialIcon from '../../MaterialIcon.svelte';
+    import Button from '../../inputs/Button/Button.svelte';
+    import EditSquare from '../../svg/material/EditSquare.svelte';
+    import TextInput from '../../inputs/TextInput/TextInput.svelte';
+    import Check from '../../svg/material/Check.svelte';
+    import Close from '../../svg/material/Close.svelte';
+
+
+    interface Props {
+        title: string;
+        subtitle: string;
+        icon: any;
+        editable?: boolean;
+        updateSubtitle?: (newSubtitle: string) => void;
+    }
+
+    let {
+        title = "",
+        subtitle = "",
+        icon,
+        editable = false,
+        updateSubtitle = () => {},
+    }: Props = $props();
+
+    let stagedSubtitle: string = $state(subtitle); 
+    let editing: boolean = $state(false);
+
+    const toggleEditing = () => {
+        // Reset staged subtitle if exiting edit mode
+        if (editing) {
+            stagedSubtitle = subtitle;
+        }
+        editing = !editing;
+    };
+</script>
+
+<div class="flex items-start gap-2 mt-4 text-xs">
+    <div class="bg-neutral-100 p-1 rounded-lg h-max">
+        <MaterialIcon size="1.3rem" color="fill-neutral-700">
+            <svelte:component this={icon} />
+        </MaterialIcon>
+    </div>
+    <div class="flex flex-col w-full">
+        <div class="flex justify-between items-start">
+            <h3 class="uppercase text-neutral-500">{title}</h3>
+
+            {#if editable}
+                <div class="-mt-1">
+                    <Button variant="ghost" handleClick={toggleEditing}>
+                        <MaterialIcon color="fill-neutral-700">
+                            <EditSquare />
+                        </MaterialIcon>
+                    </Button>
+                </div>
+            {/if}
+        </div>
+        
+        {#if editable && editing}
+            <TextInput
+                id="edit-subtitle-input"
+                label="Edit Subtitle"
+                hideLabel={true}
+                value={stagedSubtitle}
+                setValue={(newValue) => stagedSubtitle = newValue.trim()}
+            />
+            <div class="flex items-center justify-around gap-2 mt-2">
+                <div class="grow">
+                    <Button
+                        variant="approve"
+                        fullWidth={true}
+                        handleClick={() => {
+                            updateSubtitle(stagedSubtitle);
+                            toggleEditing();
+                        }}
+                    >
+                        <div class="flex justify-center w-full gap-2">
+                            <MaterialIcon color="fill-white">
+                                <Check />
+                            </MaterialIcon>
+
+                            Save
+                        </div>
+                    </Button>
+                </div>
+                <div class="grow">
+                    <Button
+                        fullWidth={true}
+                        handleClick={() => toggleEditing()}
+                    >
+                        <div class="flex justify-center w-full gap-2">
+                            <MaterialIcon color="fill-neutral-500">
+                                <Close />
+                            </MaterialIcon>
+
+                            Cancel
+                        </div>
+                    </Button>
+                </div>
+            </div>
+        {:else}
+            <p>{subtitle}</p>
+        {/if}
+    </div>
+</div>
---
+++ frontend/src/components/dashboard/RespondentSidebarItem/RespondentSidebarItemStory.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+    import Switch from "../../inputs/Switch/Switch.svelte";
+    import TextInput from "../../inputs/TextInput/TextInput.svelte";
+    import Star from "../../svg/material/Star.svelte";
+    import RespondentSidebarItem from "./RespondentSidebarItem.svelte";
+
+
+    let title = $state("Test Title");
+    let subtitle = $state("Test subtitle");
+    let editable = $state(false);
+</script>
+
+<RespondentSidebarItem
+    title={title}
+    subtitle={subtitle}
+    icon={Star}
+    editable={editable}
+    updateSubtitle={(newSubtitle: string) => subtitle = newSubtitle}
+/>
+
+<hr class="my-8" />
+
+<TextInput
+    id="input-title"
+    label="Title"
+    value={title}
+    setValue={(newValue) => title = newValue}
+/>
+
+<TextInput
+    id="input-subtitle"
+    label="Subtitle"
+    value={subtitle}
+    setValue={(newValue) => subtitle = newValue}
+/>
+
+<Switch
+    id="editable-input"
+    label="Editable"
+    value={editable}
+    handleChange={(newVal) => editable = newVal}    
+/>
