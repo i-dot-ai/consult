1703770424a4f316f129c4dd6cebca0da1d8800c
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-09-17 11:13:17 +0100

    added respondentanswer along with story page and dashboard subpath to design path

---
+++ frontend/src/components/RespondentAnswer/RespondentAnswer.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+    import clsx from "clsx";
+
+    import Panel from "../dashboard/Panel/Panel.svelte";
+    import Tag from "../Tag/Tag.svelte";
+    import MaterialIcon from "../MaterialIcon.svelte";
+    import Diamond from "../svg/material/Diamond.svelte";
+
+
+    interface Props {
+        questionTitle: string;
+        questionNumber: number;
+        answerText: string;
+        themes: string[];
+    }
+
+    let {
+        questionTitle = "",
+        questionNumber = 0,
+        answerText = "",
+        themes = [],
+    }: Props = $props();
+</script>
+
+<style lang="scss">
+    a:hover {
+        h3 {
+            color: var(--color-primary);
+        }
+        .question-number {
+            color: white;
+            background-color: var(--color-primary);
+        }
+    }
+</style>
+
+<Panel border={true} bg={true}>
+    <article>
+        <header class="flex flex-col-reverse md:flex-row gap-y-4 justify-between items-start gap-2">
+            <a href="#">
+                <div class="flex items-start gap-2">
+                    <div class={clsx([
+                        "question-number",
+                        "p-1",
+                        "rounded-lg",
+                        "border",
+                        "border-neutral-200",
+                        "text-xs",
+                        "bg-neutral-100",
+                        "transition-colors",
+                    ])}>
+                        Q{questionNumber}
+                    </div>
+
+                    <h3 class={clsx([
+                        "text-xs",
+                        "text-neutral-500",
+                        "transition-colors",
+                        "mt-0.5",
+                    ])}>
+                        {questionTitle}
+                    </h3>
+                </div>
+            </a>
+
+            <div class="whitespace-nowrap">
+                <Tag variant="warning">
+                    <MaterialIcon size="0.9rem" color="fill-yellow-600">
+                        <Diamond />
+                    </MaterialIcon>
+
+                    <span class="text-xs">Evidence-rich</span>
+                </Tag>
+            </div>
+        </header>
+
+        <p class="text-sm my-4">
+            {answerText}
+        </p>
+
+        <footer class="flex items-center flex-wrap gap-2">
+            <small class="text-xs text-neutral-500">
+                Themes:
+            </small>
+
+            {#each themes as theme}
+                <Tag variant="dark">
+                    {theme}
+                </Tag>
+            {/each}
+        </footer>
+    </article>
+</Panel>
---
+++ frontend/src/components/RespondentAnswer/RespondentAnswerStory.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import TextInput from "../inputs/TextInput/TextInput.svelte";
+    import RespondentAnswer from "./RespondentAnswer.svelte";
+
+
+    let questionTitle = $state("Do you agree with the proposal to align the flavour categories of chocolate bars as outlined in the draft guidelines of the Chocolate Bar Regulation for the United Kingdom?");
+    let questionNumber = $state(1);
+    let answerText = $state("I agree in principle, but I think the guidelines should include a provision for periodic review to adapt to market changes.");
+    let themes = $state([ "Innovation", "Standardized framework" ]);
+</script>
+
+<RespondentAnswer
+    questionTitle={questionTitle}
+    questionNumber={questionNumber}
+    answerText={answerText}
+    themes={themes}
+/>
+
+<hr class="my-8" />
+
+<TextInput
+    id="input-question-title"
+    label="Question Title"
+    value={questionTitle}
+    setValue={(newValue) => questionTitle = newValue}
+/>
+
+<TextInput
+    id="input-question-number"
+    label="Question Number"
+    value={questionNumber.toString()}
+    setValue={(newValue) => questionNumber = parseInt(newValue)}
+/>
+
+<TextInput
+    id="input-answer-text"
+    label="Answer Text"
+    value={answerText}
+    setValue={(newValue) => answerText = newValue}
+/>
+
+<TextInput
+    id="input-themes-all"
+    label="Themes"
+    value={`[ "Innovation", "Standardized framework" ]`}
+    setValue={(newValue) => {
+        try {
+            themes = JSON.parse(newValue);
+        } catch {}
+    }}
+/>
