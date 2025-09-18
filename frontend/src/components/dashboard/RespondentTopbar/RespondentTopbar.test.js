7c1e7fc15965048c783d7b96ed75ffd17026eecd
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-09-18 14:53:37 +0100

    added respondent topbar tests

---
+++ frontend/src/components/dashboard/RespondentTopbar/RespondentTopbar.test.js
@@ -0,0 +1,34 @@
+import { afterEach, beforeEach, describe, expect, it, test, vi } from "vitest";
+import userEvent from '@testing-library/user-event';
+import { render, cleanup, screen } from "@testing-library/svelte";
+
+import RespondentTopbar from "./RespondentTopbar.svelte";
+import RespondentTopbarStory from "./RespondentTopbarStory.svelte";
+
+
+let testData;
+
+describe("RespondentTopbar", () => {
+    beforeEach(() => {
+        testData = {
+            respondentId: "test-respondent",
+        };
+    })
+
+    afterEach(() => cleanup())
+
+    it("should render data and links", () => {
+        const { getByText, container } = render(RespondentTopbar, {
+            ...testData,
+        });
+
+        expect(getByText(`Respondent ${testData.respondentId}`));
+        expect(getByText("Back to Analysis"));
+        expect(getByText("Previous Respondent"));
+        expect(getByText("Next Respondent"));        
+    })
+
+    it("should render story", () => {
+        render(RespondentTopbarStory);
+    })
+})
