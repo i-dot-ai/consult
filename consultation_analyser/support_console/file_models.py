12ab865e2da25feff7b23f2b95f474688ab65d3c
George Burton <8233643+gecBurton@users.noreply.github.com>
2025-09-18 15:16:17 +0100

    Feature/dont-throw-error-if-file-doesnt-exist (#846)
    * feature/dont-throw-error-if-file-doesnt-exist
    
    * added unit test

--- consultation_analyser/support_console/file_models.py
+++ consultation_analyser/support_console/file_models.py
@@ -1,12 +1,25 @@
 from typing import Any, Literal
 
+from botocore.exceptions import ClientError
+from django.conf import settings
 from pydantic import BaseModel, computed_field, field_validator
 
 from consultation_analyser.consultations.models import ResponseAnnotation
 
+logger = settings.LOGGER
 
-def read_from_s3(model, client, bucket: str, key: str):
-    s3_obj = client.get_object(Bucket=bucket, Key=key)
+
+def read_from_s3(model, client, bucket: str, key: str, raise_error_if_file_missing: bool = True):
+    try:
+        s3_obj = client.get_object(Bucket=bucket, Key=key)
+    except ClientError:
+        if raise_error_if_file_missing is False:
+            return  # early return as no data found
+
+        logger.error("file not found, bucket={bucket}, key={key}", bucket=bucket, key=key)
+        msg = f"file not found {bucket}/{key}"
+        raise ValueError(msg)
+
     for line in s3_obj["Body"].iter_lines():
         yield model.model_validate_json(line.decode("utf-8"))
 
