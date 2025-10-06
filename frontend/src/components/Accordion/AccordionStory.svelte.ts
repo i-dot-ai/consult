9531d2098167a0aad5135d22915c63d6cb4953d1
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-10-06 15:37:56 +0100

    added accordion component

---
+++ frontend/src/components/Accordion/AccordionStory.svelte.ts
@@ -0,0 +1,46 @@
+import { createRawSnippet } from "svelte";
+
+import Accordion from "./Accordion.svelte";
+
+
+const createSnippetWithDefault = (html: string) => {
+  try {
+    return createRawSnippet(() => ({
+      render: () => html,
+    }));
+  } catch {
+    return createRawSnippet(() => ({
+      render: () => "<p>Child Element</p>",
+    }));
+  }
+}
+
+let titleHtml = $state(`<span>Accordion Title</span>`);
+let titleComponent = $derived.by(() => {
+  return createSnippetWithDefault(titleHtml);
+});
+
+let contentHtml = $state(`<p>Panel Content</p>`);
+let contentComponent = $derived.by(() => {
+  return createSnippetWithDefault(contentHtml);
+});
+
+export default {
+  name: "Accordion",
+  component: Accordion,
+  props: [
+    {
+      name: "title",
+      value: titleComponent,
+      type: "html",
+      rawHtml: titleHtml,
+    },
+    {
+      name: "content",
+      value: contentComponent,
+      type: "html",
+      rawHtml: contentHtml,
+    },
+  ],
+  stories: [ /* TODO: Add stories */ ],
+};
