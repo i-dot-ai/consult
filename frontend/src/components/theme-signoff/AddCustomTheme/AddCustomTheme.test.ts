abb25d15acc906ef5bb4b17143c1183f91c3dda3
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-10-15 16:22:58 +0100

    added custom theme panel

---
+++ frontend/src/components/theme-signoff/AddCustomTheme/AddCustomTheme.test.ts
@@ -0,0 +1,37 @@
+import { afterEach, beforeEach, describe, expect, it } from "vitest";
+import { render, cleanup } from "@testing-library/svelte";
+
+import AddCustomTheme, { type Props } from "./AddCustomTheme.svelte";
+
+let testData: Props;
+
+describe("AddCustomTheme", () => {
+  beforeEach(() => {
+    testData = {
+      variant: "add",
+      handleConfirm: () => {},
+      handleCancel: () => {},
+    };
+  })
+
+  afterEach(() => cleanup());
+
+  it("should render add variant", async () => {
+    const { container, getByText } = render(AddCustomTheme, {
+      ...testData,
+    });
+
+    expect(getByText("Add Custom Theme"));
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("should render add variant", async () => {
+    const { container, getByText } = render(AddCustomTheme, {
+      ...testData,
+      variant: "edit",
+    });
+
+    expect(getByText("Edit Theme"));
+  });
+});
