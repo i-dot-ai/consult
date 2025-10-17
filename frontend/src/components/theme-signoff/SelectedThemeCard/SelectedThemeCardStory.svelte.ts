9d7d1b1f8daa3c4b2c2c970c20b7a940e06a1369
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-10-17 11:22:59 +0100

    added selectedthemecard story

---
+++ frontend/src/components/theme-signoff/SelectedThemeCard/SelectedThemeCardStory.svelte.ts
@@ -0,0 +1,40 @@
+import SelectedThemeCard from "./SelectedThemeCard.svelte";
+
+let theme = $state({
+  id: "theme-id",
+  name: "Theme Name",
+  description: "Theme description",
+});
+let answers = $state(["Answer 1", "Answer 2"]);
+let removeTheme = $state((themeId) => alert(`Remove theme event triggered with: ${themeId}`));
+let updateTheme = $state((...args) => alert(`Update theme event triggered with: ${args.join(", ")}`));
+
+export default {
+  name: "SelectedThemeCard",
+  component: SelectedThemeCard,
+  props: [
+    {
+      name: "theme",
+      value: theme,
+      type: "json",
+    },
+    {
+      name: "answers",
+      value: answers,
+      type: "json",
+    },
+    {
+      name: "removeTheme",
+      value: removeTheme,
+      type: "func",
+      schema: `(themeId: string) => void`,
+    },
+    {
+      name: "updateTheme",
+      value: updateTheme,
+      type: "func",
+      schema: `(themeId: string, title: string, description: string) => void,`,
+    },
+  ],
+  stories: [],
+};
