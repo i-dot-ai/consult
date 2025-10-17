1cc348e8dc1d23bcde7c3229621f36f8407e5871
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-10-17 11:57:42 +0100

    added stories component and route

---
+++ frontend/src/components/Stories/stories.ts
@@ -0,0 +1,21 @@
+import ProgressStory from "../Progress/ProgressStory.svelte";
+import HeaderStory from "../Header/HeaderStory.svelte";
+import FooterStory from "../Footer/FooterStory.svelte";
+import AccordionStory from "../Accordion/AccordionStory.svelte";
+import PanelStory from "../dashboard/Panel/PanelStory.svelte";
+import DemoFilterStory from "../DemoFilter/DemoFilterStory.svelte";
+import RespondentSidebarItemStory from "../dashboard/RespondentSidebarItem/RespondentSidebarItemStory.svelte";
+import AddCustomThemeStory from "../theme-signoff/AddCustomTheme/AddCustomThemeStory.svelte";
+import SelectedThemeCardStory from "../theme-signoff/SelectedThemeCard/SelectedThemeCardStory.svelte";
+
+export default [
+  ProgressStory,
+  HeaderStory,
+  FooterStory,
+  AccordionStory,
+  PanelStory,
+  DemoFilterStory,
+  RespondentSidebarItemStory,
+  AddCustomThemeStory,
+  SelectedThemeCardStory,
+]
