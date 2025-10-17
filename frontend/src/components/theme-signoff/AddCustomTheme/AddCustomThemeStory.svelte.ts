b0b88831b77956c24811c923ada46e8a7156a556
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-10-17 10:59:01 +0100

    added story

---
+++ frontend/src/components/theme-signoff/AddCustomTheme/AddCustomThemeStory.svelte.ts
@@ -0,0 +1,52 @@
+import AddCustomTheme from "./AddCustomTheme.svelte";
+
+let variant = $state("add");
+const handleConfirm = () => alert("Confirm event triggered");
+const handleCancel = () => alert("Cancel event triggered");
+
+
+export default {
+  name: "AddCustomTheme",
+  component: AddCustomTheme,
+  props: [
+    {
+      name: "variant",
+      value: variant,
+      type: "select",
+      options: [
+        { value: "add", label: "Add" },
+        { value: "edit", label: "Edit" },
+      ],
+    },
+    {
+      name: "handleConfirm",
+      value: handleConfirm,
+      type: "func",
+      schema: "(title: string, description: string) => void",
+    },
+    {
+      name: "handleCancel",
+      value: handleCancel,
+      type: "func",
+      schema: "() => void",
+    },
+  ],
+  stories: [
+    {
+      name: "Add Variant",
+      props: {
+        variant: "add",
+        handleConfirm: handleConfirm,
+        handleCancel: handleCancel,
+      },
+    },
+    {
+      name: "Edit Variant",
+      props: {
+        variant: "edit",
+        handleConfirm: handleConfirm,
+        handleCancel: handleCancel,
+      },
+    },
+  ],
+};
