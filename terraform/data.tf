54c5fbd4110c4a55c97ae0fc28131af76963ef4c
gecBurton <g.e.c.burton@gmail.com>
2025-10-28 12:24:49 +0000

    upgraded rds

--- terraform/data.tf
+++ terraform/data.tf
@@ -122,3 +122,8 @@ data "aws_ssm_parameter" "slack_webhook_url" {
 data "aws_ssm_parameter" "litellm_api_key" {
   name = "/i-dot-ai-${terraform.workspace}-core-llm-gateway/env_secrets/${var.project_name}-api-key"
 }
+
+data "aws_wafv2_ip_set" "ip_whitelist_internal" {
+  name  = "i-dot-ai-core-ip-config-ip-set-internal"
+  scope = var.scope
+}
