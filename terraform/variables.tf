5aa4a32c687794697116379cc8d835f5905cddac
gecBurton <g.e.c.burton@gmail.com>
2025-10-28 11:01:05 +0000

    updated variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,40 +1,84 @@
-variable "account_id" {
+variable "image_tag" {
+  description = "The tag of the image to use"
   type        = string
-  description = "AWS Account ID"
+  default     = "latest"
 }
 
-variable "app-replica-count-desired" {
-  type        = number
-  default     = 1
-  description = "The desired number of replicas"
+variable "region" {
+  type        = string
+  description = "AWS region for infrastructure to be deployed to"
 }
 
-variable "container_port" {
-  type        = number
-  description = "The port to run the application on"
-  default     = 80
-}
-
-variable "cpu" {
-  type        = number
-  default     = 1024
-  description = "The cpu resource to give to the task"
-}
-
-variable "vcpus" {
-  type        = number
-  description = "The number of vcpus to give to the task."
-  default     = 2
-}
-
-variable "developer_ips" {
-  type        = list(string)
-  description = "List of developer IPs"
+variable "state_bucket" {
+  type        = string
+  description = "Name of the S3 bucket to use a terraform state"
 }
 
 variable "domain_name" {
   type        = string
   description = "The base domain name for the project"
+}
+
+variable "env" {
+  type        = string
+  description = "environment"
+  default     = "dev"
+}
+
+variable "project_name" {
+  type        = string
+  description = "Name of project"
+}
+
+variable "team_name" {
+  type        = string
+  description = "The name of the team"
+}
+
+variable "universal_tags" {
+  type        = map(string)
+  description = "Map to tag resources with"
+}
+
+variable "github_org" {
+  type        = string
+  default     = "github.com/i-dot-ai/"
+  description = "The default I.AI GitHub Org URL"
+}
+
+variable "repository_name" {
+  type        = string
+  description = "The GitHub repository name"
+}
+
+variable "deployed_via" {
+  type        = string
+  default     = "GitHub_Actions"
+  description = "Mechanism for how the Infra was deployed."
+}
+
+variable "security_level" {
+  type        = string
+  default     = "base"
+  description = "Security Level of the infrastructure."
+}
+
+variable "scope" {
+  description = "Scope of the WAF, either 'CLOUDFRONT' or 'REGIONAL'"
+  type        = string
+  default     = "REGIONAL"
+}
+
+# <old-variables>
+
+variable "account_id" {
+  type        = string
+  description = "AWS Account ID"
+}
+
+variable "developer_ips" {
+  type        = list(string)
+  description = "List of developer IPs"
 }
 
 variable "external_ips" {
@@ -52,37 +96,6 @@ variable "frontend_repository_uri" {
   description = "Frontend ECR repo uri"
 }
 
-variable "ecs_cluster_name" {
-  type        = string
-  description = "ECS cluster name to attach service to"
-  default     = "null"
-}
-
-variable "environment_variables" {
-  type        = map(any)
-  default     = { "ENVIRONMENT" : "dev" }
-  description = "A map of the environment variables to be passed to the ECS task"
-}
-
-variable "env" {
-  type        = string
-  description = "environment"
-  default     = "dev"
-}
-
-variable "health_check" {
-  type = object({
-    accepted_response = string
-    path              = string
-    timeout           = number
-  })
-  default = {
-    accepted_response = "200"
-    path              = "/health"
-    timeout           = 5
-  }
-  description = "Set the health check configuration for the target group"
-}
 
 variable "hosted_zone_id" {
   type        = string
@@ -94,68 +107,5 @@ variable "internal_ips" {
   description = "IP's of No10 and CO"
 }
 
-variable "image_tag" {
-  type        = string
-  description = "The tag of the image to use"
-}
+# </old-variables>
 
-variable "memory" {
-  type        = number
-  description = "The memory resource to give to the task"
-  default     = 2048
-}
-
-variable "ecs_memory" {
-  type        = number
-  description = "The memory resource to give to the ecs"
-  default     = 2048
-}
-
-variable "ecs_cpus" {
-  type        = number
-  description = "The cpu resource to give to the task"
-  default     = 1
-}
-
-variable "prefix" {
-  type        = string
-  description = "value to prefix resources with"
-  default     = "i-dot-ai"
-}
-
-variable "project_name" {
-  type        = string
-  description = "Name of project"
-}
-
-variable "region" {
-  type        = string
-  description = "aws region"
-  default     = "eu-west-2"
-}
-
-variable "state_bucket" {
-  type        = string
-  description = "Name of the S3 bucket to use a terraform state"
-}
-
-variable "scope" {
-  description = "Scope of the WAF, either 'CLOUDFRONT' or 'REGIONAL'"
-  type        = string
-  default     = "REGIONAL"
-}
-
-variable "team_name" {
-  type        = string
-  description = "The name of the team"
-}
-
-variable "universal_tags" {
-  type        = map(string)
-  description = "Map to tag resources with"
-}
-
-variable "publicly_accessible" {
-  type        = bool
-  description = "Flag to determine if the database is publicly accessible"
-}
