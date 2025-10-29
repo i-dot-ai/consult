8137f5cdb948432e49978f94430c311e22af1327
gecBurton <g.e.c.burton@gmail.com>
2025-10-29 07:00:16 +0000

    updated data.tf

--- terraform/data.tf
+++ terraform/data.tf
@@ -1,14 +1,6 @@
-locals {
-  record_prefix = var.env == "prod" ? var.project_name : "${var.project_name}-${var.env}"
-  host          = terraform.workspace == "prod" ? "${var.project_name}.ai.cabinetoffice.gov.uk" : "${var.project_name}-${terraform.workspace}.ai.cabinetoffice.gov.uk"
-  host_backend  = terraform.workspace == "prod" ? "${var.project_name}-backend-external.ai.cabinetoffice.gov.uk" : "${var.project_name}-backend-external-${terraform.workspace}.ai.cabinetoffice.gov.uk"
-  name          = "${var.team_name}-${var.env}-${var.project_name}"
-  batch_memory  = 8192
-  batch_vcpus   = 4
-  ecs_memory    = var.env == "prod" ? 4096 : 4096
-  ecs_cpus      = var.env == "prod" ? 2048 : 1024
+data "aws_caller_identity" "current" {}
 
-}
+data "aws_region" "current" {}
 
 data "terraform_remote_state" "vpc" {
   backend   = "s3"
@@ -41,7 +33,6 @@ data "terraform_remote_state" "universal" {
   }
 }
 
-
 data "terraform_remote_state" "account" {
   backend = "s3"
   config = {
@@ -51,17 +42,68 @@ data "terraform_remote_state" "account" {
   }
 }
 
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
-
-
-data "aws_secretsmanager_secret" "env_vars" {
-  name = "${local.name}-environment-variables"
+data "terraform_remote_state" "keycloak" {
+  backend   = "s3"
+  workspace = terraform.workspace
+  config = {
+    bucket = var.state_bucket
+    key    = "core/keycloak/keycloak/terraform.tfstate"
+    region = var.region
+  }
 }
+
+locals {
+  # name              = "${var.team_name}-${var.env}-${var.project_name}"
+  hosted_zone_name  = terraform.workspace == "prod" ? var.domain_name : "${terraform.workspace}.${var.domain_name}"
+  # host              = terraform.workspace == "prod" ? "${var.project_name}.${var.domain_name}" : "${var.project_name}.${terraform.workspace}.${var.domain_name}"
+  # host_backend      = terraform.workspace == "prod" ? "${var.project_name}-backend-external.${var.domain_name}" : "${var.project_name}-backend-external.${terraform.workspace}.${var.domain_name}" 
+
+  record_prefix = var.env == "prod" ? var.project_name : "${var.project_name}-${var.env}"
+  host          = terraform.workspace == "prod" ? "${var.project_name}.ai.cabinetoffice.gov.uk" : "${var.project_name}-${terraform.workspace}.ai.cabinetoffice.gov.uk"
+  host_backend  = terraform.workspace == "prod" ? "${var.project_name}-backend-external.ai.cabinetoffice.gov.uk" : "${var.project_name}-backend-external-${terraform.workspace}.ai.cabinetoffice.gov.uk"
+  name          = "${var.team_name}-${var.env}-${var.project_name}"
+  batch_memory  = 8192
+  batch_vcpus   = 4
+  ecs_memory    = var.env == "prod" ? 4096 : 4096
+  ecs_cpus      = var.env == "prod" ? 2048 : 1024
+
+}
+
+# data "aws_ssm_parameter" "client_secret" {
+#   name = "/${var.team_name}-${terraform.workspace}-core-keycloak/app_client_secret/${var.project_name}"
+# }
+
+data "aws_ssm_parameter" "auth_provider_public_key" {
+  name = "/i-dot-ai-${terraform.workspace}-core-keycloak/realm_public_key"
+}
+
+data "aws_ssm_parameter" "auth_api_invoke_url" {
+  name = "/i-dot-ai-${terraform.workspace}-core-auth-api/auth/INVOKE_URL"
+}
+
+data "aws_secretsmanager_secret" "slack" {
+  name = "i-dot-ai-${var.env}-platform-slack-webhook"
+}
+
+data "aws_secretsmanager_secret_version" "platform_slack_webhook" {
+  secret_id = data.aws_secretsmanager_secret.slack.id
+}
+
+data "aws_wafv2_ip_set" "ip_whitelist_internal" {
+  name  = "i-dot-ai-core-ip-config-ip-set-internal"
+  scope = var.scope
+}
+
+# data "aws_route53_zone" "zone" {
+#   name = local.hosted_zone_name
+# }
 
 data "aws_secretsmanager_secret_version" "env_vars" {
   secret_id = data.aws_secretsmanager_secret.env_vars.id
+}
+
+data "aws_secretsmanager_secret" "env_vars" {
+  name = "${local.name}-environment-variables"
 }
 
 data "archive_file" "submit_batch_job_archive" {
@@ -121,17 +163,4 @@ data "aws_ssm_parameter" "slack_webhook_url" {
 
 data "aws_ssm_parameter" "litellm_api_key" {
   name = "/i-dot-ai-${terraform.workspace}-core-llm-gateway/env_secrets/${var.project_name}-api-key"
-}
-
-data "aws_wafv2_ip_set" "ip_whitelist_internal" {
-  name  = "i-dot-ai-core-ip-config-ip-set-internal"
-  scope = var.scope
-}
-
-data "aws_secretsmanager_secret_version" "platform_slack_webhook" {
-  secret_id = data.aws_secretsmanager_secret.slack.id
-}
-
-data "aws_secretsmanager_secret" "slack" {
-  name = "i-dot-ai-${var.env}-platform-slack-webhook"
 }
