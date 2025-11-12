6f57cedd347cabd607bad353b6003f889817f07b
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-11-12 13:17:05 +0000

    Changed styling on Details.svelte to be less complicated

--- frontend/src/components/Details/Details.svelte
+++ frontend/src/components/Details/Details.svelte
@@ -32,48 +32,16 @@
   }
 
   .govuk-details__summary {
-    display: inline-block;
     position: relative;
-    padding-left: 25px;
-    color: #1d70b8;
+    color: var(--color-primary);;
     cursor: pointer;
   }
 
   .govuk-details__summary:hover {
-    color: #003078;
-  }
-
-  .govuk-details__summary:focus {
-    outline: 3px solid #fd0;
-    outline-offset: 0;
-    background-color: #fd0;
-  }
-
-  .govuk-details__summary:before {
-    content: "";
-    position: absolute;
-    top: -1px;
-    left: 0;
-    width: 0;
-    height: 0;
-    border-style: solid;
-    border-color: transparent;
-    -webkit-clip-path: polygon(0% 0%, 0% 100%, 100% 50%);
-    clip-path: polygon(0% 0%, 0% 100%, 100% 50%);
-    border-width: 7px 0 7px 12.124px;
-    border-left-color: inherit;
-  }
-
-  .govuk-details[open] > .govuk-details__summary:before {
-    -webkit-clip-path: polygon(0% 0%, 50% 100%, 100% 0%);
-    clip-path: polygon(0% 0%, 50% 100%, 100% 0%);
-    border-width: 12.124px 7px 0 7px;
-    border-top-color: inherit;
-    border-left-color: transparent;
+    text-decoration: underline;
   }
 
   .govuk-details__summary-text {
-    text-decoration: underline;
     text-decoration-thickness: max(1px, 0.0625rem);
     text-underline-offset: 0.1578em;
   }
