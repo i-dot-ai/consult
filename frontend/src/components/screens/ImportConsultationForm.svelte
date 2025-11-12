f3e957f684673d805dbffd24102c36243aefff38
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-11-12 09:37:21 +0000

    Added svelte components to ImportConsultationForm.svelte

--- frontend/src/components/screens/ImportConsultationForm.svelte
+++ frontend/src/components/screens/ImportConsultationForm.svelte
@@ -1,23 +1,47 @@
 <script lang="ts">
   import clsx from "clsx";
 
+  import { onMount } from "svelte";
+
   import { Routes } from "../../global/routes";
 
   import TextInput from "../inputs/TextInput/TextInput.svelte";
+  import Radio from "../Radio/Radio.svelte";
+  import type { RadioItem } from "../../global/types";
   import Button from "../inputs/Button/Button.svelte";
   import Select from "../inputs/Select.svelte";
+  import type { SelectOption } from "../../global/types";
 
   let sending: boolean = false;
   let error: string = "";
   let success: boolean = false;
+  let loading: boolean = false;
   let consultationName: string = "";
   let timestamp: string = "";
+  let consultationFolders: SelectOption[] = [];
+  let RADIO_OPTIONS: RadioItem[] = [
+      {value: 'sign_off',
+      text: 'Populate Sign Off',
+      checked: false,
+      disabled: false}
+    ,
+      {value: 'dashboard',
+      text: 'Populate Dashboard',
+      checked: true,
+      disabled: false}
+  ]
 
   onMount(async () => {
     loading = true;
     const response = await fetch(`${Routes.ApiConsultationFolders}`);
-    const consultationData = await response.json();
-    consultations = consultationData.results;
+    const consultationFolderData = await response.json();
+    for (let folder of consultationFolderData.results) {
+      let option: SelectOption = {
+      value: folder.value, 
+      label: folder.text
+    };
+    consultationFolders.push(option);
+    };
     loading = false;
   });
 
@@ -67,20 +91,18 @@
   };
 </script>
 
-<form
-    class={clsx(["flex", "flex-col", "gap-4"])}
-    on:submit|preventDefault={handleSubmit}
->
-<TextInput id="consultation_name" name="consultation_name" label="Consultation title" autocomplete="false" value={consultationName} setValue={setConsultationName} />
-<Select label="Select consultation folder" options= />
-<!--  <Radios />-->
-<TextInput autocomplete="false" id="timestamp" name="timestamp" label="Mapping timestamp folder (only needed to populate the dashboard)" placeholder="e.g., 2024-01-15-14-30-00" value={timestamp} setValue={setTimestamp} />
-<Button
+<form class={clsx(["flex", "flex-col", "gap-4"])}
+    on:submit|preventDefault={handleSubmit}>
+  <TextInput id="consultation_name" name="consultation_name" label="Consultation title" autocomplete="false" value={consultationName} setValue={setConsultationName} />
+  <Select label="Select consultation folder" options={consultationFolders} />
+  <Radio name="action" items={RADIO_OPTIONS} />
+  <TextInput autocomplete="false" id="timestamp" name="timestamp" label="Mapping timestamp folder (only needed to populate the dashboard)" placeholder="e.g., 2024-01-15-14-30-00" value={timestamp} setValue={setTimestamp} />
+  <Button
     type="submit"
     variant="primary"
     handleClick={handleSubmit}
     disabled={sending}
->
+  >
     {sending ? "Importing..." : "Import consultation"}
-</Button>
+  </Button>
 </form>
