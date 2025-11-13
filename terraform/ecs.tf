db77df2ae0a53efe12e116fe78fcf32e89725d5c
gecBurton <g.e.c.burton@gmail.com>
2025-11-13 16:05:13 +0000

    restired ecs spec

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -24,6 +24,7 @@ locals {
     "BACKEND_URL"                          = "http://${aws_service_discovery_service.service_discovery_service.name}.${aws_service_discovery_private_dns_namespace.private_dns_namespace.name}:${local.backend_port}",
     # need to duplicate this because Astro's import.meta.env only exposes environment variables that start with PUBLIC_.
     "PUBLIC_BACKEND_URL"                   = "http://${aws_service_discovery_service.service_discovery_service.name}.${aws_service_discovery_private_dns_namespace.private_dns_namespace.name}:${local.backend_port}",
+    "LLM_GATEWAY_URL"                      = "https://llm-gateway.i.ai.gov.uk/",
   }
 
   batch_env_vars = merge(local.base_env_vars, {
@@ -107,6 +108,9 @@ module "backend" {
     "RolePassableByRunner" = "True"
   }
   entrypoint = ["./start.sh"]
+
+  memory = 4096
+  cpu    = 1024
 
 }
 
