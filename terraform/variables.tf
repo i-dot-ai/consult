d2ca900b0645106a301b7ea1eb33bae7a6854d28
George Burton <8233643+gecBurton@users.noreply.github.com>
2025-11-18 15:49:22 +0000

    chore/move-to-core-ip-whitelist (#984)

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -70,22 +70,3 @@ variable "scope" {
   default     = "REGIONAL"
 }
 
-# <old-variables>
-
-variable "developer_ips" {
-  type        = list(string)
-  description = "List of developer IPs"
-}
-
-variable "external_ips" {
-  type        = list(string)
-  description = "List of external IPs"
-}
-
-variable "internal_ips" {
-  type        = list(string)
-  description = "IP's of No10 and CO"
-}
-
-# </old-variables>
-
