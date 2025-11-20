01ce03c7b8a345c35118e70d292c57fa58e2fb65
gecBurton <g.e.c.burton@gmail.com>
2025-11-20 11:57:00 +0000

    fixed import bug

--- consultation_analyser/consultations/urls.py
+++ consultation_analyser/consultations/urls.py
@@ -3,10 +3,10 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework import routers
 from rest_framework_nested.routers import NestedDefaultRouter
 
+from .api.views.auth import validate_token
 from .api.views.candidate_theme import CandidateThemeViewSet
 from .api.views.consultation import ConsultationViewSet
 from .api.views.git_sha import get_git_sha
-from .api.views.magic_link import validate_token
 from .api.views.question import QuestionViewSet
 from .api.views.respondent import RespondentViewSet
 from .api.views.response import ResponseViewSet
