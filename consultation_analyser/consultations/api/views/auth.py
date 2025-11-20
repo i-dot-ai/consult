1839fb196901ee0793caf8f8587454f9b8b6eadf
gecBurton <g.e.c.burton@gmail.com>
2025-11-20 16:53:39 +0000

    fixed basic mistake

--- consultation_analyser/consultations/api/views/auth.py
+++ consultation_analyser/consultations/api/views/auth.py
@@ -32,7 +32,7 @@ def validate_token(request):
         internal_access_token = serializer.validated_data["internal_access_token"]
         if HostingEnvironment.is_deployed():
             user_authorisation_info = client.get_user_authorisation_info(internal_access_token)
-            if user_authorisation_info.is_authorised:
+            if not user_authorisation_info.is_authorised:
                 logger.error("{email} is not authenticated", email=user_authorisation_info.email)
                 return JsonResponse(data={"detail": "authentication failed"}, status=403)
             email = user_authorisation_info.email
