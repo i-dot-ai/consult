cc1a0309dce57e99a69e9457f1037cc6828a17f6
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-11-21 10:25:44 +0000

    added loading message component

---
+++ frontend/src/components/LoadingMessage/LoadingMessage.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { slide } from "svelte/transition";
+
+  import LoadingIndicator from "../LoadingIndicator/LoadingIndicator.svelte";
+
+  interface Props {
+    message?: string;
+  }
+
+  let {
+    message = "",
+  }: Props = $props();
+</script>
+
+<div transition:slide class="my-8">
+  <LoadingIndicator size="5rem" />
+  <p class="text-center text-neutral-500">
+    {message || "Loading..."}
+  </p>
+</div>
