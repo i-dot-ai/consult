6333fc9a19cf629a0ea087b2dc419ae99829f817
gecBurton <g.e.c.burton@gmail.com>
2025-11-27 10:01:13 +0000

    bug fix

--- consultation_analyser/consultations/api/views/auth.py
+++ consultation_analyser/consultations/api/views/auth.py
@@ -24,7 +24,7 @@ class TokenSerializer(serializers.Serializer):
 @api_view(["POST"])
 @permission_classes([AllowAny])
 def validate_token(request):
-    logger.info("request.data={data}", data=request.request)
+    logger.info("request.data={data}", data=request.data)
     serializer = TokenSerializer(data=request.data)
     if not serializer.is_valid():
         return JsonResponse(serializer.errors, status=400)
