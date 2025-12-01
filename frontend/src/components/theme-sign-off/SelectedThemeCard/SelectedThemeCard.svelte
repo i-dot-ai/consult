5cb94c51e68282d3bea203100b0b115c13f739c7
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2025-12-01 10:11:34 +0000

    fixed width of representative responses

--- frontend/src/components/theme-sign-off/SelectedThemeCard/SelectedThemeCard.svelte
+++ frontend/src/components/theme-sign-off/SelectedThemeCard/SelectedThemeCard.svelte
@@ -143,7 +143,7 @@
           {#if showAnswers}
             <aside
               transition:fly={{ x: 300 }}
-              class="grow sm:border-l sm:border-neutral-200 sm:ml-4 sm:pl-4 pt-4 sm:pt-0"
+              class="grow sm:w-2/3 sm:border-l sm:border-neutral-200 sm:ml-4 sm:pl-4 pt-4 sm:pt-0"
             >
               <AnswersList
                 title="Representative Responses"
