c338e8deb3e63bd04a3367c540c4deef229f4b69
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2025-12-02 09:36:29 +0000

    Add test for unseenResponses

--- frontend/src/components/dashboard/FiltersSidebar/FiltersSidebar.test.ts
+++ frontend/src/components/dashboard/FiltersSidebar/FiltersSidebar.test.ts
@@ -92,4 +92,44 @@ describe("FiltersSidebar", () => {
     expect(setEvidenceRichMock).toHaveBeenCalledTimes(2);
     expect(setEvidenceRichMock).toHaveBeenCalledWith(true);
   });
+
+  it("should call set unseen responses func", async () => {
+    const user = userEvent.setup();
+    const setEvidenceRichMock = vi.fn();
+    const setUnseenResponsesMock = vi.fn();
+
+    render(FiltersSidebar, {
+      showEvidenceRich: testData.showEvidenceRich,
+      showUnseenResponse: true,
+      demoOptions: testData.demoOptions,
+      demoData: testData.demoData,
+      evidenceRich: testData.evidenceRich,
+      unseenResponses: testData.unseenResponses,
+      setEvidenceRich: setEvidenceRichMock,
+      setUnseenResponses: setUnseenResponsesMock,
+    });
+    expect(setUnseenResponsesMock).toHaveBeenCalledTimes(1);
+    expect(setUnseenResponsesMock).toHaveBeenCalledWith(false);
+
+    const unseenResponsesButton = screen.getByRole("switch", {
+      name: /show unseen responses/i,
+    });
+    await user.click(unseenResponsesButton);
+
+    expect(setUnseenResponsesMock).toHaveBeenCalledTimes(2);
+    expect(setUnseenResponsesMock).toHaveBeenCalledWith(true);
+  });
+
+  it("should not render unseen responses switch if not show unseen response", () => {
+    render(FiltersSidebar, {
+      showEvidenceRich: testData.showEvidenceRich,
+      showUnseenResponse: false,
+      demoOptions: testData.demoOptions,
+      demoData: testData.demoData,
+      evidenceRich: testData.evidenceRich,
+      unseenResponses: testData.unseenResponses,
+    });
+
+    expect(screen.queryByText("Show unseen responses")).not.toBeInTheDocument();
+  });
 });
