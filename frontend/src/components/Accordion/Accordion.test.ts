fcd29543748a7f78e18e40d6dc80e52e1bd32c48
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-01-02 14:14:42 +0000

    added Accordion tests

---
+++ frontend/src/components/Accordion/Accordion.test.ts
@@ -0,0 +1,49 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/svelte";
+
+import Accordion from "./Accordion.svelte";
+import { createRawSnippet } from "svelte";
+import userEvent from "@testing-library/user-event";
+import AccordionStory from "./AccordionStory.svelte";
+
+describe("Accordion", () => {
+  const testData = {
+    content: createRawSnippet(() => ({
+      render: () => "<p>Test Contents</p>",
+    })),
+    title: createRawSnippet(() => ({
+      render: () => "<h2>Test Title</h2>",
+    }))
+  }
+
+  it("should render title", () => {
+    render(Accordion, testData);
+    expect(screen.getByText("Test Title")).toBeInTheDocument();
+  });
+  it("should not render contents initially", () => {
+    render(Accordion, testData);
+    expect(screen.queryByText("Test Contents")).not.toBeInTheDocument();
+  });
+  it.each(["light", "gray"])("should render correctly", () => {
+    const { container } = render(Accordion, testData);
+    expect(container).toMatchSnapshot();
+  });
+  it("should render contents when expanded", async () => {
+    const user = userEvent.setup();
+    render(Accordion, testData);
+
+    const button = screen.getByRole("button");
+    await user.click(button);
+
+    expect(screen.queryByText("Test Contents")).toBeInTheDocument();
+  });
+
+  it("should have a story configured correctly", () => {
+    expect(AccordionStory).toHaveProperty("name", "Accordion");
+    expect(AccordionStory).toHaveProperty("component", Accordion);
+    expect(AccordionStory).toHaveProperty("props");
+
+    const propsDefined = AccordionStory.props.map(prop => prop.name);
+    expect(propsDefined).toEqual(["title", "content"]);
+  })
+});
