757f2e7da299fb946f30d2740ee948c8c1b11ee4
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-01-12 15:20:20 +0000

    Deleted support console

--- consultation_analyser/support_console/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class SupportConsoleConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "consultation_analyser.support_console"
