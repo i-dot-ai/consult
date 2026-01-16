6abc33ad945a8507a53ffbd26e9dda56471ea5c4
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-01-16 13:12:40 +0000

    Re-add django templates for admin pages

--- consultation_analyser/settings/base.py
+++ consultation_analyser/settings/base.py
@@ -80,6 +80,22 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = "consultation_analyser.urls"
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 WSGI_APPLICATION = "consultation_analyser.wsgi.application"
 
 AUTH_USER_MODEL = "authentication.User"
