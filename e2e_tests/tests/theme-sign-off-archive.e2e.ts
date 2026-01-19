251cdbe7a16bd532f9caac0d94005dcb4642d675
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-01-19 16:54:58 +0000

    added theme sign off archive tests

--- e2e_tests/tests/theme-sign-off-archive.e2e.ts
+++ e2e_tests/tests/theme-sign-off-archive.e2e.ts
@@ -2,49 +2,47 @@ import { test, expect } from '@playwright/test';
 
 
 const SECTION_TITLES = [
-  "Key Metrics",
-  "Favourited questions",
+  "Theme Sign Off",
   "All consultation questions",
 ]
 
-let exampleConsultation;
-let exampleConsultationQuestions;
+let testConsultation;
+let testConsultationQuestions;
 
 test.beforeAll(async ({ request }) => {
   const consultationsResponse = await request.get(`/api/consultations/?scope=assigned`);
   const consultationsResponseBody = await consultationsResponse.json();
-  exampleConsultation = consultationsResponseBody.results[0];
+  testConsultation = consultationsResponseBody.results[0];
 
-  const questionsResponse = await request.get(`/api/consultations/${exampleConsultation.id}/questions/`);
+  const questionsResponse = await request.get(`/api/consultations/${testConsultation.id}/questions/`);
   const questionsResponseBody = await questionsResponse.json();
-  exampleConsultationQuestions = questionsResponseBody.results;
+  testConsultationQuestions = questionsResponseBody.results;
 });
 
 test.beforeEach(async ({ page }) => {
-  await page.goto(`/consultations/${exampleConsultation.id}`);
+  await page.goto(`/consultations/${testConsultation.id}/theme-sign-off`);
 })
 
 test(`initially displays loading messages`, async ({ page }) => {
-  expect(page.getByText("Loading Demographics")).toBeVisible();
+  expect(page.getByText("Loading Questions")).toBeVisible();
+})
 
-  const questionsLoadingTexts = await page.getByText("Loading Questions").all();
-  expect(questionsLoadingTexts).toHaveLength(3);
+test(`displays header sub path text`, async ({ page }) => {
+  const subpathEl = page.getByTestId("header-subpath");
+  expect(subpathEl).toBeVisible();
+  expect(subpathEl).toHaveText("Theme Sign Off");
 })
 
 SECTION_TITLES.forEach(title => {
   test(`displays section titles: ${title}`, async ({ page }) => {
-    expect(page.getByText(title)).toBeVisible();
+    expect(page.getByRole("heading", { name: title, exact: true })).toBeVisible();
   })
-})
-
-test(`displays header sub path text`, async ({ page }) => {
-  expect(page.getByText("Dashboard")).toBeVisible();
 })
 
 test(`displays questions`, async ({ page }) => {
   await page.waitForLoadState('networkidle');
 
-  exampleConsultationQuestions.forEach(question => {
+  testConsultationQuestions.forEach(question => {
     const questionText = `Q${question.number}: ${question.question_text}`;
     expect(page.getByText(questionText, { exact: true })).toBeVisible();
   })
@@ -61,6 +59,9 @@ test(`searching questions updates question list`, async ({ page }) => {
 test(`favourites and unfavourites a question if button is clicked`, async ({ page }) => {
   await page.waitForSelector(`[data-testid="fav-button"]`);
 
+  const modalCloseButton = page.getByText("Get Started");
+  await modalCloseButton.click();
+
   // localStorage value starts as null
   await page.waitForFunction(() => {
     return localStorage.getItem("favouritedQuestions") === null;
@@ -70,7 +71,7 @@ test(`favourites and unfavourites a question if button is clicked`, async ({ pag
   const firstFavButton = favButtons.at(0);
 
   // button is clicked, localStorage value has the question ID in an array
-  const expectedValue = `["${exampleConsultationQuestions.at(0).id}"]`;
+  const expectedValue = `["${testConsultationQuestions.at(0).id}"]`;
   await firstFavButton!.click();
   await page.waitForFunction((expectedValue) => {
     return localStorage.getItem("favouritedQuestions") === expectedValue;
