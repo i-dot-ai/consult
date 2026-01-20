6f3ba069f3752be41053521d6036f99ee2c8ae6d
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-01-20 13:00:55 +0000

    Revert "Attempt claudes 2.0 fixes to the build pipeline"
    This reverts commit d6330ccb

--- backend/start.sh
+++ backend/start.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-../venv/bin/python manage.py migrate
-../venv/bin/python manage.py collectstatic --noinput
+venv/bin/django-admin migrate
+venv/bin/django-admin collectstatic --noinput
+venv/bin/django-admin createadminusers
+venv/bin/django-admin populate_history --auto --batchsize 1000
 
-# Skip createadminusers and populate_history in production if they fail
-../venv/bin/python manage.py createadminusers || true
-../venv/bin/python manage.py populate_history --auto --batchsize 1000 || true
-
-exec ../venv/bin/python -m gunicorn -c ./gunicorn.py backend.wsgi
+exec venv/bin/gunicorn -c ./backend/gunicorn.py backend.wsgi
