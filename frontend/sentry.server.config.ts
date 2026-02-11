3b69d277cbf4ea46494892fba8922ba7829d34e9
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-02-11 13:26:02 +0000

    Switch off sentry debug and change PII to false

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -2,6 +2,5 @@ import * as Sentry from "@sentry/astro";
 
 Sentry.init({
   dsn: "https://0c4cfe196193e5051fcb710c48cf69ad@o4507646230069248.ingest.de.sentry.io/4510839028777040",
-  sendDefaultPii: true,
-  debug: true,
+  sendDefaultPii: false,
 });
