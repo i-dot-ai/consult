7adb4c1d65f3c9286a1d61f6fcbe38d3e82deaa7
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-02-11 11:03:37 +0000

    Set debug for now

--- frontend/sentry.client.config.ts
+++ frontend/sentry.client.config.ts
@@ -3,4 +3,5 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
   dsn: import.meta.env.PUBLIC_SENTRY_DSN,
   sendDefaultPii: true,
+  debug: true,
 });
--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -3,4 +3,5 @@ import * as Sentry from "@sentry/astro";
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
   sendDefaultPii: true,
+  debug: true,
 });
