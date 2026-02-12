2d35af3ada25209ceb7cb66e5d0f9f894feb5ed2
gecBurton <g.e.c.burton@gmail.com>
2026-02-12 11:04:42 +0000

    bugfix/load-more-responses

--- frontend/src/components/screens/QuestionDetail.svelte
+++ frontend/src/components/screens/QuestionDetail.svelte
@@ -80,6 +80,7 @@
   let sortAscending: boolean = $state(false);
   let flaggedOnly: boolean = $state(false);
   let dataRequested: boolean = $state(false);
+  let isAnswersLoading: boolean = $state(true);
 
   const consultationStore = createFetchStore<ConsultationResponse>();
   const questionsStore = createFetchStore<QuestionsResponse>();
@@ -138,6 +139,7 @@
       const newAnswers = $answersStore.data?.all_respondents;
       answers = [...answers, ...newAnswers];
     }
+    isAnswersLoading = false;
     hasMorePages = $answersStore.data?.has_more_pages || false;
 
     currPage += 1;
@@ -186,6 +188,7 @@
     answers = [];
     currPage = 1;
     hasMorePages = true;
+    isAnswersLoading = true;
   }
 
   const resetFilters = () => {
@@ -384,7 +387,7 @@
         questionId={question?.id}
         pageSize={PAGE_SIZE}
         {answers}
-        isAnswersLoading={!dataRequested || $answersStore.isLoading}
+        isAnswersLoading={isAnswersLoading}
         answersError={$answersStore.error}
         filteredTotal={$answersStore.data?.filtered_total}
         {hasMorePages}
