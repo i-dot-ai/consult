69129ab89b77b42f8dc520ea1c3d91084382be64
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-02-18 14:17:34 +0000

    fix: remove mb-5 margin from Select wrapper
    Removed the bottom margin (mb-5) from the Select component wrapper div
    to fix vertical alignment with TextInput when placed side by side.
    
    This prevents the dropdown from taking up extra vertical space.

--- frontend/src/components/inputs/Select/Select.svelte
+++ frontend/src/components/inputs/Select/Select.svelte
@@ -61,7 +61,7 @@
 </script>
 
 <div
-  class={clsx(["mb-5", errorMessage && "mr-4 border-l-4 border-red-600 pl-3"])}
+  class={clsx([errorMessage && "mr-4 border-l-4 border-red-600 pl-3"])}
 >
   {#if labelText && !hideLabel}
     <label class={tailwindLabelClasses} for={id}>
