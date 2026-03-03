97a2cc7093fc3f5c072d6189a3d3a39e3ba5bbd8
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-03-03 15:23:11 +0000

    linted

--- frontend/src/components/data/IntroCard/IntroCard.test.ts
+++ frontend/src/components/data/IntroCard/IntroCard.test.ts
@@ -17,57 +17,48 @@ describe("IntroCard", () => {
   it("should render order number text", () => {
     render(IntroCard, testData);
 
-    expect(
-      screen.getByText(`Step ${testData.order}`),
-    ).toBeInTheDocument();
+    expect(screen.getByText(`Step ${testData.order}`)).toBeInTheDocument();
   });
 
   it("should render title", () => {
     render(IntroCard, testData);
 
-    expect(
-      screen.getByText(testData.title),
-    ).toBeInTheDocument();
+    expect(screen.getByText(testData.title)).toBeInTheDocument();
   });
 
   it("should render subtitle", () => {
     render(IntroCard, testData);
 
-    expect(
-      screen.getByText(testData.subtitle),
-    ).toBeInTheDocument();
+    expect(screen.getByText(testData.subtitle)).toBeInTheDocument();
   });
 
   it("should render arrow", () => {
     render(IntroCard, testData);
 
-    expect(
-      screen.getByTestId("intro-card-arrow"),
-    ).toBeInTheDocument();
+    expect(screen.getByTestId("intro-card-arrow")).toBeInTheDocument();
   });
 
   it("should not render arrow if showArrow is false", () => {
     render(IntroCard, { ...testData, showArrow: false });
 
-    expect(
-      screen.queryByTestId("intro-card-arrow"),
-    ).not.toBeInTheDocument();
+    expect(screen.queryByTestId("intro-card-arrow")).not.toBeInTheDocument();
   });
 
   it("should render as current if isActive is true", () => {
     render(IntroCard, testData);
 
-    expect(
-      screen.getByTestId("intro-card"),
-    ).toHaveAttribute("aria-current", "true");
+    expect(screen.getByTestId("intro-card")).toHaveAttribute(
+      "aria-current",
+      "true",
+    );
   });
 
   it("should not render as current if isActive is false", () => {
     render(IntroCard, { ...testData, isActive: false });
 
-    expect(
-      screen.queryByTestId("intro-card"),
-    ).not.toHaveAttribute("aria-current");
+    expect(screen.queryByTestId("intro-card")).not.toHaveAttribute(
+      "aria-current",
+    );
   });
 
   it("should match snapshot", () => {
