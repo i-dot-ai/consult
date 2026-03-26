d695e4cc1dcbc8e8fa926fb69a064d387b8a67d5
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-03-26 08:44:40 +0000

    Move new FileUpload model to models.py

--- backend/consultations/models.py
+++ backend/consultations/models.py
@@ -513,3 +513,16 @@ class MultiChoiceAnswer(UUIDPrimaryKeyModel, TimeStampedModel):  # type: ignore[
 
     def __str__(self):
         return f"{self.question.number} = {self.text}"
+
+
+class FileUpload(UUIDPrimaryKeyModel, TimeStampedModel):
+    consultation = models.ForeignKey(Consultation, on_delete=models.CASCADE, editable=False)
+    uploaded_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    s3_key = models.TextField(null=True, blank=True)
+
+    class Meta(UUIDPrimaryKeyModel.Meta, TimeStampedModel.Meta):
+        indexes = [
+            models.Index(fields=["consultation", "themefinder_id"]),
+        ]
