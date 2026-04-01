661e3d22a7c89c9264b4647a62fed6be5dd6a063
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-04-01 13:57:48 +0100

    updated css to move button to the right side of the checkbox item instead of bottom

--- frontend/src/components/data/Checklist/Checklist.svelte
+++ frontend/src/components/data/Checklist/Checklist.svelte
@@ -65,33 +65,35 @@
           onchange={(checked, value) => onChange(value!, checked)}
         />
 
-        <div>
-          <label for={getCheckboxId(item.id)} class="hover:cursor-pointer">
-            <Title level={4}>
-              <span class={clsx(["block", "mb-1"])}>
-                {item.title}
-              </span>
-            </Title>
-          </label>
+        <div class={clsx([ "w-full", ])}>
+          <div class={clsx([ "flex", "justify-between", "gap-4", "w-full", ])}>
+            <label for={getCheckboxId(item.id)} class="hover:cursor-pointer">
+              <Title level={4}>
+                <span class={clsx(["block", "mb-1"])}>
+                  {item.title}
+                </span>
+              </Title>
+            </label>
 
-          <Button
-            variant="ghost"
-            handleClick={() => {
-              if (expanded.includes(item.id)) {
-                expanded = expanded.filter((itemId) => itemId !== item.id);
-              } else {
-                expanded = [...expanded, item.id];
-              }
-            }}
-            ariaControls={`${item.id}-details`}
-            ariaLabel={`Show/Hide ${item.id}-details`}
-          >
-            <span class="-ml-2 text-secondary hover:underline">
-              {expanded.includes(item.id)
-                ? "Hide example"
-                : "Show me an example"}
-            </span>
-          </Button>
+            <Button
+              variant="ghost"
+              handleClick={() => {
+                if (expanded.includes(item.id)) {
+                  expanded = expanded.filter((itemId) => itemId !== item.id);
+                } else {
+                  expanded = [...expanded, item.id];
+                }
+              }}
+              ariaControls={`${item.id}-details`}
+              ariaLabel={`Show/Hide ${item.id}-details`}
+            >
+              <span class="-ml-2 text-secondary hover:underline text-nowrap">
+                {expanded.includes(item.id)
+                  ? "Hide example"
+                  : "Show me an example"}
+              </span>
+            </Button>
+          </div>
 
           {#if expanded.includes(item.id)}
             <div
