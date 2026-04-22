c9ee939fc0d790b2857e2d617b92dba1ca215c35
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-04-22 15:03:26 +0100

    linted

--- frontend/src/components/screens/AddUserForm/mocks.ts
+++ frontend/src/components/screens/AddUserForm/mocks.ts
@@ -3,5 +3,6 @@ import { Routes } from "../../../global/routes";
 export const defaultMock = {
   url: Routes.ApiUsers,
   method: "POST",
-  body: ({ body }: { body: unknown }) => alert("POST request sent with body: \n" + body),
+  body: ({ body }: { body: unknown }) =>
+    alert("POST request sent with body: \n" + body),
 };
