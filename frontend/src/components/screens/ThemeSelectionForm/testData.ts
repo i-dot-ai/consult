90076f2c5b9147c6804f3f0437a356f3955c4908
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-05-08 15:05:50 +0100

    updated test data

--- frontend/src/components/screens/ThemeSelectionForm/testData.ts
+++ frontend/src/components/screens/ThemeSelectionForm/testData.ts
@@ -6,6 +6,20 @@ export const allThemes = [
     description: "Theme one description",
     key: "theme-one",
   },
+  {
+    id: "theme-two",
+    name: "Theme Two",
+    assigned_by: "user@test.com",
+    description: "Theme two description",
+    key: "theme-two",
+  },
+  {
+    id: "theme-three",
+    name: "Theme Three",
+    assigned_by: "user@test.com",
+    description: "Theme three description",
+    key: "theme-three",
+  },
 ];
 
 export const selectedThemes = [
