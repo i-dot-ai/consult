e75c204ea527434b2c76afbe3245319e2a735972
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-05-11 08:53:03 +0100

    added story

--- frontend/src/components/screens/QuestionsReviewList/QuestionsReviewListStory.svelte.ts
+++ frontend/src/components/screens/QuestionsReviewList/QuestionsReviewListStory.svelte.ts
@@ -1,4 +1,4 @@
-import { CONSULTATION_ID } from "./mocks";
+import { CONSULTATION_ID, showNextFreeTextErrorMock, showNextMock } from "./mocks";
 import QuestionsReviewList from "./QuestionsReviewList.svelte";
 import { questionsData } from "./testData";
 
@@ -9,10 +9,23 @@ export default {
   name: "QuestionsReviewList",
   component: QuestionsReviewList,
   category: "Screens / Support",
-  mocks: [],
+  mocks: [
+    showNextMock,
+  ],
   props: [
     { name: "consultationId", value: consultationId, type: "text" },
     { name: "questions", value: questions, type: "json" },
   ],
-  stories: [],
+  stories: [
+    {
+      name: "Free Text Error",
+      props: {
+        consultationId,
+        questions,
+      },
+      mocks: [
+        showNextFreeTextErrorMock,
+      ]
+    }
+  ],
 };
--- frontend/src/components/screens/QuestionsReviewList/mocks.ts
+++ frontend/src/components/screens/QuestionsReviewList/mocks.ts
@@ -1,1 +1,21 @@
+import { getApiShowNextResponse } from "../../../global/routes";
+
 export const CONSULTATION_ID = "test-consultation";
+
+export const showNextMock = {
+  regexp: "*host" + getApiShowNextResponse(":consultationId", ":questionId"),
+  body: () => ({
+    next_response: {
+      consultation_id: CONSULTATION_ID,
+      question_id: "test-question",
+      id: "test-response",
+    },
+  })
+}
+
+export const showNextFreeTextErrorMock = {
+  regexp: "*host" + getApiShowNextResponse(":consultationId", ":questionId"),
+  body: () => ({
+    has_free_text: false,
+  })
+}
