09b4deaa039e3ce13b8dd673177374f7f3655998
Elliot Moore <elliot.moore@cabinetoffice.gov.uk>
2026-06-16 16:44:08 +0100

    Move bucket creation to app startup

--- backend/authentication/apps.py
+++ backend/authentication/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
+from django.conf import settings
+
+from consultations.utils import s3
 
 
 class AuthConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "authentication"
+
+    def ready(self):
+        logger = settings.LOGGER
+        if settings.ENVIRONMENT.upper() in ["LOCAL", "TEST"]:
+            s3_client = s3.get_s3_client()
+            buckets = s3_client.list_buckets()["Buckets"]
+            if not any(bucket["Name"] == settings.AWS_BUCKET_NAME for bucket in buckets):
+                logger.info(
+                    "Bucket not found, creating - {bucket_name}",
+                    bucket_name=settings.AWS_BUCKET_NAME,
+                )
+                s3_client.create_bucket(Bucket=settings.AWS_BUCKET_NAME)
--- backend/consultations/utils/s3.py
+++ backend/consultations/utils/s3.py
@@ -13,13 +13,6 @@ def get_s3_client():
             aws_secret_access_key=settings.AWS_SECRET_KEY,  # pragma: allowlist secret
             config=Config(signature_version="s3v4")
         )
-        buckets = s3_client.list_buckets()["Buckets"]
-        if not any(bucket["Name"] == settings.AWS_BUCKET_NAME for bucket in buckets):
-            logger.info(
-                "Bucket not found, creating - {bucket_name}",
-                bucket_name=settings.AWS_BUCKET_NAME,
-            )
-            s3_client.create_bucket(Bucket=settings.AWS_BUCKET_NAME)
     else:
         s3_client = boto3.client("s3")
     return s3_client
