916ecef0bc72ba86efd3ae8c199cf9afb89a440d
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-06-18 11:49:37 +0100

    created test file, added test cases

---
+++ e2e_tests/tests/support-console-detail.e2e.ts
@@ -0,0 +1,54 @@
+import { test, expect } from "@playwright/test";
+import { CleanupManager, createFixtureData } from "./helpers";
+
+import {
+  setupConsultation,
+  signOffConsultation,
+} from "../fixtures";
+
+import type { FixtureReference } from "../fixtures";
+
+test.describe("Support Console - Consultations Detail", () => {
+  let testData: FixtureReference = {};
+  const cleanupManager = new CleanupManager();
+
+  test.beforeAll(async ({ request }) => {
+    testData = await createFixtureData(request, {
+      consultations: [setupConsultation, signOffConsultation],
+    });
+    cleanupManager.add(testData);
+  });
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/support/consultations");
+    await page.waitForLoadState("networkidle");
+
+    await page.getByRole("button", { name: setupConsultation.title }).first().click();
+  });
+
+  test("displays consultation title", async ({ page }) => {
+    // Check the page heading
+    await expect(page.getByRole("heading", { name: setupConsultation.title })).toBeVisible();
+  })
+
+  const NAV_LINKS = [
+    { text: "View on frontend (question review)", url: new RegExp("/evaluations/.*/questions/") },
+    { text: "View finalise theme", url: new RegExp("/consultations/.*/finalising-themes") },
+    { text: "View on frontend (dashboard)", url: new RegExp("/consultations/.*") },
+    { text: "Import finalised themes from another consultation", url: new RegExp("/support/consultations/.*/import-themes") },
+    { text: "Delete this consultation", url: new RegExp("/support/consultations/.*/delete") },
+  ]
+  
+  NAV_LINKS.forEach(({ text, url }) => {
+    test(`displays nav link: ${text}`, async ({ page }) => {
+      // Check the page heading
+      const link = page.getByRole("button", { name: text });
+      await link.click();
+      expect(page).toHaveURL(url);
+    })
+  })
+
+  test.afterAll(async () => {
+    await cleanupManager.cleanup();
+  });
+});
