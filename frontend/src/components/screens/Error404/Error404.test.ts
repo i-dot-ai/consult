0b067ce4e69da4f62cacbbd0ca26e3ac2df7cc50
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-06-22 13:58:58 +0100

    fixed test describe and test case texts, removed unnecessary waitfor

--- frontend/src/components/screens/Error404/Error404.test.ts
+++ frontend/src/components/screens/Error404/Error404.test.ts
@@ -1,20 +1,18 @@
 import { describe, expect, it } from "vitest";
-import { render, screen, waitFor } from "@testing-library/svelte";
+import { render, screen } from "@testing-library/svelte";
 
 import Error404 from "./Error404.svelte";
 
-describe("EditUser", () => {
-  it("should display error message if fetch fails", async () => {
+describe("EditError404User", () => {
+  it("should display heading and text", async () => {
     render(Error404);
 
-    await waitFor(async () => {
-      expect(
-        screen.getByRole("heading", { name: "404 Not Found" }),
-      ).toBeInTheDocument();
-      expect(
-        screen.getByText("The page you requested could not be found"),
-      ).toBeInTheDocument();
-    });
+    expect(
+      screen.getByRole("heading", { name: "404 Not Found" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("The page you requested could not be found"),
+    ).toBeInTheDocument();
   });
 
   it("matches snapshot", () => {
