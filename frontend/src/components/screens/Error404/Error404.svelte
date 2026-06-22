f0a04c261a237ac1bd86cb23f1c1c59627af663d
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-06-22 09:30:25 +0100

    added 404 page component and 404 page route

---
+++ frontend/src/components/screens/Error404/Error404.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import MaterialIcon from "../../MaterialIcon.svelte";
+  import QuickReferenceAll from "../../svg/material/QuickReferenceAll.svelte";
+  import Text from "../../Text/Text.svelte";
+  import Title from "../../Title.svelte";
+</script>
+
+<div class="mt-16">
+  <div class="flex justify-center items-center my-8">
+    <MaterialIcon size="5rem" color="fill-neutral-500">
+      <QuickReferenceAll />
+    </MaterialIcon>
+  </div>
+
+  <Title level={1} context="public">
+    <span class="block text-primary text-center">
+      404 Not Found
+    </span>
+  </Title>
+  <Text>
+    <span class="block text-center">
+      The page you requested could not be found
+    </span>
+  </Text>
+</div>
