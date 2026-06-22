ed64602efa7ff0203bae0d0e83a9098eaedf0269
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-06-22 09:43:12 +0100

    linted

--- frontend/src/components/screens/Error404/Error404.test.ts
+++ frontend/src/components/screens/Error404/Error404.test.ts
@@ -8,8 +8,12 @@ describe("EditUser", () => {
     render(Error404);
 
     await waitFor(async () => {
-      expect(screen.getByRole("heading", { name: "404 Not Found" })).toBeInTheDocument();
-      expect(screen.getByText("The page you requested could not be found")).toBeInTheDocument();
+      expect(
+        screen.getByRole("heading", { name: "404 Not Found" }),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText("The page you requested could not be found"),
+      ).toBeInTheDocument();
     });
   });
 
