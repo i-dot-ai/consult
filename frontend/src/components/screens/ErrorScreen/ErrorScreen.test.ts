95148e0b36833142ad4c7a952d9350f175d953ee
Ekin Odabas <ekin.odabas@co-co088029.ad.cabinetoffice.gov.uk>
2026-06-24 10:02:52 +0100

    updated tests, added stories

---
+++ frontend/src/components/screens/ErrorScreen/ErrorScreen.test.ts
@@ -0,0 +1,38 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/svelte";
+
+import ErrorScreen from "./ErrorScreen.svelte";
+
+describe("ErrorScreen", () => {
+  it("should display heading and text", async () => {
+    render(ErrorScreen);
+
+    expect(
+      screen.getByRole("heading", { name: "404 Not Found" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("The page you requested could not be found"),
+    ).toBeInTheDocument();
+  });
+
+  it("should display heading and text", async () => {
+    render(ErrorScreen, { status: 500 });
+
+    expect(
+      screen.getByRole("heading", { name: "500 Internal Server Error" }),
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Something went wrong"),
+    ).toBeInTheDocument();
+  });
+
+  it("matches snapshot 404", () => {
+    const { container } = render(ErrorScreen);
+    expect(container).toMatchSnapshot();
+  });
+
+  it("matches snapshot 500", () => {
+    const { container } = render(ErrorScreen, { status: 500 });
+    expect(container).toMatchSnapshot();
+  });
+});
