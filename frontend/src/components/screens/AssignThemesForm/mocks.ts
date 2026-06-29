f137037eb2c8d40bbeee408960a11f9da7b87f34
Rachael Robinson <rachael.robinson@cabinetoffice.gov.uk>
2026-06-29 10:52:31 +0100

    remove AssignThemesForm

--- frontend/src/components/screens/AssignThemesForm/mocks.ts
+++
@@ -1,9 +0,0 @@
-import { getApiAssignThemesUrl } from "../../../global/routes";
-
-export const CONSULTATION_ID = "folder1";
-
-export const assignMock = {
-  url: getApiAssignThemesUrl(CONSULTATION_ID),
-  method: "POST",
-  name: "assignMock",
-};
