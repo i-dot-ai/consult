3f10cde729c60f5df2e7075aa7f35c46181af28c
Rachael Robinson <rachael.robinson@cabinetoffice.gov.uk>
2026-07-06 16:19:06 +0100

    check db, redis and s3 in healthcheck

--- backend/consultations/utils/s3.py
+++ backend/consultations/utils/s3.py
@@ -4,15 +4,17 @@ from django.conf import settings
 
 logger = settings.LOGGER
 
-def get_s3_client():
+def get_s3_client(config: Config | None = None):
+    config = config or Config()
     if settings.ENVIRONMENT.upper() in ["LOCAL", "TEST"]:
+        base_config = Config(signature_version="s3v4")
         s3_client = boto3.client(
             "s3",
             endpoint_url=settings.MINIO_ADDRESS,
             aws_access_key_id=settings.AWS_ACCESS_KEY,
             aws_secret_access_key=settings.AWS_SECRET_KEY,  # pragma: allowlist secret
-            config=Config(signature_version="s3v4")
+            config=base_config.merge(config),
         )
     else:
-        s3_client = boto3.client("s3")
+        s3_client = boto3.client("s3", config=config)
     return s3_client
