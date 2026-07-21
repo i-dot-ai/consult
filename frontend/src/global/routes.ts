7d1f898e0513c8329634e8426f30d53d8a960698
ekin-odabas <ekin.odabas@ai.gov.uk>
2026-07-21 10:06:25 +0100

    added api route for consultation

--- frontend/src/global/routes.ts
+++ frontend/src/global/routes.ts
@@ -68,6 +68,12 @@ export const getSupportConsultationImportThemesUrl = (
 export const getSupportConsultationEvaluationUrl = (consultationId: string) => {
   return urlJoin(Routes.SupportConsultations, consultationId, "evaluation");
 };
+export const getApiConsultationDetailUrl = (consultationId: string) => {
+  return urlJoin(
+    Routes.ApiConsultations,
+    consultationId,
+  );
+};
 export const getApiImportFinalisedThemesUrl = (consultationId: string) => {
   return urlJoin(
     Routes.ApiConsultations,
