b7575753673b9751b53bd4f5cddd6723278aef94
ekin-odabas <ekin.odabas@ai.gov.uk>
2026-08-03 10:03:36 +0100

    refactored fetchBackendApi

--- frontend/src/global/api.ts
+++ frontend/src/global/api.ts
@@ -2,34 +2,55 @@ import type { APIContext, AstroGlobal } from "astro";
 
 import { getBackendUrl } from "./utils";
 
+interface FetchBackendApiReturn<T> {
+  data?: T,
+  status: number,
+  error?: string,
+}
 export const fetchBackendApi = async <T>(
   astro: Readonly<AstroGlobal> | APIContext,
   endpoint: string,
   options: RequestInit | undefined = {},
-): Promise<T> => {
+): Promise<FetchBackendApiReturn<T>> => {
   const url = new URL(endpoint, getBackendUrl()).toString();
   const accessToken = astro.cookies.get("accessToken")?.value;
 
-  const response = await fetch(url, {
-    ...options,
-    headers: {
-      ...options.headers,
-      contentType: "application/json",
-      Authorization: `Bearer ${accessToken}`,
-      cookie: astro.request.headers.get("cookie") || "",
-    },
-  });
+  try {
+    const response = await fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        contentType: "application/json",
+        Authorization: `Bearer ${accessToken}`,
+        cookie: astro.request.headers.get("cookie") || "",
+      },
+    });
 
-  if (!response.ok) {
-    const error = await response.json();
-    throw { status: response.status, error };
-  }
+    if (!response.ok) {
+      const error = await response.json();
+      return ({
+        status: response.status,
+        error: error,
+      });
+    }
 
-  if (response.status == 204) {
-    // 204 No Content
-    return {} as Promise<T>;
-  } else {
-    return response.json() as Promise<T>;
+    if (response.status == 204) {
+      // 204 No Content
+      return ({
+        status: 204,
+        data: {} as T,
+      });
+    } else {
+      return {
+        status: response.status,
+        data: await response.json() as T,
+      };
+    }
+  } catch(err) {
+    return ({
+      status: 500,
+      error: "Fetch failed - unknown error",
+    })
   }
 };
 
@@ -52,8 +73,8 @@ export async function checkUrlStatus(
   url: string,
 ) {
   try {
-    await fetchBackendApi(astro, url);
-    return 200;
+    const { status } = await fetchBackendApi(astro, url);
+    return status;
   } catch (err) {
     return (err as { status: number }).status || 500;
   }
