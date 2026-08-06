c6a6e1b6020be4fc551bcc4bb8de6ea857e45e16
ekin-odabas <ekin.odabas@ai.gov.uk>
2026-08-06 15:31:40 +0100

    refactored logger setup

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -1,11 +1,19 @@
 /// <reference types="astro/client" />
 
+declare namespace App {
+  interface Locals {
+    contextId: string;
+  }
+}
+
 interface ImportMetaEnv {
   readonly PUBLIC_BACKEND_URL?: string;
   readonly PUBLIC_INTERNAL_ACCESS_CLIENT_ID?: string;
   readonly PUBLIC_LANGFUSE_URL?: string;
   readonly PUBLIC_HOMEPAGE_URL?: string;
   readonly ENVIRONMENT?: string;
+  readonly OTEL_EXPORTER_OTLP_ENDPOINT?: string;
+  readonly LOGGING_ENABLED?: string;
 }
 
 interface ImportMeta {
