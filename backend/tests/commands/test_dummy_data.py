5bcdd239d17d04740223af256688fe1d08adfb92
Elliot Moore <elliot.moore@dsit.gov.uk>
2026-08-19 13:10:42 +0100

    Fix changes in rebase and ruff formatting

--- backend/tests/commands/test_dummy_data.py
+++ backend/tests/commands/test_dummy_data.py
@@ -23,8 +23,9 @@ def test_name_parameter_sets_consultation_name(mock_is_local):
 @pytest.mark.django_db
 @pytest.mark.parametrize("environment", ["prod"])
 def test_the_tool_will_only_run_in_dev(environment):
-    with patch.dict(os.environ, {"ENVIRONMENT": environment}), pytest.raises(
-        Exception, match=r"Dummy data generation should not be run in production"
+    with (
+        patch.dict(os.environ, {"ENVIRONMENT": environment}),
+        pytest.raises(Exception, match=r"Dummy data generation should not be run in production"),
     ):
         call_command(
             "generate_dummy_data",
