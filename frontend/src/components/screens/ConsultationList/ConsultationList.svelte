1310178c31ba7e2ea4d785d980bef6a73c929da3
ekin-odabas <ekin.odabas@ai.gov.uk>
2026-09-04 11:42:58 +0100

    added DataTable to ConsultationList screen (wip)

--- frontend/src/components/screens/ConsultationList/ConsultationList.svelte
+++ frontend/src/components/screens/ConsultationList/ConsultationList.svelte
@@ -3,6 +3,8 @@
 
   import Title from "../../Title.svelte";
   import Link from "../../Link.svelte";
+  import Alert from "../../Alert.svelte";
+  import DataTable from "../../DataTable/DataTable.svelte";
   import LoadingMessage from "../../LoadingMessage/LoadingMessage.svelte";
 
   import {
@@ -11,9 +13,28 @@
     getFinaliseThemesUrl,
   } from "../../../global/routes.ts";
   import { buildConsultationsGetQuery } from "../../../global/queries/consultations/queries.ts";
-  import Alert from "../../Alert.svelte";
+  import type { Consultation } from "../../../global/types.ts";
 
   const consultations = buildConsultationsGetQuery();
+  const consultationRows = $derived(consultations.query.data?.results.map((consultation: Consultation) => ({
+    name: consultation.title,
+    createdAt: consultation.created_at,
+    evalLink: {
+      url: getConsultationEvalUrl(consultation.id),
+      ariaLabel: `View Evaluation for ${consultation.title}`,
+      text: "View Evaluation",
+    },
+    themesLink: {
+      url: getFinaliseThemesUrl(consultation.id),
+      ariaLabel: `Finalise Themes for ${consultation.title}`,
+      text: "Finalise Themes",
+    },
+    dashboardLink: {
+      url: getConsultationDetailUrl(consultation.id),
+      ariaLabel: `View Dashboard for ${consultation.title}`,
+      text: "View Dashboard",
+    },
+  })))
 </script>
 
 <section class="mt-4">
@@ -57,4 +78,49 @@
       {/each}
     </ul>
   {/if}
+
+  <DataTable
+    columns={[
+      { label: "Name", key: "name", sortable: true },
+      {
+        label: "Date Created",
+        key: "createdAt",
+        sortable: true,
+        sortValue: (item: any) => new Date(item.createdAt).getTime(),
+        displayValue: (item: any) => new Date(item.createdAt).toLocaleDateString(),
+      },
+      {
+        label: "Evaluation",
+        key: "evalLink",
+        sortable: false,
+      },
+      {
+        label: "Themes",
+        key: "themesLink",
+        sortable: false,
+      },
+      {
+        label: "Dashboard",
+        key: "dashboardLink",
+        sortable: false,
+      }
+    ]}
+    rows={consultationRows}
+    loading={consultations.query.isPending}
+  >
+    {#snippet cellContent(content, row, column)}
+      {#if ["evalLink", "themesLink", "dashboardLink"].includes(column.key)}
+        {@const rowData = row[column.key]}
+
+        <Link
+          href={rowData.url}
+          ariaLabel={rowData.ariaLabel}
+        >
+          {rowData.text}
+        </Link>
+      {:else}
+        <span>{content}</span>
+      {/if}
+    {/snippet}
+  </DataTable>
 </section>
