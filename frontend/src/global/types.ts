a7801974452c4949073574ebbe24631d55c6e950
ekin-odabas <ekin.odabas@ai.gov.uk>
2026-09-17 11:49:00 +0100

    added running job to consultation response and added new tests to cover running job alert display

--- frontend/src/global/types.ts
+++ frontend/src/global/types.ts
@@ -185,6 +185,7 @@ export interface ConsultationResponse {
   code: string;
   users: User[];
   stage: ConsultationStage;
+  running_job: null | "assign-themes" | "find-themes";
 }
 export interface QuestionsResponse {
   count: number;
