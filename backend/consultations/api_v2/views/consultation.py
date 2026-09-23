098c5e43295cb4c545766cf53b13f6625f6b11eb
Elliot Moore <elliot.moore@dsit.gov.uk>
2026-09-23 11:37:44 +0100

    PRO-787: Add deletion endpoint to consultation.py views

--- backend/consultations/api_v2/views/consultation.py
+++ backend/consultations/api_v2/views/consultation.py
@@ -1,7 +1,10 @@
 from typing import ClassVar
 
+from django.db.models import Q
+from rest_framework import status
 from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from consultations.api_v2.permissions import CanSeeConsultationV2
@@ -10,6 +13,7 @@ from consultations.api_v2.serializers import (
     ConsultationSerializerV2,
 )
 from consultations.models import Consultation
+from ingest.jobs import delete_consultation_job
 
 
 class ConsultationViewSet(CreateModelMixin, ReadOnlyModelViewSet):
@@ -28,4 +32,17 @@ class ConsultationViewSet(CreateModelMixin, ReadOnlyModelViewSet):
         queryset = Consultation.objects.prefetch_related("users").order_by("-created_at")
         if self.request.user.is_staff:
             return queryset
-        return queryset.filter(users=self.request.user)
+        return queryset.filter(
+            Q(users=self.request.user) | Q(created_by=self.request.user)
+        ).distinct()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        delete_consultation_job.delay(instance.id)
+        return Response(
+            {
+                "message": f"Deletion of consultation '{instance.title}' has been queued",
+                "consultation_id": str(instance.id),
+            },
+            status=status.HTTP_202_ACCEPTED,
+        )
